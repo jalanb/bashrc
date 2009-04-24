583a4d0c0433e78446a65f3a4468c3fceb162720
Alan Brogan <github@al-got-rhythm.net>
2009-04-24 02:03:20 +0000

    handle known paths
    git-svn-id: file:///usr/local/svn/jabs@99 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ python/site/paths.py
@@ -0,0 +1,29 @@
+from path import path
+
+home = path('~').expanduser()
+here = path('.')
+
+def mp3_files(self):
+	return self.files('*.mp3')
+
+path.mp3_files = mp3_files
+
+def environ_paths():
+	import environ
+	result = {}
+	for key,value in environ.jab.iteritems():
+		if key in result: raise KeyError('%s:%s -> %s' % (key,result[key],value))
+		paths = [ path(v) for v in value.split(':') if path(v).exists() ]
+		if paths:
+			if paths[1:]:
+				result[key] = paths
+			else:
+				result[key] = paths[0]
+		elif '/' in value:
+			result[key] = value
+	return result
+	raise str(result['MONTYLINGUA'])
+	if key == 'MONTYLINGUA': raise str(result[key])
+
+globals().update(environ_paths())
+
