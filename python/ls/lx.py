28074bac9e88c3c87d1adc12847a4b0f1986fb89
Alan Brogan <github@al-got-rhythm.net>
2009-05-02 03:02:30 +0000

    group dot files as a single extension
    git-svn-id: file:///usr/local/svn/jabs@144 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/ls/lx.py
+++ python/ls/lx.py
@@ -6,22 +6,25 @@ import path
 from l import get_dirs, get_files, show, show_dirs
 
 def get_exts(files):
-    exts = {}
-    for f in files:
-        ext = f.ext
-        got = exts.get(ext,[])
-        got.append(f)
-        try: exts[ext] = got
-        except Exception, e: raise ', '.join([ str(got), str(exts), ext, f ])
-    return exts
-        
+	exts = {}
+	for f in files:
+		if f.name.startswith('.'):
+			ext = '.'
+		else:
+			ext = f.ext
+		got = exts.get(ext,[])
+		got.append(f)
+		try: exts[ext] = got
+		except Exception, e: raise ', '.join([ str(got), str(exts), ext, f ])
+	return exts
+
 def main(args):
-    dirs = get_dirs(args)
-    show_dirs(dirs)
-    exts = get_exts(get_files(dirs))
-    if not exts: return 0
-    show(exts)
-    return 0
+	dirs = get_dirs(args)
+	show_dirs(dirs)
+	exts = get_exts(get_files(dirs))
+	if not exts: return 0
+	show(exts)
+	return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+	sys.exit(main(sys.argv[1:]))
