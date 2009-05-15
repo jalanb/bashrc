55820ac728e4771bb57a33efe86e3fa3db039b5a
Alan Brogan <github@al-got-rhythm.net>
2009-05-15 13:10:24 +0000

    show python files always, unless user hides them
    git-svn-id: file:///usr/local/svn/jabs@160 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/ls/ly.py
+++ python/ls/ly.py
@@ -6,33 +6,44 @@ import options
 
 try: any([])
 except NameError:
-	def any(x):
-		return len(x) > 0
+	def any(x): return [a for a in x if a]
 
 from l import get_dirs, get_files, screen_width, show_dirs, remove_ignored
 
 my_options = [
-	( 'python', 'show python scripts', True),
+	( 'ython', 'hide python scripts', False),
 	( 'tests', 'show tests', False),
 	( 'compiled', 'show compiled python', False),
 	( 'grammar', 'show grammars', False),
 	( 'fails', 'show fails', False),
-	( 'any', 'show others', False),
+	( 'all', 'show all python related files', False),
+	( 'others', 'show non python related files as well', False),
 ]
+
+def post_parse(options,args):
+	anything = False
+	wanted = {
+		'tests' : ['.test','.tests'],
+		'compiled' : [ '.pyc', '.pyo' ],
+		'grammar' : ['.g', '.j'],
+		'fails' : ['.fail'],
+	}
+	options.exts = ['.py',]
+	options.wanted = ['.py',]
+	for k,v in options.__dict__.iteritems():
+		try:
+			if v: options.wanted += wanted[k]
+			options.exts += wanted[k]
+		except KeyError: pass
+	return options, args
+	
 def wanted(path_to_file):
 	ext = path_to_file.ext
-	if options.options.any:
-		return True
-	rules = [
-		[[ '.py' ], options.options.python ],
-		[[ '.test', '.tests' ], options.options.tests ],
-		[[ '.pyc', '.pyo' ], options.options.compiled ],
-		[[ '.g' ], options.options.grammar ],
-		[[ '.fail' ], options.options.fails ],
-	]
-	for exts, option in rules:
-		if ext in exts:
-			return option
+	oo = options.options
+	if ext == '.py': return not oo.ython
+	if oo.others: return True
+	if oo.all: return ext in oo.exts
+	return ext in oo.wanted
 
 def get_stems(files):
 	stems = {}
@@ -44,7 +55,27 @@ def get_stems(files):
 		except Exception, e: raise ', '.join([ str(got), str(stems), stem, f ])
 	return stems
 		
+def check_exts(things):
+	oo = options.options
+	try: exts = [ f.ext for f in things ]
+	except AttributeError:
+		if ',' in things:
+			exts = things.split(',')
+			if exts[0].endswith('.py'):
+				exts[0] = '.py'
+		else:
+			exts = things
+	python_present = '.py' in exts or 'py' in exts
+	python_missing = '.py' not in exts and 'py' not in exts
+	hide_python = oo.ython
+	show_python = not oo.ython
+	if hide_python:
+		assert not python_present, str(exts)
+	if python_present:
+		assert not hide_python, str(exts)
+
 def filter_stems(stems):
+	oo = options.options
 	remove = []
 	for stem,files in stems.iteritems():
 		keeps = [f for f in files if wanted(f)]
@@ -52,6 +83,8 @@ def filter_stems(stems):
 			remove += [ stem ]
 		else:
 			stems[stem] = keeps
+			exts = [ k.ext for k in keeps ]
+			check_exts(exts)
 	for key in remove:
 		del stems[key]
 
@@ -70,10 +103,17 @@ def sort_by_name(x):
 def format(stems):
 	names_and_extensions = []
 	for name, files in stems.iteritems():
-		ordered_exts = [ 'py', 'test', 'tests', 'fail', 'pyc', 'pyo ' ]
+		ordered_exts = [ 'py', 'j', 'g', 'test', 'tests', 'fail', 'pyc', 'pyo' ]
+		oo = options.options
+		missing = [ e for e in oo.exts if e[1:] not in ordered_exts ]
+		assert not missing
 		f_exts = [ f.ext[1:] for f in files ]
+		check_exts(f_exts)
 		exts = [ e for e in ordered_exts if e in f_exts ] + [ e for e in f_exts if e not in ordered_exts ]
-		names_and_extensions += [ '%s.%s' % (name,' '.join(exts)) ]
+		check_exts(f_exts)
+		name_and_extensions = '%s.%s' % (name,','.join(exts))
+		check_exts(name_and_extensions)
+		names_and_extensions += [ name_and_extensions ]
 	return sort_by_name(names_and_extensions)
 
 def show_line(outs,columns_per_line):
@@ -90,7 +130,7 @@ def show(outs):
 	show_line(padded,columns_per_line)
 
 def main():
-	options.run(my_options)
+	options.run(my_options,post_parse)
 	dirs = get_dirs(options.args)
 	show_dirs(dirs)
 	files = get_files(dirs)
