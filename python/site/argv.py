0c5c548e597e4faadc8e3c9d292d129eb937466b
Alan Brogan <github@al-got-rhythm.net>
2009-05-21 01:03:35 +0000

    better testing leads to simpler code
    git-svn-id: file:///usr/local/svn/jabs@172 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/argv.py
+++ python/site/argv.py
@@ -2,16 +2,19 @@
 Convenience methods for populating an OptionPraser
 
 	>>> import argv
-	>>> option_parser = argv.make_parser( [
+	>>> my_opts = ( [
 	...	 ('exit','stop this nonsense'),
 	...	 ('save','backups are fun',False),
 	...	 ('name','who am i','Fred','f'),
 	... ] )
 
+	>>> my_parser = argv.make_parser( my_opts )
 That's an optparse.OptParser,
 	which handles command line options and args
 
-	>>> options, args = option_parser.parse_args(['-s','crash.log'])
+More commonly:
+	>>> argv.add(my_opts)
+	>>> options, args = option_parser.parse_args(command_line='-s crash.log'])
 	>>> print options.__dict__
 	{'save': True, 'exit': '', 'name': 'Fred'}
 	>>> print args
@@ -44,27 +47,27 @@ def ordered_option(option_arguments):
 		default = option_arguments[4]
 	)
 
-def letter_boolean_option(long_name,help,default,short_name):
-	'''Make a boolean option that can be passed to OptionParser.__init__()'''
-	action = 'store_true'
-	if default:
-		action = 'store_false'
-	return ordered_option((short_name,long_name,action,help,default))
+def make_argv_option(long_name,help,default,short=None,action=None):
+	'''Return an optparse option
+	
+	Short name defaulted to long name's initial.
+	Action defaults to default_action()
+	'''
+	if not short:
+		short = long_name[0]
+	action = default_action(default,action)
+	return ordered_option((short,long_name,action,help,default))
 
-def letter_string_option(long_name,help,default,short_name):
-	'''Make a string option that can be passed to OptionParser.__init__()'''
-	return ordered_option((short_name,long_name,'store',help,default))
+def default_action(default,action=None):
+	'''Default action for that type of default'''
+	if action: return action
+	if type(default) == type(True):
+		if default:
+			return 'store_false'
+		return'store_true'
+	return 'store'
 
-def initialise(option_method):
-	'''Return the same method, with short name defaulted to long name's initial'''
-	def freddy(long_name,help,default):
-		return option_method(long_name,help,default,long_name[0])
-	return freddy
-
-string_option = initialise(letter_string_option)
-boolean_option = initialise(letter_boolean_option)
-
-def as_four_tuple(tupel):
+def as_five_tuple(tupel):
 	'''Convert the tuple to a 4 tuple.
 
 	Add a default "" to a 2-tuple
@@ -72,7 +75,10 @@ def as_four_tuple(tupel):
 	Any other n-tuple
 		 raise Value Erro
 	'''
-	if len(tupel) == 4:
+	action = None
+	if len(tupel) == 5:
+		long, help, default, short, action = tupel
+	elif len(tupel) == 4:
 		long, help, default, short = tupel
 	elif len(tupel) == 3:
 		long, help, default = tupel
@@ -82,8 +88,8 @@ def as_four_tuple(tupel):
 		short = long[0]
 		default = ''
 	else:
-		raise ValueError('Use 2, 3, or 4 arguments, not %r' % tupel)
-	return long, help, default, short
+		raise ValueError('Use 2, 3, 4 or 5 arguments, not %r' % tupel)
+	return long, help, default, short, default_action(default,action)
 
 def listize(method):
 	'''Return a method which converts a list of tuples to a list of options.
@@ -93,13 +99,12 @@ def listize(method):
 	def to_options(tuples):
 		options = []
 		for tupel in tuples:
-			long, help, default, short = as_four_tuple(tupel)
-			options += [ method(long, help, default, short) ]
+			long, help, default, short, action = as_five_tuple(tupel)
+			options += [ method(long, help, default, short, action) ]
 		return options
 	return to_options
 
-strings = listize(letter_string_option)
-booleans = listize(letter_boolean_option)
+make_argv_options = listize(make_argv_option)
 
 def options_to_parser(options):
 	'''Make an OptionParser from a list of options'''
@@ -108,40 +113,50 @@ def options_to_parser(options):
 added_args = []
 def add(args): added_args[:0] = args
 
-def make_parser(args):
-	'''Make an OptionParser from a list of tuples'''
+def make_parser(args=None):
+	'''Make an OptionParser from a list of tuples
+
+	tuples can have
+		2 values: a string defaulting to ""
+		3 values: name, help, default
+		4 values: name, help, default, short name
+		5 values: name, help, default, short name, action
+	'''
 	options = []
 	if not args: args = []
 	tuples = args + added_args
 	for tupel in tuples:
-		tupel = as_four_tuple(tupel)
-		for t in tupel:
-			if type(t) == type(True):
-				options += [ letter_boolean_option(tupel[0],tupel[1],tupel[2],tupel[3]) ]
-				break
-		else:
-			options += [ letter_string_option(tupel[0],tupel[1],tupel[2],tupel[3]) ]
+		try: five_tuple = as_five_tuple(tupel)
+		except: raise ValueError(repr(tupel))
+		options += [ make_argv_option(*five_tuple) ]
 	return options_to_parser(options)
 
 options = None
 args = None
 post_parses = []
 
-def parse_args(tuples=None):
-	option_parser = make_parser(tuples)
+def parse_args(command_line=None):
+	'''Use options are made from a list of tuples
+
+	The list used is: those as args here, and any sent in to add() earlier
+
+	tuples can have
+		2 values: a string defaulting to ""
+		3 values: name, help, default
+		4 values: name, help, default, short name
+		5 values: name, help, default, short name, action
+	Type of the default sets the type of option
+		It also sets the action (unless supplied)
+	'''
+	option_parser = make_parser()
 	global options
 	global args
-	options, args = option_parser.parse_args()
+	if command_line:
+		try: command_line = command_line.split()
+		except AttributeError: pass
+		options, args = option_parser.parse_args(command_line)
+	else:
+		options, args = option_parser.parse_args()
 	for post_parse in post_parses:
 		options, args = post_parse(options,args)
-
-def test_args(command_line=None,tuples=None):
-	if not command_line: command_line = ''
-	option_parser = make_parser(tuples)
-	global options
-	global args
-	try:
-		options, args = option_parser.parse_args(command_line.split())
-		for post_parse in post_parses:
-			options, args = post_parse(options,args)
-	except SystemExit: pass
+	return options, args
