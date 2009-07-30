8f3b92411a9e25a006023db9ce50ee84ceb15cd7
Alan Brogan <github@al-got-rhythm.net>
2009-07-30 14:42:03 +0000

    use doc strings with examples for help
    git-svn-id: file:///usr/local/svn/jabs@231 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/dates.py
+++ python/site/dates.py
@@ -2,6 +2,11 @@ import time
 import datetime
 
 def now():
+	'''A datetime initialised when this is called
+
+	>>> now().year
+	2009
+	'''
 	ts = time.time()
 	frac = ts - int(ts)
 	year, month, day, hour, minute, second, weekday, yearday, daylight_saving_time = time.gmtime()
