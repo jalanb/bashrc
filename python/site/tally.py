2972e375257a5903d67c8e7bf43de90ad20d33bc
Alan Brogan <github@al-got-rhythm.net>
2009-07-30 14:39:33 +0000

    use doc strings with examples for help
    git-svn-id: file:///usr/local/svn/jabs@230 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/tally.py
+++ python/site/tally.py
@@ -15,18 +15,52 @@ class Tally(list):
 	at http://mail.python.org/pipermail/python-dev/2006-January/060115.html
 	'''
 	def __str__(self):
+		'''Represent the list as a string
+
+		A list of chars 
+		>>> str(Tally('abc'))
+		'abc'
+
+		A list of non-chars
+		>>> str(Tally([1,2,3]))
+		'[1, 2, 3]'
+		'''
 		try: return self.as_str()
 		except TypeError: return self.to_str()
 
 	def __repr__(self):
+		'''Represent the list as a type
+
+		A list of chars 
+		>>> Tally('abc')
+		<Tally 'abc'>
+
+		A list of non-chars
+		>>> Tally([1,2,3])
+		<Tally [1, 2, 3]>
+		'''
 		try: return "<%s '%s'>" % (self.class_name(),self.as_str())
 		except TypeError: return "<%s %s>" % (self.class_name(),self.to_str())
 
 	def as_str(self,separator=None):
+		'''Use the separator to join the chars into a string
+
+		>>> Tally('123').as_str('|')
+		'1|2|3'
+		'''
 		if separator is None: separator = ''
 		return separator.join(self)
 
 	def to_str(self,i=None,j=None,separator=None):
+		'''Turn the list into a string
+
+		i and j are indices to take only part of the list
+
+		>>> Tally([0,1,2,3,4,5]).to_str()
+		'[0, 1, 2, 3, 4, 5]'
+		>>> Tally([0,1,2,3,4,5]).to_str(2,5,'|')
+		'[2|3|4]'
+		'''
 		if i is None and j is None:
 			items = self
 		elif i is None:
@@ -39,6 +73,11 @@ class Tally(list):
 		return '[%s]' % separator.join([repr(s) for s in items])
 
 	def full_class_name(self):
+		'''The fully qualified class name
+
+		>>> print Tally([]).full_class_name()
+		tally.Tally
+		'''
 		class_repr = str(self.__class__)
 		prefix = "<class '"
 		suffix = "'>"
@@ -46,6 +85,11 @@ class Tally(list):
 		return trimmed
 
 	def class_name(self):
+		'''The unqualified class name
+
+		>>> print Tally([]).class_name()
+		Tally
+		'''
 		result = self.full_class_name()
 		i = 0
 		try:
@@ -55,23 +99,44 @@ class Tally(list):
 		return result[i:]
 
 	def list_name(self):
+		'''The plural of the class name
+		
+		Intended as the name to use for a list of these objects
+
+		>>> print Tally().list_name()
+		Tallies
+		'''
 		try:
 			list_of_items = self[0].list_name()
 		except AttributeError:
+			list_of_items = ''
+		except IndexError:
 			list_of_items = ''
 		if self.class_name() == list_of_items:
 			return self.class_name()
 		return pluralize( self.class_name() )
 
 	def str_class_and_list(self):
+		'''A tuple with three strings for the value, class and list names
+
+		>>> print Tally().str_class_and_list()
+		('', 'Tally', 'Tallies')
+		'''
 		return str(self), self.class_name(), self.list_name()
 
-def tlist(name,bases=None,ns=None):
-	if bases is None: bases = []
-	if ns is None: ns = {}
+def tlist(name,super_classes=None,name_space=None):
+	'''Create a sub-class of Tally with the given name
+
+	Super classes and a namespace can also be supplied
+
+	>>> tlist('world')('Hello').str_class_and_list()
+	('Hello', 'world', 'worlds')
+	'''
+	if super_classes is None: super_classes = []
+	if name_space is None: name_space = {}
 	class MySecondMeta(type):
-		def __init__(cls, name, bases, ns):
+		def __init__(cls, name, super_classes, name_space):
 			cls.lets_tlist_again_like_we_did_last_summer = True
 	escaped = name.replace(' ','_')
-	return MySecondMeta(escaped, (Tally,), ns)
+	return MySecondMeta(escaped, (Tally,), name_space)
 
