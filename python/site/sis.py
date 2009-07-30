9810a59171b497a64e27913c716534124c2ec522
Alan Brogan <github@al-got-rhythm.net>
2009-07-30 14:55:08 +0000

    use doc strings with examples for help
    git-svn-id: file:///usr/local/svn/jabs@232 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/sis.py
+++ python/site/sis.py
@@ -1,22 +1,32 @@
+'''A companion module to sys'''
 import sys
 from path import path as path_path
 
 paths = None
 
-def sys_path():
+def refresh_paths():
+	'''Refresh the module-level paths'''
 	global paths
 	paths = [ path_path(p) for p in sys.path ]
 
-def get_sys_path():
-	sys_path()
+def get_refreshed_paths():
+	'''Refresh the module-level paths and return it'''
+	refresh_paths()
 	return paths
 
 def sites(some_paths=None):
+	'''All items in some_paths which have "site-"
+
+	Uses sis.paths by default, so gives all the site- paths from sys
+	'''
 	if not some_paths: some_paths = paths
 	return [ l for l in some_paths if 'site-' in l ]
 
 def site_packages(some_paths=None):
+	'''All items in some_paths with basename "site-packages"
+
+	Uses sis.paths by default, so gives all site-packages from sys'''
 	if not some_paths: some_paths = paths
 	return [ p for p in some_paths if p.basename() =='site-packages' ]
 
-get_sys_path()
+refresh_paths()
