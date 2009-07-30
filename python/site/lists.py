63a551a69e84381d709badc07c445db5a87454c3
Alan Brogan <github@al-got-rhythm.net>
2009-07-30 14:00:34 +0000

    use doc strings with examples for help
    git-svn-id: file:///usr/local/svn/jabs@228 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/lists.py
+++ python/site/lists.py
@@ -22,6 +22,13 @@ def triplets(lyst):
 	return results
 
 def groups(match,lyst):
+	'''Split the list into sub-lists based on the match method
+
+	match takes two items and returns true if they are in the same group
+	>>> def match(a,b): return a == b
+	>>> groups(match,[0,0,0,1,1,0,1,1,'1'])
+	[[0, 0, 0], [1, 1], [0], [1, 1], ['1']]
+	'''
 	if lyst is None: return lyst
 	result = []
 	try: group = [ lyst[0] ]
@@ -36,30 +43,44 @@ def groups(match,lyst):
 	return result
 
 def before(x,i):
+	'''Split a list into two lists: everything before i, and the rest
+	
+	>>> before('Fred',2)
+	('Fr', 'ed')
+	'''
 	return  x[:i], x[i:]
 
 def before_and_after(x,i):
+	'''Split a list into three parts: everything before index i, at i, and all after it
+	
+	>>> before_and_after('Fred was here',4)
+	('Fred', ' ', 'was here')
+	'''
 	a, b = before(x,i)
 	c, d = before(b,1)
 	return a, c, d
 
 def befores_and_afters(x):
+	'''A generator giving before and after for each item in a list
+
+	>>> for x in befores_and_afters('Fred'): print x
+	('', 'F', 'red')
+	('F', 'r', 'ed')
+	('Fr', 'e', 'd')
+	('Fre', 'd', '')
+	'''
 	for i in range(0,len(x)):
 		yield before_and_after(x,i)
 
 
-def take_first(things):
-	return before_and_after(things,0)
-
-def take_next(before,item,after):
-	return before + item, after[:1], after[1:]
-
-def take(a,b=None,c=None):
-	if b is None:
-		return take_first(a)
-	else:
-		return take_next(a,b,c)
-
 def head_rest(x):
+	'''Return the head and the rest of a list
+
+	>>> head,rest = head_rest([23,45,67,99,45])
+	>>> head
+	23
+	>>> rest
+	[45, 67, 99, 45]
+	'''
 	a, b = before(x,1)
 	return a[0], b
