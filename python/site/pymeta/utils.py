9c7caa7ce7d1f395199e01cbc64931ff5f8d7903
Alan Brogan <github@al-got-rhythm.net>
2010-02-26 13:45:41 +0000

    test the moved directory
    git-svn-id: file:///usr/local/svn/jabs@307 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/pymeta/utils.py
+++ python/site/pymeta/utils.py
@@ -2,7 +2,10 @@ import os
 
 def save_source(source,modname):
 	main_python = os.environ['MAIN_PYTHON']
-	mymeta_directory = '/Users/jab/python/site/mymeta'
-	destination = os.path.join(mymeta_directory,'generated','%s.py' % modname)
+	pymeta_directory = os.path.dirname(__file__)
+	generated_directory = os.path.join(pymeta_directory,'generated')
+	if not os.path.isdir(generated_directory):
+		os.makedirs(generated_directory)
+	destination = os.path.join(generated_directory,'%s.py' % modname)
 	file(destination,'w').write(source)
 
