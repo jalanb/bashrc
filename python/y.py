134350b674875beccd1a313cd20ba9fc1c69c3bd
Alan Brogan <github@al-got-rhythm.net>
2010-04-01 03:02:54 +0000

    replace shell with python
    git-svn-id: file:///usr/local/svn/jabs@359 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ python/y.py
@@ -0,0 +1,53 @@
+import os
+import argv
+import paths
+
+argv.add_options([
+	('really', 'remove out python compiled files as well', False),
+	('wipe', 'remove known garbage',False),
+	('stat', 'run svn stat', False),
+	('ptags', 'do not refresh the tags file', True),
+	('verbose', 'run ptags verbosely', False),
+])
+
+from ls import ly
+
+def remove_globs(globs):
+	for glob in globs:
+		#print glob, [ f for f in argv.first_directory.files(glob) ]
+		for p in argv.first_directory.listdir(glob):
+			if p.islink():
+				p.unlink()
+			elif p.isfile():
+				p.remove()
+			elif p.isdir():
+				p.rmdir()
+			else:
+				raise ValueError( 'Do not know how to remove %s' % p )
+
+def wipe():
+	remove_globs([ '*~', '.*~', '*.orig', 'fred*', 'mary', '*.tmp', '*.bak', 'one', 'two' ])
+
+def really():
+	remove_globs([ '*.pyc', '*.pyo' ] )
+
+def svn_stat():
+	if paths.path('.svn').isdir():
+		os.system('svn stat ')
+
+def ptags():
+	import ptags
+	ptags.read_write_dir(argv.first_directory)
+
+def main():
+	#print 'cd', argv.first_directory
+	argv.first_directory.cd()
+	print argv.first_directory
+	for method in argv.methods:
+		method()
+	ly.show_dirs([p.relpathto(argv.first_directory) for p in argv.directories])
+	if argv.options.stat:
+		svn_stat()
+
+if __name__ == '__main__':
+	argv.main(globals())
