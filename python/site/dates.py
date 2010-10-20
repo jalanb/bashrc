daed4ad64a30df59fa51dde65903ea207b5f9f2a
Alan Brogan <github@al-got-rhythm.net>
2010-10-20 14:39:17 +0000

    More general testing
    git-svn-id: file:///usr/local/svn/jabs@465 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/dates.py
+++ python/site/dates.py
@@ -4,8 +4,14 @@ import datetime
 def now():
 	'''A datetime initialised when this is called
 
-	>>> now().year
-	2009
+	The year will be somtime this century
+	>>> 2000 < now().year < 2100
+	True
+
+	The month will be between 1 and 12
+	>>> 0 < now().month <= 12
+	True
+
 	'''
 	ts = time.time()
 	frac = ts - int(ts)
