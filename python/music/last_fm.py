776ccf1f54e6a67c0c7f1803ebb83b13b41d773e
Alan Brogan <github@al-got-rhythm.net>
2010-12-09 04:28:50 +0000

    switch to libfm
    git-svn-id: file:///usr/local/svn/jabs@485 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/music/last_fm.py
+++ python/music/last_fm.py
@@ -1,4 +1,4 @@
-import urllib
+import libfm
 
 def api_key():
 	return '2a8bc6369cdaaf9101ec74b29432b95a'
@@ -6,8 +6,37 @@ def api_key():
 def api_secret():
 	return 'e0965872b7f5a0988d41fd1f44471506'
 
-def get_recent_tracks(username):
-	url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&limit=%s" % (username, api_key(), 1000)
-	return urllib.urlopen(url)
-	return response.get('status')
+def username():
+	return 'jalanb'
 
+def password():
+	return 'bcx4bzrt'
+
+def init():
+	lib_fm = libfm.LibFM(api_key(),api_secret())
+	lib_fm.create_mobile_session(username(),password())
+	return lib_fm
+
+class LastFmPlayedTrack:
+	def __init__(self,data):
+		self.artist = data['artist']['#text']
+		self.album = data['album']['#text']
+		self.track = data['name']
+		self.date = data['date']['#text']
+
+	def __str__(self):
+		return '%s/%s at %s' % (self.artist, self.track, self.date)
+
+	def __repr__(self):
+		return '<LastFmPlayedTrack %r>' % unicode(self)
+
+def my_recent_tracks(session):
+	first = session.read('user.getRecentTracks',user='jalanb',limit=1)
+	number_of_tracks = first['recenttracks']['@attr']['totalPages']
+	result = []
+	i = 1
+	while len(result) < number_of_tracks and i < 2:
+		tracks = session.read('user.getRecentTracks',user='jalanb',page=i,limit=200)[u'recenttracks']['track']
+		result.extend( [LastFmPlayedTrack(t) for t in tracks])
+		i = i + 1
+	return number_of_tracks, result
