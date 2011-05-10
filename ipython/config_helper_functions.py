205f7c9ede8676c78acb6251fea82d4880677c48
Alan Brogan <github@al-got-rhythm.net>
2011-05-10 11:47:03 +0000

    configure ipython
    git-svn-id: file:///usr/local/svn/jabs@549 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ ipython/config_helper_functions.py
@@ -0,0 +1,23 @@
+import IPython.ipapi
+ip = IPython.ipapi.get()
+
+# some config helper functions you can use 
+def import_mod(modules):
+	""" Usage: import_mod("os sys") """ 
+	for m in modules.split():
+		ip.ex("import %s" % m)
+		
+def import_all(modules):
+	""" Usage: import_all("os sys") """ 
+	for m in modules.split():
+		ip.ex("from %s import *" % m)
+		
+def import_some(module,things):
+	'''Usage: import_some('path','makepath path')'''
+	for thing in things.split():
+		ip.ex('from %s import %s' % (module,thing))
+
+def execf(fname):
+	""" Execute a file in user namespace """
+	ip.ex('execfile("%s")' % os.path.expanduser(fname))
+
