a0197676237b41394c7aef736edbe7f690145935
Alan Brogan <github@al-got-rhythm.net>
2011-09-22 14:26:12 +0000

    better naming
    git-svn-id: file:///usr/local/svn/jabs@575 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- bin/add_to_a_path.py
+++ bin/add_to_a_path.py
@@ -8,42 +8,43 @@ import os
 import sys
 import doctest
 
-def add_path_to_paths(paths,path):
+def append_unique_item_to_list(paths,path):
 	'''Add a path to a list of paths
 
 	Include no empties nor duplicates in the result
+	Preserve the order of the list (which set() does not guarantee)
 
-	>>> add_path_to_paths([0], 1)
+	>>> append_unique_item_to_list([0], 1)
 	[1]
 
-	>>> add_path_to_paths([1], 2)
+	>>> append_unique_item_to_list([1], 2)
 	[1, 2]
 
-	>>> add_path_to_paths([1, 2, 3], 1)
+	>>> append_unique_item_to_list([1, 2, 3], 1)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 3], 1)
+	>>> append_unique_item_to_list([1, 2, 3], 1)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 3], 2)
+	>>> append_unique_item_to_list([1, 2, 3], 2)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 3], 3)
+	>>> append_unique_item_to_list([1, 2, 3], 3)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 2, 3], 2)
+	>>> append_unique_item_to_list([1, 2, 2, 3], 2)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 3, 2], 2)
+	>>> append_unique_item_to_list([1, 2, 3, 2], 2)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 2, 3, 2], 0)
+	>>> append_unique_item_to_list([1, 2, 3, 2], 0)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([1, 0, 2, 3], 0)
+	>>> append_unique_item_to_list([1, 0, 2, 3], 0)
 	[1, 2, 3]
 
-	>>> add_path_to_paths([0, 1, 2, 3, 2, 0], 0)
+	>>> append_unique_item_to_list([0, 1, 2, 3, 2, 0], 0)
 	[1, 2, 3]
 	'''
 	result = []
@@ -64,11 +65,11 @@ def add_path_to_path_string(path_string, path, separator=':'):
 	'''
 	paths = []
 	for p in path_string.split(separator):
-		paths = add_path_to_paths(paths,p)
+		paths = append_unique_item_to_list(paths,p)
 	if path:
 		real_path = os.path.realpath(path)
 		if os.path.isdir(real_path):
-			paths = add_path_to_paths(paths,real_path)
+			paths = append_unique_item_to_list(paths,real_path)
 	return separator.join(paths)
 
 def main(name_of_paths,new_path):
