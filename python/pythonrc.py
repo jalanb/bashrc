14e2ac3f98072283ce4b33bc90493f447c34f29a
Alan Brogan <github@al-got-rhythm.net>
2012-02-10 14:24:27 +0000

    re-formatting
    git-svn-id: file:///usr/local/svn/jabs@676 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/pythonrc.py
+++ python/pythonrc.py
@@ -1,18 +1,20 @@
-#! /usr/bin/env python
+"""Dirty up the main namespace with some extra imports"""
 
-# dirty up the main namespace a bit
 import os
 import sys
-from path import makepath
 from pprint import pprint as show
 
-try: import environ
-except ImportError: pass
-try: import paths
-except ImportError: pass
+try:
+	import environ
+except ImportError:
+	pass
+try:
+	import paths
+except ImportError:
+	pass
 
 try:
 	from see import see, code, see_methods, see_attributes, spread
 except ImportError:
-	def see(x):
-		show(dir(x))
+	def see(thing):
+		show(dir(thing))
