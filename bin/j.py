2f53ed4f0bd2bdad435458e6b021a9df5c0f2d12
Alan Brogan <github@al-got-rhythm.net>
2012-02-29 19:06:02 +0000

    update j.sh to use j.py
    git-svn-id: file:///usr/local/svn/jabs@700 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ bin/j.py
@@ -0,0 +1,263 @@
+"""
+maintains a jump-list of the directories you actually use
+
+INSTALL:
+  * put something like this in your .bashrc:
+	export JPY=/path/to/j.py # tells j.sh where the python script is
+	. /path/to/j.sh		  # provides the j() function
+  * make sure j.py is executable
+  * cd around for a while to build up the db
+  * PROFIT!!
+
+USE:
+  * j foo	 # goes to most frecent dir matching foo
+  * j foo bar # goes to most frecent dir matching foo and bar
+  * j -t rank # goes to highest ranked dir matching foo
+  * j -l foo  # list all dirs matching foo
+"""
+
+import os
+import sys
+import time
+from optparse import OptionParser
+
+def read_data_file(path_to_old_directories):
+	"""Read cache data from the given path
+
+	Data is stored as path|rank|atime
+	"""
+	if not os.path.isfile(path_to_old_directories):
+		return []
+	old_directories = [ line.strip().split('|') for line in file(path_to_old_directories, 'r') ]
+	result = []
+	for old_directory in old_directories:
+		try:
+			path, rank, atime = old_directory
+		except ValueError:
+			continue
+		result.append((path, float(rank), int(atime)))
+	return result
+
+def write_data_file(path_to_old_directories, data):
+	"""Write the given data to the given file
+
+	Data is stored as path|rank|atime
+	"""
+	out = None
+	try:
+		out = file(path_to_old_directories, 'w')
+		for path, rank, atime in data:
+			print >> out, '%s|%s|%s' % (path, rank, atime)
+	finally:
+		if out:
+			out.close()
+
+def rewrite_data_file(path_to_old_directories):
+	"""Read data, the write it back out again
+
+	Rewiting is preserves format (strips spaces, removes any line without "|"
+	"""
+	data = read_data_file(path_to_old_directories)
+	write_data_file(path_to_old_directories, data)
+	return data
+	
+def datum_to_match(datum):
+	"""Convert from data format (time since epoch) to match format (seconds since use)"""
+	path, rank, atime = datum
+	seconds_since_use = int(time.time()) - atime
+	return rank, seconds_since_use, path
+
+class DirectoryMemory(object):
+	"""Cache used directories to disk"""
+
+	def __init__(self, data):
+		self.data = data
+		self.all_arguments_match_a_common_prefix = None
+		self.path_matches = [ datum_to_match(d) for d in data ]
+		self.ranking_methods = {
+			'rank' : self.rank,
+			'recent' : self.recent,
+			'frecent': self.frecent
+	}
+
+	def pretty(self, ranking_algorithm):
+		""" return a listing by ranking_algorithm """
+		if ranking_algorithm not in self.ranking_methods:
+			return ''
+		result = ['by %s' % ranking_algorithm]
+		result.extend(['%-15s %s' % i for i in self.ranking_methods[ranking_algorithm]()])
+		return '\n'.join(result)
+
+	def find_destination(self, ranking_algorithm):
+		"""Find a destination by using the given ranking_algorithm """
+		if ranking_algorithm == 'common':
+			return self.all_arguments_match_a_common_prefix
+		if self.path_matches and ranking_algorithm in self.ranking_methods:
+			return self.ranking_methods[ranking_algorithm]()[-1][1]
+
+	def rank(self):
+		""" time spent/aging, taken care of in .sh """
+		return sorted(([(i[0], i[2]) for i in self.path_matches]))
+
+	def recent(self):
+		""" by recently accessed """
+		return  sorted(([(i[1], i[2]) for i in self.path_matches]), reverse = True)
+
+	def frecent(self):
+		""" rank weighted by recently accessed """
+		result = []
+		one_hour = (3600, 4.0)
+		one_day = (86400, 2.0)
+		one_week = (604800, 0.5)
+		for rank, seconds_since_match, path in self.path_matches:
+			for seconds, factor in [ one_hour, one_day, one_week ]:
+				if seconds_since_match < seconds:
+					result.append( (rank * factor, path) )
+					break
+			else:
+				result.append( ( rank * 0.25, path) )
+		return sorted(result)
+
+	def matches(self, arguments, ignore_case=False):
+		"""
+		set self.path_matches to a list of possibly case sensitive path matches
+		m format: (rank, atime, path)
+		"""
+		# pylint is not good at distinguishing sub-methods
+		# so ignore the warning about "Too many branches"
+		# pylint: disable-msg=R0912
+		def all_arguments_match_a_common_prefix(matches):
+			"""
+			return prefix if there's a common prefix to all matches,
+			the prefix is in the list,
+			and all the arguments match in it
+			"""
+			if not matches:
+				return None
+			prefix = os.path.commonprefix([i[2] for i in matches])
+			if not prefix or prefix == '/':
+				return None
+			prefixed_matches = [i for i in self.path_matches if i[2] == prefix]
+			if not prefixed_matches:
+				return None
+			if ignore_case:
+				prefix = prefix.lower()
+			for argument in arguments:
+				if argument not in prefix:
+					return None
+			return prefixed_matches[0][2]
+
+		def every_item_has(path):
+			""" every item in list arguments must match in path path """
+			match = True
+			if ignore_case:
+				path = path.lower()
+			for argument in arguments:
+				if argument not in path:
+					match = False
+			return match
+
+		self.path_matches = []
+		if ignore_case:
+			arguments = [i.lower() for i in arguments]
+		for path, rank, atime in self.data:
+			if not every_item_has(path):
+				continue
+			seconds_since_use = int(time.time()) - atime
+			path_match = (rank, seconds_since_use, path)
+			self.path_matches.append(path_match)
+		self.all_arguments_match_a_common_prefix = all_arguments_match_a_common_prefix(self.path_matches)
+		return self.path_matches
+
+	def add_path(self, new_path):
+		"""Add the given path to self.data
+
+		Do not add $HOME
+		
+		If there enough old paths then age them (reduce their rank by 10%)
+		"""
+		if os.path.realpath(new_path) == os.path.realpath(os.path.expanduser('~')):
+			return
+		new_rank = 1
+		new_data = []
+		sum_of_ranks = 0.0
+		for old_path, rank, atime in self.data:
+			sum_of_ranks += rank
+			if old_path == new_path:
+				new_rank = rank + 1
+			else:
+				new_data.append((old_path, rank, atime))
+		new_data.append( (new_path, new_rank, int(time.time())) )
+		max_sum = 1000.0
+		if sum_of_ranks > max_sum:
+			self.data = [ (path, rank * max_sum / sum_of_ranks, atime) for path, rank, atime in new_data ]
+		else:
+			self.data = new_data
+
+	def complete_path(self, path_to_complete, ranking_algorithm):
+		"""Complete a path for the shell"""
+		if self.path_matches and ranking_algorithm in self.ranking_methods:
+			result = []
+			method = self.ranking_methods[ranking_algorithm]
+			higher_ranked_first = reversed(method())
+			for _, path in higher_ranked_first:
+				if os.path.basename(path).startswith(path_to_complete):
+					result.append(path)
+			return ' '.join(result)
+		return path_to_complete
+
+def handle_command_line():
+	"""Read the command line and parse to options and arguments"""
+	parser = OptionParser(usage='%prog [options] part of a directory name')
+	parser.add_option('-a', '--add', dest='add_path', help='Add a path to the store')
+	parser.add_option('-c', '--complete', dest='complete', help='Complete a path for the shell')
+	parser.add_option('-f', '--file', dest='path_to_old_directories', help='where previously used directories are stored')
+	parser.add_option('-r', '--ranking', dest = 'ranking_algorithm', default='frecent', help='The ranking algorithm to use which is one of "frecent" (default), "rank" or "recent"')
+	parser.add_option('-l', '--list', dest='list', action='store_true', default=False, help='List all known directories')
+	parser.add_option('-d', '--documentation', action='store_true', default=False, help='documentation')
+	options, arguments = parser.parse_args()
+	if options.documentation:
+		sys.exit(__doc__)
+	if not options.path_to_old_directories:
+		options.path_to_old_directories = os.path.expanduser('~/.j2')
+	if options.ranking_algorithm not in ['frecent', 'rank', 'recent']:
+		print >> sys.stderr
+		print >> sys.stderr, '%r is not a known ranking algorithm' % options.ranking_algorithm
+		print >> sys.stderr
+		sys.exit(__doc__)
+	return options, arguments
+
+def final_argument_is_a_directory(arguments):
+	"""If we hit enter on a completion, go there"""
+	if not arguments:
+		return False
+	final_argument = arguments[-1]
+	if os.path.isdir(final_argument):
+		print final_argument
+		return True
+	return False
+
+def main():
+	""" make sure the only thing that gets to stdout is a place to cd """
+	options, arguments = handle_command_line()
+	if final_argument_is_a_directory(arguments):
+		return 0
+	data = rewrite_data_file(options.path_to_old_directories)
+	directory_memory = DirectoryMemory(data)
+	if options.complete:
+		output = directory_memory.complete_path(options.complete, options.ranking_algorithm)
+	elif options.add_path:
+		directory_memory.add_path(options.add_path)
+		write_data_file(options.path_to_old_directories, directory_memory.data)
+		return 0
+	elif options.list or not arguments:
+		output = directory_memory.pretty(options.ranking_algorithm)
+	else:
+		if not directory_memory.matches(arguments):
+			directory_memory.matches(arguments, True)
+		output = directory_memory.find_destination(options.ranking_algorithm)
+	print output
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main())
--- bin/j.sh
+++ bin/j.sh
@@ -1,72 +1,39 @@
-#! /bin/sh
+#!/bin/bash
 
-# From http://wiki.github.com/rupa/j
-# 
-# maintains a jump-list of directories you actually use
-# old directories eventually fall off the list
-# inspired by http://wiki.github.com/joelthelion/autojump
-# and something similar i had - but i could never get the dir list right.
-#
-# INSTALL:
-#   source into .bashrc under your '[-z "$PS1" ] || return' line
-#   cd around for a while
-#
-# USE:
-#   j [--l] [regex1 ... regexn]
-#     regex1 ... regexn jump to the most used directory matching all masks
-#     --l               show the list instead of jumping
-#                       with no args, returns full list
-j() {
- # change jfile if you already have a .j file for something else
- jfile=$HOME/.j
- if [ "$1" = "--add" ]; then
-  shift
-  # we're in $HOME all the time, let something else get all the good letters
-  [ "$*" = "$HOME" ] && return
-  awk -v q="$*" -v mx=1000 -F"|" '
-   $2 >= 1 { 
-    if( $1 == q ) { l[$1] = $2 + 1; x = 1 } else l[$1] = $2
-    y += $2
-   }
-   END {
-    x || l[q] = 1
-    if( y > mx ) {
-     for( i in l ) print i "|" l[i]*(0.9*mx/y) # aging
-    } else for( i in l ) print i "|" l[i]
-   }
-  ' $jfile 2>/dev/null > $jfile.tmp
-  test -f $jfile.tmp && mv -f $jfile.tmp $jfile
- elif [ "$1" = "" -o "$1" = "--l" ];then
-  shift
-  awk -v q="$*" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) print $2 "\t" $1 }
-  ' $jfile 2>/dev/null | sort -n
- # for completion
- elif [ "$1" = "--complete" ];then
-  awk -v q="$3" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) print $1 }
-  ' $jfile 2>/dev/null
- # if we hit enter on a completion just go there
- elif [ "${1:0:1}" = "/" -a -d "$*" ]; then
-  cd "$*" 
- else
-  # prefer case sensitive
-  cd=$(awk -v q="$*" -F"|" '
-   BEGIN { split(q,a," ") }
-   { for( o in a ) $1 !~ a[o] && $1 = ""; if( $1 ) { print $2 "\t" $1; x = 1 } }
-   END {
-    if( x ) exit
-    close(FILENAME)
-    while( getline < FILENAME ) {
-     for( o in a ) tolower($1) !~ tolower(a[o]) && $1 = ""
-     if( $1 ) print $2 "\t" $1
-    }
-   }
-  ' $jfile 2>/dev/null | sort -nr | head -n 1 | cut -f 2)
-  [ "$cd" ] && cd "$cd"
- fi
+python_source=$(python -c "import os; print '%s.py' % os.path.splitext(os.path.realpath(\"$BASH_SOURCE\"))[0]")
+if [[ -f $python_source ]]
+then
+
+j ()
+{
+    local datafile=$HOME/.j
+	if [[ ${1/--/} == $1 ]]
+	then
+		python $python_source $*
+	else
+		cd=$(python $python_source $*)
+		if [[ -d $cd ]]
+		then
+			cd $cd
+		fi
+	fi
 }
-# bash completions for j
-complete -o dirnames -o filenames -C "j --complete" j
+
+fi
+
+j_complete ()
+{
+	local cmd="${1##*/}"
+	local args=${COMP_LINE/$cmd/}
+	suggestion=$(python $python_source --complete ${line/$cmd/})
+	COMPREPLY=( $(compgen -W "$suggestion" "$cur") )
+}
+	
+# tab completion tested in bash
+complete -F j_complete j
+
+# So was the prompt alteration
+if [[ ${PROMPT_COMMAND/add/} == $PROMPT_COMMAND ]]
+then
+	PROMPT_COMMAND='j --add "$(pwd -P)";'"$PROMPT_COMMAND"
+fi
