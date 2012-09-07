aa11c68cdd41c836ff61c330e66e141a85fe2eee
Alan Brogan <github@al-got-rhythm.net>
2012-09-07 00:43:51 +0000

    better documentation
    git-svn-id: file:///usr/local/svn/jabs@820 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/kd.py
+++ python/kd.py
@@ -2,12 +2,20 @@
 
 It gets a close match to a directory from command line arguments
 	Then prints that to stdout
+	which allows a usage in bash like
+		$ cd $(python kd.py /usr local bin)
+	or
+		$ cd $(python kd.py /bin/ls)
 
 First argument is a directory
 	subsequent arguments are prefixes of sub-directories
-		For example:
+	For example:
 		$ python kd.py /usr/local bi
 		/usr/local/bin
+
+Or first argument is a file
+	$ python kd.py /bin/ls
+	/bin
 
 If nothing matches then give directories in $PATH which have matching executables
 	$ python kd.py ls
