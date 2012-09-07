7797a15a2d13d1fa488130056b93b82add8d3503
Alan Brogan <github@al-got-rhythm.net>
2012-09-07 10:50:09 +0000

    if first item is not a directory, try it as a prefix
    git-svn-id: file:///usr/local/svn/jabs@823 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/kd.py
+++ python/kd.py
@@ -129,9 +129,12 @@ def find_directory(item, prefixes):
 			return path_to_item
 		path_to_item = find_under_directory(path_to_item, prefixes)
 	else:
+		if item:
+			prefixes.insert(0, item)
 		if prefixes:
 			path_to_item = find_under_here(prefixes)
-		raise ValueError('%r is not a directory' % item)
+			if not path_to_item:
+				raise ValueError('could not use %r as a directory' % item)
 	if path_to_item:
 		return path_to_item
 	return find_in_environment_path(item)
