f078c2652c80ef1acb1e088a727d29885fe9849f
Alan Brogan <github@al-got-rhythm.net>
2012-10-11 00:05:27 +0000

    better test narratives
    git-svn-id: file:///usr/local/svn/jabs@853 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/testing/see.py
+++ python/testing/see.py
@@ -4,28 +4,28 @@
 see
 A human alternative to dir().
 
-    >>> from see import see
-    >>> help(see)
-    Help on function see in module see:
-    <BLANKLINE>
-    see(obj=anything, pattern=None, r=None, methods=None, attributes=None)
-        Inspect an object. Like the dir() builtin, but easier on the eyes.
-    <BLANKLINE>
-        Keyword arguments (all optional):
-        obj -- object to be inspected
-        pattern -- shell-style search pattern (e.g. '*len*')
-        r -- regular expression
-    <BLANKLINE>
-        If obj is omitted, objects in the current scope are listed instead.
-    <BLANKLINE>
-        Some unique symbols are used:
-    <BLANKLINE>
-            .*      implements obj.anything
-            []      implements obj[key]
-            in      implements membership tests (e.g. x in obj)
-            +obj    unary positive operator (e.g. +2)
-            -obj    unary negative operator (e.g. -2)
-    <BLANKLINE>
+	>>> from see import see
+	>>> help(see)
+	Help on function see in module see:
+	<BLANKLINE>
+	see(obj=anything, pattern=None, r=None, methods=None, attributes=None)
+	    Inspect an object. Like the dir() builtin, but easier on the eyes.
+	<BLANKLINE>
+	    Keyword arguments (all optional):
+	    obj -- object to be inspected
+	    pattern -- shell-style search pattern (e.g. '*len*')
+	    r -- regular expression
+	<BLANKLINE>
+	    If obj is omitted, objects in the current scope are listed instead.
+	<BLANKLINE>
+	    Some unique symbols are used:
+	<BLANKLINE>
+	        .*      implements obj.anything
+	        []      implements obj[key]
+	        in      implements membership tests (e.g. x in obj)
+	        +obj    unary positive operator (e.g. +2)
+	        -obj    unary negative operator (e.g. -2)
+	<BLANKLINE>
 
 Copyright (c) 2009 Liam Cooke
 http://inky.github.com/see/
@@ -59,13 +59,13 @@ __all__ = ['see'] #,'see_attributes', 'see_methods', 'code']
 
 __author__ = 'Liam Cooke'
 __contributors__ = [
-    'Bob Farrell',
-    'Gabriel Genellina',
-    'Baishampayan Ghose',
-    'Charlie Nolan',
-    'Ed Page',
-    'guff',
-    'jdunck',
+	'Bob Farrell',
+	'Gabriel Genellina',
+	'Baishampayan Ghose',
+	'Charlie Nolan',
+	'Ed Page',
+	'guff',
+	'jdunck',
 ]
 __version__ = '0.5.4'
 __copyright__ = 'Copyright (c) 2009 Liam Cooke'
@@ -73,108 +73,108 @@ __license__ = 'GNU General Public License v3'
 
 
 def regex_filter(names, pat):
-    pat = re.compile(pat)
+	pat = re.compile(pat)
 
-    def match(name, fn=pat.search):
-        return fn(name) is not None
-    return tuple(filter(match, names))
+	def match(name, fn=pat.search):
+	    return fn(name) is not None
+	return tuple(filter(match, names))
 
 
 def fn_filter(names, pat):
 
-    def match(name, fn=fnmatch.fnmatch, pat=pat):
-        return fn(name, pat)
-    return tuple(filter(match, names))
+	def match(name, fn=fnmatch.fnmatch, pat=pat):
+	    return fn(name, pat)
+	return tuple(filter(match, names))
 
 
 class _SeeOutput(tuple):
-    """Tuple-like object with a pretty string representation."""
+	"""Tuple-like object with a pretty string representation."""
 
-    def __new__(self, actions=None):
-        return tuple.__new__(self, actions or [])
+	def __new__(self, actions=None):
+	    return tuple.__new__(self, actions or [])
 
-    def __repr__(self):
-        return textwrap.fill('    '.join(self), 78,
-                             initial_indent='    ',
-                             subsequent_indent='    ')
+	def __repr__(self):
+	    return textwrap.fill('    '.join(self), 78,
+	                         initial_indent='    ',
+	                         subsequent_indent='    ')
 
 
 class _SeeDefault(object):
-    def __repr__(self):
-        return 'anything'
+	def __repr__(self):
+	    return 'anything'
 
 _LOCALS = _SeeDefault()
 
 
 def see(obj=_LOCALS, pattern=None, r=None, methods=None, attributes=None):
-    """
-    Inspect an object. Like the dir() builtin, but easier on the eyes.
+	"""
+	Inspect an object. Like the dir() builtin, but easier on the eyes.
 
-    Keyword arguments (all optional):
-    obj -- object to be inspected
-    pattern -- shell-style search pattern (e.g. '*len*')
-    r -- regular expression
+	Keyword arguments (all optional):
+	obj -- object to be inspected
+	pattern -- shell-style search pattern (e.g. '*len*')
+	r -- regular expression
 
-    If obj is omitted, objects in the current scope are listed instead.
+	If obj is omitted, objects in the current scope are listed instead.
 
-    Some unique symbols are used:
+	Some unique symbols are used:
 
-        .*      implements obj.anything
-        []      implements obj[key]
-        in      implements membership tests (e.g. x in obj)
-        +obj    unary positive operator (e.g. +2)
-        -obj    unary negative operator (e.g. -2)
+	    .*      implements obj.anything
+	    []      implements obj[key]
+	    in      implements membership tests (e.g. x in obj)
+	    +obj    unary positive operator (e.g. +2)
+	    -obj    unary negative operator (e.g. -2)
 
-    """
-    use_locals = obj is _LOCALS
-    actions = []
-    dot = not use_locals and '.' or ''
-    func = lambda f: hasattr(f, '__call__') and '()' or ''
-    name = lambda a, f: ''.join((dot, a, func(f)))
-    if methods is None and attributes is None:
-        methods = attributes = True
-    if use_locals:
-        obj.__dict__ = inspect.currentframe().f_back.f_locals
-    attrs = dir(obj)
-    if not use_locals:
-        for var, symbol in SYMBOLS:
-            if var not in attrs or symbol in actions:
-                continue
-            elif var == '__doc__':
-                if not obj.__doc__ or not obj.__doc__.strip():
-                    continue
-            actions.append(symbol)
+	"""
+	use_locals = obj is _LOCALS
+	actions = []
+	dot = not use_locals and '.' or ''
+	func = lambda f: hasattr(f, '__call__') and '()' or ''
+	name = lambda a, f: ''.join((dot, a, func(f)))
+	if methods is None and attributes is None:
+	    methods = attributes = True
+	if use_locals:
+	    obj.__dict__ = inspect.currentframe().f_back.f_locals
+	attrs = dir(obj)
+	if not use_locals:
+	    for var, symbol in SYMBOLS:
+	        if var not in attrs or symbol in actions:
+	            continue
+	        elif var == '__doc__':
+	            if not obj.__doc__ or not obj.__doc__.strip():
+	                continue
+	        actions.append(symbol)
 
-    def is_callable(p):
-        return hasattr(p,"__call__")
+	def is_callable(p):
+	    return hasattr(p,"__call__")
 
-    for attr in filter(lambda a: not a.startswith('_'), attrs):
-        try:
-            prop = getattr(obj, attr)
-        except AttributeError:
-            continue
-        if not methods and is_callable(prop): continue
-        if not attributes and not is_callable(prop): continue
-        actions.append(name(attr, prop))
+	for attr in filter(lambda a: not a.startswith('_'), attrs):
+	    try:
+	        prop = getattr(obj, attr)
+	    except AttributeError:
+	        continue
+	    if not methods and is_callable(prop): continue
+	    if not attributes and not is_callable(prop): continue
+	    actions.append(name(attr, prop))
 
-    if pattern is not None:
-        actions = fn_filter(actions, pattern)
-    if r is not None:
-        actions = regex_filter(actions, r)
+	if pattern is not None:
+	    actions = fn_filter(actions, pattern)
+	if r is not None:
+	    actions = regex_filter(actions, r)
 
-    return _SeeOutput(actions)
+	return _SeeOutput(actions)
 
 def see_methods(*args,**kwargs):
-    kwargs['methods'] = True
-    return see(*args,**kwargs)
+	kwargs['methods'] = True
+	return see(*args,**kwargs)
 
 def see_attributes(*args,**kwargs):
-    kwargs['attributes'] = True
-    return see(*args,**kwargs)
+	kwargs['attributes'] = True
+	return see(*args,**kwargs)
 
 def spread(thing, exclude = None):
 	'''Spread out the attributes of thing onto stdout
-	
+
 	exclude is a list of regular expressions
 		attributes matching any if these will not be shown
 		if the default of None is used it is set to [ '__.*__' ]
@@ -222,134 +222,134 @@ def spread(thing, exclude = None):
 	print spread_out_the_attributes(thing,'\n\t')
 
 def indent(filename,line_number):
-    line = linecache.getline(filename,line_number)
-    match = re.match('(\s*)',line)
-    if not match:
-        return None, None
-    return line.rstrip(), match.groups()[0]
-    
+	line = linecache.getline(filename,line_number)
+	match = re.match('(\s*)',line)
+	if not match:
+	    return None, None
+	return line.rstrip(), match.groups()[0]
+
 def code(method):
-    if not callable(method):
-        print 'Not callable: %r' % method
-        return
-    result = [ ]
-    try: first_line_number = method.func_code.co_firstlineno
-    except AttributeError:
-        print 'No code available for', method
-        return
-    filename = method.func_code.co_filename
-    if filename.endswith('.pyc') or filename.endswith('.pyo'):
-        filename = filename[:-1]
-    line, first_indentation = indent(filename,first_line_number)
-    result = [ line ]
-    line_number = first_line_number + 1
-    line, indentation = indent(filename,line_number)
-    while indentation > first_indentation:
-        result.append( line )
-        line_number += 1
-        line, indentation = indent(filename,line_number)
-    print '\n'.join(result)
+	if not callable(method):
+	    print 'Not callable: %r' % method
+	    return
+	result = [ ]
+	try: first_line_number = method.func_code.co_firstlineno
+	except AttributeError:
+	    print 'No code available for', method
+	    return
+	filename = method.func_code.co_filename
+	if filename.endswith('.pyc') or filename.endswith('.pyo'):
+	    filename = filename[:-1]
+	line, first_indentation = indent(filename,first_line_number)
+	result = [ line ]
+	line_number = first_line_number + 1
+	line, indentation = indent(filename,line_number)
+	while indentation > first_indentation:
+	    result.append( line )
+	    line_number += 1
+	    line, indentation = indent(filename,line_number)
+	print '\n'.join(result)
 
 PY_300 = sys.version_info >= (3, 0)
 PY_301 = sys.version_info >= (3, 0, 1)
 
 SYMBOLS = tuple(filter(lambda x: x[0], (
-    # callable
-    ('__call__', '()'),
+	# callable
+	('__call__', '()'),
 
-    # element/attribute access
-    ('__getattr__', '.*'),
-    ('__getitem__', '[]'),
-    ('__setitem__', '[]'),
-    ('__delitem__', '[]'),
+	# element/attribute access
+	('__getattr__', '.*'),
+	('__getitem__', '[]'),
+	('__setitem__', '[]'),
+	('__delitem__', '[]'),
 
-    # iteration
-    ('__enter__', 'with'),
-    ('__exit__', 'with'),
-    ('__contains__', 'in'),
+	# iteration
+	('__enter__', 'with'),
+	('__exit__', 'with'),
+	('__contains__', 'in'),
 
-    # operators
-    ('__add__', '+'),
-    ('__radd__', '+'),
-    ('__iadd__', '+='),
-    ('__sub__', '-'),
-    ('__rsub__', '-'),
-    ('__isub__', '-='),
-    ('__mul__', '*'),
-    ('__rmul__', '*'),
-    ('__imul__', '*='),
-    (not PY_300 and '__div__', '/'),
-    (not PY_301 and '__rdiv__', '/'),
-    ('__truediv__', '/'),
-    ('__rtruediv__', '/'),
-    ('__floordiv__', '//'),
-    ('__rfloordiv__', '//'),
-    (not PY_300 and '__idiv__', '/='),
-    ('__itruediv__', '/='),
-    ('__ifloordiv__', '//='),
-    ('__mod__', '%'),
-    ('__rmod__', '%'),
-    ('__divmod__', '%'),
-    ('__imod__', '%='),
-    ('__pow__', '**'),
-    ('__rpow__', '**'),
-    ('__ipow__', '**='),
-    ('__lshift__', '<<'),
-    ('__rlshift__', '<<'),
-    ('__ilshift__', '<<='),
-    ('__rshift__', '>>'),
-    ('__rrshift__', '>>'),
-    ('__irshift__', '>>='),
-    ('__and__', '&'),
-    ('__rand__', '&'),
-    ('__iand__', '&='),
-    ('__xor__', '^'),
-    ('__rxor__', '^'),
-    ('__ixor__', '^='),
-    ('__or__', '|'),
-    ('__ror__', '|'),
-    ('__ior__', '|='),
-    ('__pos__', '+obj'),
-    ('__neg__', '-obj'),
-    ('__invert__', '~'),
-    ('__lt__', '<'),
-    (not PY_301 and '__cmp__', '<'),
-    ('__le__', '<='),
-    (not PY_301 and '__cmp__', '<='),
-    ('__eq__', '=='),
-    (not PY_301 and '__cmp__', '=='),
-    ('__ne__', '!='),
-    (not PY_301 and '__cmp__', '!='),
-    ('__gt__', '>'),
-    (not PY_301 and '__cmp__', '>'),
-    ('__ge__', '>='),
-    (not PY_301 and '__cmp__', '>='),
+	# operators
+	('__add__', '+'),
+	('__radd__', '+'),
+	('__iadd__', '+='),
+	('__sub__', '-'),
+	('__rsub__', '-'),
+	('__isub__', '-='),
+	('__mul__', '*'),
+	('__rmul__', '*'),
+	('__imul__', '*='),
+	(not PY_300 and '__div__', '/'),
+	(not PY_301 and '__rdiv__', '/'),
+	('__truediv__', '/'),
+	('__rtruediv__', '/'),
+	('__floordiv__', '//'),
+	('__rfloordiv__', '//'),
+	(not PY_300 and '__idiv__', '/='),
+	('__itruediv__', '/='),
+	('__ifloordiv__', '//='),
+	('__mod__', '%'),
+	('__rmod__', '%'),
+	('__divmod__', '%'),
+	('__imod__', '%='),
+	('__pow__', '**'),
+	('__rpow__', '**'),
+	('__ipow__', '**='),
+	('__lshift__', '<<'),
+	('__rlshift__', '<<'),
+	('__ilshift__', '<<='),
+	('__rshift__', '>>'),
+	('__rrshift__', '>>'),
+	('__irshift__', '>>='),
+	('__and__', '&'),
+	('__rand__', '&'),
+	('__iand__', '&='),
+	('__xor__', '^'),
+	('__rxor__', '^'),
+	('__ixor__', '^='),
+	('__or__', '|'),
+	('__ror__', '|'),
+	('__ior__', '|='),
+	('__pos__', '+obj'),
+	('__neg__', '-obj'),
+	('__invert__', '~'),
+	('__lt__', '<'),
+	(not PY_301 and '__cmp__', '<'),
+	('__le__', '<='),
+	(not PY_301 and '__cmp__', '<='),
+	('__eq__', '=='),
+	(not PY_301 and '__cmp__', '=='),
+	('__ne__', '!='),
+	(not PY_301 and '__cmp__', '!='),
+	('__gt__', '>'),
+	(not PY_301 and '__cmp__', '>'),
+	('__ge__', '>='),
+	(not PY_301 and '__cmp__', '>='),
 
-    # built-in functions
-    ('__abs__', 'abs()'),
-    (PY_300 and '__bool__' or '__nonzero__', 'bool()'),
-    ('__complex__', 'complex()'),
-    (PY_300 and '__dir__', 'dir()'),
-    ('__divmod__', 'divmod()'),
-    ('__rdivmod__', 'divmod()'),
-    ('__float__', 'float()'),
-    ('__hash__', 'hash()'),
-    ('__doc__', 'help()'),
-    (PY_300 and '__index__' or '__hex__', 'hex()'),
-    ('__int__', 'int()'),
-    ('__iter__', 'iter()'),
-    ('__len__', 'len()'),
-    (not PY_300 and '__long__', 'long()'),
-    (PY_300 and '__index__' or '__oct__', 'oct()'),
-    ('__repr__', 'repr()'),
-    ('__reversed__', 'reversed()'),
-    (PY_300 and '__round__', 'round()'),
-    ('__str__', 'str()'),
-    (PY_300 and '__unicode__', 'unicode()'),
+	# built-in functions
+	('__abs__', 'abs()'),
+	(PY_300 and '__bool__' or '__nonzero__', 'bool()'),
+	('__complex__', 'complex()'),
+	(PY_300 and '__dir__', 'dir()'),
+	('__divmod__', 'divmod()'),
+	('__rdivmod__', 'divmod()'),
+	('__float__', 'float()'),
+	('__hash__', 'hash()'),
+	('__doc__', 'help()'),
+	(PY_300 and '__index__' or '__hex__', 'hex()'),
+	('__int__', 'int()'),
+	('__iter__', 'iter()'),
+	('__len__', 'len()'),
+	(not PY_300 and '__long__', 'long()'),
+	(PY_300 and '__index__' or '__oct__', 'oct()'),
+	('__repr__', 'repr()'),
+	('__reversed__', 'reversed()'),
+	(PY_300 and '__round__', 'round()'),
+	('__str__', 'str()'),
+	(PY_300 and '__unicode__', 'unicode()'),
 )))
 
 
 if __name__ == '__main__':
-    help(see)
+	help(see)
 
 # vim: expandtab tabstop=4 shiftround shiftwidth=4 fdm=marker
--- python/testing/test_files.py
+++ python/testing/test_files.py
@@ -1,4 +1,4 @@
-"""This module provides convenience methids to handle test files
+"""This module provides convenience methods to handle test files
 
 Test files are expected to exist as triples:
 	fred.py, fred.test, fred.tests
@@ -8,6 +8,7 @@ We also allow for python scripts which have no extension
 """
 
 
+import os
 import re
 
 
@@ -15,6 +16,7 @@ from paths import makepath
 
 
 class UserMessage(Exception):
+	"""Tell the user something went wrong"""
 	pass
 
 
@@ -43,34 +45,47 @@ def _get_path_to_test_directory(args):
 	return result
 
 
-def _any_extension(p):
-	"""A list of all existing extensions of the given path"""
-	if p.isfile():
-		dirr = p.parent
-		glob = p.namebase + '.*'
-	elif p.isdir():
-		dirr = p
+def _existing_test_files(path_to_stem):
+	"""A list of all existing extensions of the given path
+
+	Find all such extensions by replacing any extension in a real file
+		or adding "/*" if it is a directory
+		or otherwise looking the current directory
+
+	Not expecting any test scripts in the root directory
+	>>> _existing_test_files(makepath('/test_files.txt')) == []
+	True
+
+	But we should expect them for this script
+	>>> __file__ in _existing_test_files(makepath(__file__))
+	True
+	"""
+	if path_to_stem.isfile():
+		dirr = path_to_stem.parent
+		glob = path_to_stem.namebase + '.*'
+	elif path_to_stem.isdir():
+		dirr = path_to_stem
 		glob = '*'
 	else:
-		if p.parent:
-			dirr = p.parent
+		if path_to_stem.parent:
+			dirr = path_to_stem.parent
 		else:
 			dirr = makepath('.')
-		glob = '%s.*' % p.namebase
+		glob = '%s.*' % path_to_stem.namebase
 	if not dirr.isdir():
 		return []
 	return [f for f in dirr.files(glob)]
 
 
-def existing_test_extensions(path_stem):
-	"""A list of all text extensions of the given paths stem"""
-	return [f for f in _any_extension(path_stem) if f.ext in possible_test_extensions()]
+def _existing_test_extensions(path_stem):
+	"""A list of all text extensions of the given path stem"""
+	return [f for f in _existing_test_files(path_stem) if f.ext in possible_test_extensions()]
 
 
-def get_paths(args, recursive):
+def _get_path_stems(args, recursive):
 	"""A list of any paths in args
 
-	Of there is a "/" in the args then it is used in the list
+	If there is a "/" in the args then it is used in the list
 	Otherwise it is appended to the current working directory
 	"""
 	if not args:
@@ -79,10 +94,10 @@ def get_paths(args, recursive):
 	paths = []
 	for arg in args:
 		if '/' in arg:
-			p = makepath(arg)
+			path_to_arg = makepath(arg)
 		else:
-			p = here / arg
-		paths.append(p)
+			path_to_arg = here / arg
+		paths.append(path_to_arg)
 	if recursive:
 		paths = add_sub_dirs(paths)
 	return paths
@@ -92,8 +107,8 @@ def add_sub_dirs(paths):
 	"""Add all sub-directories for the directories of the paths"""
 	dirs = set([p.directory() for p in paths])
 	result = list(dirs)
-	for p in dirs:
-		for sub_dir in p.walkdirs():
+	for path_to_dir in dirs:
+		for sub_dir in path_to_dir.walkdirs():
 			if '.svn' in sub_dir:
 				continue
 			if sub_dir not in result:
@@ -103,12 +118,34 @@ def add_sub_dirs(paths):
 
 def positive_test_extensions():
 	"""Positive extensions for python files and test files"""
-	return ['.py', '.tests', '.test']
+	return ['.tests', '.test', '.py']
 
 
 def possible_test_extensions():
 	"""All possible extensions for python files and test files"""
-	return [''] + positive_test_extensions()
+	return positive_test_extensions() + ['']
+
+
+def is_test_extension(string):
+	"""Whether the string is a possible test extension"""
+	return string in possible_test_extensions()
+
+
+def is_positive_test_extension(string):
+	"""Whether the string is a possible test extension"""
+	return string in positive_test_extensions()
+
+
+def has_test_extension(string):
+	"""Whether the string has a possible test extension"""
+	_, ext = os.path.splitext(string)
+	return is_test_extension(ext)
+
+
+def has_positive_test_extension(string):
+	"""Whether the string has a possible test extension"""
+	_, ext = os.path.splitext(string)
+	return is_positive_test_extension(ext)
 
 
 def possible_test_globs():
@@ -125,8 +162,8 @@ def _get_scripts_here(recursive):
 		method = here.files
 	result = []
 	for glob in possible_test_globs():
-		for f in method(glob):
-			result.append(f)
+		for path_to_file in method(glob):
+			result.append(path_to_file)
 	return result
 
 
@@ -137,24 +174,24 @@ def _expand_stems(path_stems):
 		if path_stem.isfile():
 			result.append(path_stem)
 		else:
-			result.extend(existing_test_extensions(path_stem))
+			result.extend(_existing_test_extensions(path_stem))
 	if not result:
 		path_stem = path_stems[0]
 		raise UserMessage('%s.test*, %s.py not found' % (path_stem, path_stem))
 	return result
 
 
-def _get_paths_to_test_scripts(args, recursive):
+def __get_path_stems_to_test_scripts(args, recursive):
 	"""Get a list of test scripts from the given args
 
 	Only known extensions are included
 	If recursive is true then include sub_directories
 	"""
-	path_stems = get_paths(args, recursive)
-	if not path_stems:
-		paths_to_test_scripts = _get_scripts_here(recursive)
-	else:
+	path_stems = _get_path_stems(args, recursive)
+	if path_stems:
 		paths_to_test_scripts = _expand_stems(path_stems)
+	else:
+		paths_to_test_scripts = _get_scripts_here(recursive)
 
 	if not paths_to_test_scripts:
 		path_to_test_directory = _get_path_to_test_directory(args)
@@ -162,20 +199,46 @@ def _get_paths_to_test_scripts(args, recursive):
 	return paths_to_test_scripts
 
 
+def _text_has_python_shebang(path_to_script):
+	"""Whether the first line of that script looks like
+
+	#! ...python...
+	"""
+	lines = path_to_script.lines()
+	if not lines:
+		return False
+	first_line = lines[0]
+	return re.match('#!.*python.*', first_line)
+
+
+def _re_order_scripts(paths_to_scripts):
+	"""Re order the list of scripts so that their extensions are in order
+
+	The order to be used is same as the extensions of possible_test_extensions()
+		which goes from more testy to more doccy
+	"""
+	result = []
+	for extension in possible_test_extensions():
+		result.extend([path_to_script for path_to_script in paths_to_scripts if path_to_script.ext == extension])
+	return result
+
+
 def get_test_scripts(args, recursive):
 	"""Get all test files for the given args
 
-	args should be a list of paths - possible empty
-	If recursove is True then include sub_directories
+	args should be a list of paths - possibly empty
+	If recursive is True then include sub_directories
 	"""
-	paths_to_test_scripts = _get_paths_to_test_scripts(args, recursive)
-	result = [p for p in paths_to_test_scripts if p.ext in positive_test_extensions()]
-	for script in paths_to_test_scripts:
-		if script.ext == '':
-			lines = script.lines()
-			if not lines:
-				continue
-			if re.match('#!.*python.*', lines[0]):
-				if script not in result and str('%s.py' % script) not in result:
-					result.append(script)
-	return result
+	paths_to_test_scripts = __get_path_stems_to_test_scripts(args, recursive)
+	paths_to_positive_scripts = [p for p in paths_to_test_scripts if p.ext in positive_test_extensions()]
+	for path_to_script in paths_to_test_scripts:
+		if path_to_script.ext:
+			continue
+		if not _text_has_python_shebang(path_to_script):
+			continue
+		if path_to_script in paths_to_positive_scripts:
+			continue
+		if str('%s.py' % path_to_script) in paths_to_positive_scripts:
+			continue
+		paths_to_positive_scripts.append(path_to_script)
+	return _re_order_scripts(paths_to_positive_scripts)
