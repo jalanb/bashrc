3468568485bd347fa4f743501265b813fd24d465
Alan Brogan <github@al-got-rhythm.net>
2012-10-28 23:39:53 +0000

    use latest see.py from github
    git-svn-id: file:///usr/local/svn/jabs@874 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/testing/see.py
+++ python/testing/see.py
@@ -7,55 +7,52 @@ A human alternative to dir().
 	>>> from see import see
 	>>> help(see)
 	Help on function see in module see:
-	<BLANKLINE>
-	see(obj=anything, pattern=None, r=None, methods=None, attributes=None)
-		Inspect an object. Like the dir() builtin, but easier on the eyes.
-	<BLANKLINE>
-		Keyword arguments (all optional):
-		obj -- object to be inspected
-		pattern -- shell-style search pattern (e.g. '*len*')
-		r -- regular expression
-	<BLANKLINE>
-		If obj is omitted, objects in the current scope are listed instead.
-	<BLANKLINE>
-		Some unique symbols are used:
-	<BLANKLINE>
-			.*	  implements obj.anything
-			[]	  implements obj[key]
-			in	  implements membership tests (e.g. x in obj)
-			+obj	unary positive operator (e.g. +2)
-			-obj	unary negative operator (e.g. -2)
-	<BLANKLINE>
+	...
 
-Copyright (c) 2009 Liam Cooke
+Copyright (c) 2009-2010 Liam Cooke
 http://inky.github.com/see/
 
-Licensed under the GNU General Public License v3. {{{
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-}}}
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. The name of the author may not be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY LIAM COOKE "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
 """
 
+# pylint: disable-msg=W0141, R0912
+
+
 import re
+import os
 import sys
 import textwrap
 import linecache
 import fnmatch
 import inspect
 
+
 from pprint import pformat
 
-__all__ = ['see'] #,'see_attributes', 'see_methods', 'code']
+__all__ = ['see']
 
 __author__ = 'Liam Cooke'
 __contributors__ = [
@@ -66,10 +63,12 @@ __contributors__ = [
 	'Ed Page',
 	'guff',
 	'jdunck',
+	'Steve Losh',
+	'Adam Lloyd',
 ]
-__version__ = '0.5.4'
-__copyright__ = 'Copyright (c) 2009 Liam Cooke'
-__license__ = 'GNU General Public License v3'
+__version__ = '1.0.1'
+__copyright__ = 'Copyright (c) 2009-2010 Liam Cooke'
+__license__ = 'BSD License'
 
 
 def regex_filter(names, pat):
@@ -87,16 +86,36 @@ def fn_filter(names, pat):
 	return tuple(filter(match, names))
 
 
+class SeeError(Exception): pass
+
+
 class _SeeOutput(tuple):
 	"""Tuple-like object with a pretty string representation."""
 
-	def __new__(self, actions=None):
-		return tuple.__new__(self, actions or [])
+	def __new__(cls, actions=None):
+		return tuple.__new__(cls, actions or [])
 
 	def __repr__(self):
-		return textwrap.fill('	'.join(self), 78,
-							 initial_indent='	',
-							 subsequent_indent='	')
+		lens = sorted(map(len, self)) or [0]
+		most = lens[int(len(lens)*0.9)]
+		if lens[-1] + 6 > most:
+			max_len = most
+		else:
+			max_len = lens[-1]
+
+		def justify(i):
+			if len(i) <= max_len + 2:
+				return i.ljust(max_len + 4)
+			else:
+				return i.ljust(max_len * 2 + 8)
+
+		padded = [justify(i) for i in self]
+		if 'ps1' in dir(sys):
+			indent = ' ' * len(sys.ps1)
+		else:
+			indent = '	'
+		columns = os.environ.get('COLUMNS', 78)
+		return textwrap.fill(''.join(padded), columns, initial_indent=indent, subsequent_indent=indent)
 
 
 class _SeeDefault(object):
@@ -124,15 +143,24 @@ def see(obj=_LOCALS, pattern=None, r=None, methods=None, attributes=None):
 		in	  implements membership tests (e.g. x in obj)
 		+obj	unary positive operator (e.g. +2)
 		-obj	unary negative operator (e.g. -2)
+		?	   raised an exception
 
 	"""
 	use_locals = obj is _LOCALS
 	actions = []
 	dot = not use_locals and '.' or ''
-	func = lambda f: hasattr(f, '__call__') and '()' or ''
-	name = lambda a, f: ''.join((dot, a, func(f)))
+	name = lambda a, f: ''.join((dot, a, suffix(f)))
 	if methods is None and attributes is None:
 		methods = attributes = True
+
+	def suffix(f):
+		if isinstance(f, SeeError):
+			return '?'
+		elif hasattr(f, '__call__'):
+			return '()'
+		else:
+			return ''
+
 	if use_locals:
 		obj.__dict__ = inspect.currentframe().f_back.f_locals
 	attrs = dir(obj)
@@ -146,9 +174,16 @@ def see(obj=_LOCALS, pattern=None, r=None, methods=None, attributes=None):
 			actions.append(symbol)
 
 	for attr in filter(lambda a: not a.startswith('_'), attrs):
-		prop = getattr(obj, attr, None)
-		if not methods and callable(prop): continue
-		if not attributes and not callable(prop): continue
+		try:
+			prop = getattr(obj, attr)
+			if callable(prop):
+				if not methods:
+					continue
+			else:
+				if not attributes:
+					continue
+		except (AttributeError, Exception):
+			prop = SeeError()
 		actions.append(name(attr, prop))
 
 	if pattern is not None:
@@ -158,87 +193,98 @@ def see(obj=_LOCALS, pattern=None, r=None, methods=None, attributes=None):
 
 	return _SeeOutput(actions)
 
-def see_methods(*args,**kwargs):
-	kwargs['methods'] = True
-	return see(*args,**kwargs)
 
-def see_attributes(*args,**kwargs):
+def see_methods(*args, **kwargs):
+	kwargs['methods'] = True
+	return see(*args, **kwargs)
+
+
+def see_attributes(*args, **kwargs):
 	kwargs['attributes'] = True
-	return see(*args,**kwargs)
+	return see(*args, **kwargs)
+
 
 def spread(thing, exclude = None):
 	'''Spread out the attributes of thing onto stdout
 
 	exclude is a list of regular expressions
 		attributes matching any if these will not be shown
-		if the default of None is used it is set to [ '__.*__' ]
+		if the default of None is used it is set to ['__.*__']
 	'''
 	ids = []
 	if not exclude:
-		exclude = [ '__.*__' ]
-	exclusions = [ re.compile(e) for e in exclude ]
+		exclude = ['__.*__']
+	exclusions = [re.compile(e) for e in exclude]
 
-	def spread_out_an_attribute(v,separator):
+	def spread_out_an_attribute(v, separator):
 		if not v:
 			return repr(v)
 		if id(v) in ids:
 			return str(v)
-		return spread_out_the_attributes(v,separator)
+		return spread_out_the_attributes(v, separator)
 
-	def spread_out_the_attributes(thing,separator):
-		if not thing or not hasattr(thing,'__dict__'):
+	def spread_out_the_attributes(thing, separator):
+		if not thing or not hasattr(thing, '__dict__'):
 			return pformat(thing)
 		ids.append(id(thing))
 		attributes_list = []
-		for k,v in thing.__dict__.iteritems():
-			if isinstance(v,type(sys)): continue
-			if callable(v): continue
+		for k, v in thing.__dict__.iteritems():
+			if isinstance(v, type(sys)):
+				continue
+			if callable(v):
+				continue
 			excluded = False
 			for exclusion in exclusions:
 				if exclusion.search(k):
 					excluded = True
 					break
-			if excluded: continue
-			if hasattr(v,'__repr__'):
+			if excluded:
+				continue
+			if hasattr(v, '__repr__'):
 				value = v.__repr__()
 			else:
-				value = spread_out_an_attribute(v,separator)
+				value = spread_out_an_attribute(v, separator)
 			lines = separator.join(value.splitlines())
-			attributes_list.append('%s : %s' % (k,lines))
-		attributes_string = separator.join( attributes_list )
+			attributes_list.append('%s : %s' % (k, lines))
+		attributes_string = separator.join(attributes_list)
 		ids.pop()
-		klass = hasattr(thing,'__class__') and thing.__class__.__name__ or dir(thing)
-		return '''<%s%s%s\n%s>''' % ( klass, separator, attributes_string, separator[1:-2])
+		klass = hasattr(thing, '__class__') and thing.__class__.__name__ or dir(thing)
+		return '''<%s%s%s\n%s>''' % (klass, separator, attributes_string, separator[1:-2])
 
-	print spread_out_the_attributes(thing,'\n\t')
+	print spread_out_the_attributes(thing, '\n\t')
 
-def indent(filename,line_number):
-	line = linecache.getline(filename,line_number)
-	match = re.match('(\s*)',line)
+
+def indent_line(filename, line_number):
+	line = linecache.getline(filename, line_number)
+	match = re.match('(\s*)', line)
 	return line.rstrip(), match.groups()[0]
+
 
 def code(method):
 	if not callable(method):
 		print 'Not callable: %r' % method
 		return
-	result = [ ]
-	try: first_line_number = method.func_code.co_firstlineno
+	result = []
+	try:
+		first_line_number = method.func_code.co_firstlineno
 	except AttributeError:
 		print 'No code available for', method
 		return
 	filename = re.sub('.py[co]$', '.py', method.func_code.co_filename)
-	line, first_indentation = indent(filename,first_line_number)
-	result = [ line ]
+	line, first_indentation = indent_line(filename, first_line_number)
+	result = [line]
 	line_number = first_line_number + 1
-	line, indentation = indent(filename,line_number)
+	line, indentation = indent_line(filename, line_number)
 	while indentation > first_indentation:
-		result.append( line )
+		result.append(line)
 		line_number += 1
-		line, indentation = indent(filename,line_number)
+		line, indentation = indent_line(filename, line_number)
 	print '\n'.join(result)
+
 
 PY_300 = sys.version_info >= (3, 0)
 PY_301 = sys.version_info >= (3, 0, 1)
+
 
 SYMBOLS = tuple(filter(lambda x: x[0], (
 	# callable
