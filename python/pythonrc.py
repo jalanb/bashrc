01b4ceaee01ec7b42fd2450af0e289eb19b557b0
Alan Brogan <github@al-got-rhythm.net>
2012-10-30 11:33:16 +0000

    simplify
    git-svn-id: file:///usr/local/svn/jabs@876 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/pythonrc.py
+++ python/pythonrc.py
@@ -1,32 +1,13 @@
 """Dirty up the main namespace with some extra imports"""
 
-import os
-import sys
+
 from pprint import pprint as show
 
 
-def pir(thing):
-	"""Layout a dir listing more nicely"""
-	show(dir(thing))
-
-
-def pirr(thing, regexp):
+def see(thing, regexp=None):
 	"""layout a dir listing filtered by the given regexp"""
-	import re
-	show([item for item in dir(thing) if re.match(regexp, item)])
-
-try:
-	import environ
-except ImportError:
-	pass
-
-try:
-	import paths
-except ImportError:
-	pass
-
-try:
-	from see import see, code, see_methods, see_attributes, spread
-except ImportError:
-	def see(thing):
+	if regexp:
+		import re
+		show([item for item in dir(thing) if re.match(regexp, item)])
+	else:
 		show(dir(thing))
