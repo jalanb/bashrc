6b8463f2a58655dc570f2499a657992a7a92fc49
Alan Brogan <github@al-got-rhythm.net>
2012-11-02 03:03:23 +0000

    better coding
    git-svn-id: file:///usr/local/svn/jabs@904 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/words.py
+++ python/site/words.py
@@ -1,16 +1,19 @@
-'''
+"""
 Some useful methods found (initially) at
 	http://www.google.com/codesearch/p?hl=en#632pFyTt4Ec/Supybot-0.83.2/src/utils/str.py&q=pluralize%20lang:python
-'''
+"""
+
+
 import re
 from itertools import ifilter
 
+
 def matchCase(s1, s2):
-	'''Matches the case of s1 in s2
-	
-	>>> matchCase('MATCHcase','somestuff')
+	"""Matches the case of s1 in s2
+
+	>>> matchCase('MATCHcase', 'somestuff')
 	'SOMEStuff'
-	'''
+	"""
 	if s1.isupper():
 		return s2.upper()
 	else:
@@ -24,11 +27,12 @@ def matchCase(s1, s2):
 
 
 def any_true(function, iterable):
-	'''Returns true if any element in iterable satisfies the function.'''
+	"""Returns true if any element in iterable satisfies the function."""
 	for _ in ifilter(function, iterable):
 		return True
 	else:
 		return False
+
 
 consonants = 'bcdfghjklmnpqrstvwxz'
 ends_in_consonant_y = re.compile('[%s]y$' % consonants)
@@ -55,12 +59,13 @@ exceptional_plurals = {
 	'woman' : 'women',
 }
 
+
 def pluralize(s):
-	'''Returns the plural of s. 
-	
+	"""Returns the plural of s.
+
 	>>> pluralize('Index')
 	'Indices'
-	'''
+	"""
 	lowered = s.lower()
 	result = s + 's'
 	if lowered in exceptional_plurals:
@@ -72,7 +77,7 @@ def pluralize(s):
 	elif any_true(lowered.endswith, ['x', 'ch', 'sh', 'ss', 'z']):
 		result = s+'es'
 	elif lowered.endswith('o'):
-		eos = [ 'echo', 'hero', 'potato', 'veto', 'tomato' ]
+		eos = ['echo', 'hero', 'potato', 'veto', 'tomato']
 		if lowered in eos:
 			result = s + 'es'
 		else:
@@ -80,35 +85,36 @@ def pluralize(s):
 	elif ends_in_consonant_y.search(lowered):
 		result = s[:-1] + 'ies'
 	else:
-		regexp, subs = re.compile(r'([^f])fe$',re.IGNORECASE), r'\1ves'
+		regexp, subs = re.compile(r'([^f])fe$', re.IGNORECASE), r'\1ves'
 		if regexp.search(s):
-			return regexp.sub(subs,s)
+			return regexp.sub(subs, s)
 	return matchCase(s, result)
+
 
 ends_in_consonant_ies = re.compile('[%s]ies' % consonants)
 exceptional_singlulars = {}
-for k,v in exceptional_plurals.iteritems(): exceptional_singlulars[v] = k
+for k, v in exceptional_plurals.iteritems(): exceptional_singlulars[v] = k
+
 
 def depluralize(s):
-	'''Returns the singular of s.
-	
+	"""Returns the singular of s.
+
 	>>> depluralize('Cows')
 	'Cow'
-	'''
+	"""
 	lowered = s.lower()
 	if lowered in exceptional_plurals:
 		return matchCase(s, exceptional_plurals[lowered])
-	elif lowered in uncountables:
+	if lowered in uncountables:
 		return s
-	elif any_true(lowered.endswith, ['ches', 'shes', 'sses']):
+	if any_true(lowered.endswith, ['ches', 'shes', 'sses']):
 		return s[:-2]
-	elif re.search(ends_in_consonant_ies, lowered):
+	if re.search(ends_in_consonant_ies, lowered):
 		return s[:-3] + 'y'
-	else:
-		if lowered.endswith('s'):
-			return s[:-1] # Chop off 's'.
-		else:
-			return s # Don't know what to do.
+	if lowered.endswith('s'):
+		return s[:-1]  # Chop off 's'.
+	raise NotImplementedError("Don't know how to depluralize %r" % s)
+
 
 def number_name(i):
 	try:
@@ -127,4 +133,4 @@ def number_name(i):
 		8: 'eight',
 		9: 'nine'
 	}
-	return number_names.get(i,str(i))
+	return number_names.get(i, str(i))
