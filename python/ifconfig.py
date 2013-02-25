3d0266f3b73a0ea1286183cfde62eb077f10e201
Alan Brogan <github@al-got-rhythm.net>
2013-02-25 12:23:08 +0000

    python script for better choice among IP addresses
    git-svn-id: file:///usr/local/svn/jabs@992 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ python/ifconfig.py
@@ -0,0 +1,42 @@
+"""A script to run ifconfig, extract IP addresses"""
+
+import re
+import sys
+import commands
+
+def run_ifconfig():
+	"""return the output from ifconfig as a list of lines"""
+	command = 'ifconfig -a'
+	_status, output = commands.getstatusoutput(command)
+	return output.splitlines()
+
+
+def get_ip_addresses(lines):
+	"""Find the IP addresses is the given lines"""
+	ip_regexp = re.compile(r"(\b(([0-9]{1,3}\.){3})([0-9]{1,3})\b)")
+	ip_addresses = [ip_regexp.search(l).group(0) for l in lines if ip_regexp.search(l)]
+	return sorted(ip_addresses)
+
+
+def make_chooser(args):
+	"""Make a method to choose one of the args, or anything if args are empty"""
+	if not args:
+		return lambda x: True
+	def choose(string):
+		for arg in args:
+			if re.search(arg, string):
+				return True
+	return choose
+
+
+def main(args):
+	"""Run the program"""
+	choose = make_chooser(args)
+	lines = run_ifconfig()
+	for ip_address in get_ip_addresses(lines):
+		if choose(ip_address):
+			print ip_address
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
