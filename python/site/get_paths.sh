dfebf1173ef471a61ac8a8fc4fbc3dd7e2ada8f0
Alan Brogan <github@al-got-rhythm.net>
2013-04-18 13:45:04 +0000

    get paths modules from alto repo
    git-svn-id: file:///usr/local/svn/jabs@1056 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ python/site/get_paths.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -x
+REPO="https://repository.altobridge.com/svn/amg/trunk/trackers/python/site-packages"
+svn cat $REPO/path.py > path.py
+svn cat $REPO/paths.py > paths.py
+svn cat $REPO/path.test > path.test
+svn cat $REPO/paths.test > paths.test
+if [[ -d $TRACKERS ]]
+then
+	python $TRACKERS/testing/try.py path.
+	python $TRACKERS/testing/try.py paths.
+fi
