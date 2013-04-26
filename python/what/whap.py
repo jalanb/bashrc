d1fa0432b91644c746d47dae9e777befcbf6d4d2
Alan Brogan <github@al-got-rhythm.net>
2013-04-26 15:45:56 +0000

    check for builtins, modularise and test the code
    git-svn-id: file:///usr/local/svn/jabs@1077 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/what/whap.py
+++ python/what/whap.py
@@ -1,8 +1,11 @@
 """This small script offers a python equivalent to "which"
 
-For every argument on the command line it displays
+For every argument on the command line it lists
 	all python files and all directories with that name
 	in each of the directories in sys.path
+
+If a name is found more than once each is listed
+	(but python will only import one, probably the first listed)
 """
 
 _copyright = """
@@ -14,19 +17,79 @@ _copyright = """
 
 import os
 import sys
+import fnmatch
 
-def main():
-	for arg in sys.argv[1:]:
+
+def directory_list(path):
+	"""A list of all items in that path
+
+	If path is not a directory, an empty list
+	"""
+	if not os.path.isdir(path):
+		return []
+	return os.listdir(path)
+
+
+def is_file_in(path, name):
+	"""Whether that name is a file in the directory at that path"""
+	path_to_name = os.path.join(path, name)
+	return os.path.isfile(path_to_name)
+
+
+def is_matching_file_in(path, name, glob):
+	"""Whether that name is a file in the directory at that path and matches that glob"""
+	return is_file_in(path, name) and fnmatch.fnmatch(name, glob)
+
+
+def path_to_module(path, name):
+	"""Whether the name matches a python source or compiled file in the given path
+
+	If source and compiled files are found, give the source
+	"""
+	glob = '%s.py*' % name
+	python_files = [f for f in directory_list(path) if is_matching_file_in(path, f, glob)]
+	if not python_files:
+		return None
+	source_files = [f for f in python_files if os.path.splitext(f)[-1] == '.py']
+	python_file = source_files and source_files[0] or python_files[0]
+	return os.path.join(path, python_file)
+
+
+def path_to_sub_directory(path, name):
+	"""If name is a real sub-directory of path, return that"""
+	result = os.path.join(path, name)
+	if os.path.isdir(result):
+		return result
+
+
+def path_to_python(path, name):
+	"""Path to either a module or sub-dir of the given path, withthe given name"""
+	result = path_to_sub_directory(path, name)
+	if result:
+		return result
+	return path_to_module(path, name)
+
+
+def built_in(name):
+	"""Whether the name is that of one of python's builtin modules"""
+	try:
+		#  Not all builtin modules are initially imported, so bring it in first
+		__import__(name)
+	except ImportError:
+		return False
+	return '(built-in)' in str(sys.modules[name])
+
+
+def main(args):
+	"""Run the program"""
+	for arg in args:
+		if built_in(arg):
+			print 'builtin', arg
 		for path in sys.path:
-			path_to_item = os.path.join(path, arg)
-			if os.path.isdir(path_to_item):
-				os.system('ls -ld %s' % path_to_item)
-			else:
-				path_to_python = os.path.join(path, '%s.py' % arg)
-				if os.path.isfile(path_to_python):
-					os.system('ls -l %s' % path_to_python)
-	return 0
+			path_to_arg = path_to_python(path, arg)
+			if path_to_arg:
+				os.system('ls -ld %s' % path_to_arg)
 
 
 if __name__ == '__main__':
-	sys.exit(main())
+	main(sys.argv[1:])
