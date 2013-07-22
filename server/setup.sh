329c36b82a96302034903086ecfa5df4fd148605
Alan Brogan <github@al-got-rhythm.net>
2013-07-22 09:44:20 +0000

    directories have moved
    git-svn-id: file:///usr/local/svn/jabs@1171 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- server/setup.sh
+++ server/setup.sh
@@ -7,10 +7,10 @@ as_root ()
 {
 	cd linux
 	sudo bash make_software.sh
-	local distro=$(~/bin/python2.7 -c "import platform; print platform.linux_distribution()[0].lower()")
+	local distro=$(python2.7 -c "import platform; print platform.linux_distribution()[0].lower()")
 	if [[ -d $distro ]]
 	then
-		for file in $distro/*.sh
+		for file in linux/$distro/*.sh
 		do
 			cat $file
 		done
