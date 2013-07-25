bbaab72a50b600501a1a0f6f1b023e001deadee2
Alan Brogan <github@al-got-rhythm.net>
2013-07-25 09:46:18 +0000

    add pandemic to add vim bundles
    git-svn-id: file:///usr/local/svn/jabs@1179 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ server/linux/make_pandemic.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+get_pandemic ()
+{
+	wget --no-check-certificate https://github.com/jwcxz/vim-pandemic/archive/master.zip
+	unzip vim-pandemic-master.zip
+	rm -rf vim-pandemic-master.zip
+}
+
+make_pandemic ()
+{
+	cd vim-pandemic-master
+	sed -i -e "s|vim/bundle.remote|vim/bundle|" bin/pandemic
+	python2.7 setup.py install --prefix=$HOME
+	if [[ ! -f ~/bin/pamdemic ]]
+	then
+		echo Installation of pandemic failed >&2
+	fi
+}
+
+update_pandemic ()
+{
+	pandemic list | sed -e "s/:.*//" | xargs env GIT_SSL_NO_VERIFY=true pandemic update
+}
+
+clean_up ()
+{
+	cd $START_HERE
+	test -d vim-pandemic-master && rm -rf vim-pandemic-master
+	test -f vim-pandemic-master.zip && rm -f vim-pandemic-master.zip
+}
+
+main ()
+{
+	START_HERE=$PWD
+	get_pandemic
+	make_pandemic
+	update_pandemic
+}
+
+trap cleanup EXIT
+main $*
