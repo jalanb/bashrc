0572ff0688f2045c4679889ab82bc38f409cd612
Alan Brogan <github@al-got-rhythm.net>
2013-08-08 15:22:38 +0000

    Dirty up the main namespace with some extra imports
    git-svn-id: file:///usr/local/svn/jabs@1187 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/pythonrc.py
+++ python/pythonrc.py
@@ -1,13 +1,35 @@
 """Dirty up the main namespace with some extra imports"""
 
 
-from pprint import pprint as show
 
 
 def see(thing, regexp=None):
 	"""layout a dir listing filtered by the given regexp"""
+	from pprint import pprint as show
 	if regexp:
 		import re
 		show([item for item in dir(thing) if re.match(regexp, item)])
 	else:
 		show(dir(thing))
+
+
+def read_history(readline):
+	import os
+	import atexist
+	history_file = os.path.expanduser('.pythonhistory')
+	if os.path.isfile(history_file):
+		readline.read_history_file(history_file)
+	atexit.register(readline.write_history_file, history_file)
+
+
+def complete():
+	try:
+		import rlcompleter
+		import readline
+		readline.parse_and_bind('tab: complete')
+		read_history(readline)
+	except ImportError, e:
+		print 'Python shell enhancement modules not available because %s' % e
+
+
+complete()
