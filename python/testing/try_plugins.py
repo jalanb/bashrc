0b920122ef619a40c4ad49f5fb2c18994374c1d1
Alan Brogan <github@al-got-rhythm.net>
2013-08-17 07:45:51 +0000

    allow for missing altobridge package
    git-svn-id: file:///usr/local/svn/jabs@1208 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/testing/try_plugins.py
+++ python/testing/try_plugins.py
@@ -1,7 +1,7 @@
 """This module holds plugins for try.py"""
 
 
-def pre_test(path_to_test_file):
+def pre_test(_path_to_test_file):
 	"""This method is called before each test has been run
 
 	If this method returns a false value then that file is not tested
@@ -9,14 +9,17 @@ def pre_test(path_to_test_file):
 	return True
 
 
-def post_test(path_to_test_file, failures, tests_run):
+def post_test(_path_to_test_file, _failures, _tests_run):
 	"""This method is called after each test has been run"""
 	check_bash()
 	return True
 
 
 def check_bash():
-	from altobridge.shell import bash
+	try:
+		from altobridge.shell import bash
+	except ImportError:
+		return
 	shows = ['commands', 'outputs', 'errors']
 	shown = [show for show in shows if bash.BashOptions.verbose.showing(show)]
 	if shown:
