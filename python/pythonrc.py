355f5bc09110a0622cdc4a1de88cf59b11ca1dd7
Alan Brogan <github@al-got-rhythm.net>
2013-09-04 17:47:10 +0000

    do not redefine see
    git-svn-id: file:///usr/local/svn/jabs@1236 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/pythonrc.py
+++ python/pythonrc.py
@@ -1,19 +1,22 @@
 """Dirty up the main namespace with some extra imports"""
 
 
-def see(thing, regexp=None):
-	"""layout a dir listing filtered by the given regexp"""
-	from pprint import pprint as show
-	if regexp:
-		import re
-		show([item for item in dir(thing) if re.match(regexp, item)])
-	else:
-		show(dir(thing))
+try:
+	see
+except NameError:
+	def see(thing, regexp=None):
+		"""layout a dir listing filtered by the given regexp"""
+		from pprint import pprint as show
+		if regexp:
+			import re
+			show([item for item in dir(thing) if re.match(regexp, item)])
+		else:
+			show(dir(thing))
 
 
 def read_history(readline):
 	import os
-	import atexist
+	import atexit
 	history_file = os.path.expanduser('.pythonhistory')
 	if os.path.isfile(history_file):
 		readline.read_history_file(history_file)
@@ -22,7 +25,6 @@ def read_history(readline):
 
 def complete():
 	try:
-		import rlcompleter
 		import readline
 		readline.parse_and_bind('tab: complete')
 		read_history(readline)
