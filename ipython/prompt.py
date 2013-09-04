48726ebe2a2f6302af8e61cbe338329496bd3373
Alan Brogan <github@al-got-rhythm.net>
2013-09-04 16:06:22 +0000

    prompt module for ipython
    git-svn-id: file:///usr/local/svn/jabs@1227 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ ipython/prompt.py
@@ -0,0 +1,25 @@
+"""Module to provide a prompt I like for ipython"""
+
+import os
+import time
+
+from IPython.utils import coloransi
+from IPython.core.prompts import LazyEvaluate as promptify
+
+@promptify
+def prompt():
+	color = coloransi.TermColors()
+	return '%s0 %s[%s%s %s@%s:%s%s]%s%s' % (
+		color.White,
+		color.LightRed,
+		color.LightCyan,
+		time.strftime("%A %Y-%m-%d.%H:%M:%S"),
+		os.environ.get('USER','nobody'),
+		os.uname()[1],
+		os.path.basename(os.getcwd()),
+		color.LightRed,
+		color.Normal,
+		color.Green,
+	)
+
+my_prompt = prompt()
