76b724b0ef683fdd38a13f9788b2bd35581ad3bc
Alan Brogan <github@al-got-rhythm.net>
2013-09-04 17:06:18 +0000

    newer ipython config system
    git-svn-id: file:///usr/local/svn/jabs@1229 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ ipython/profile_default/startup/00-imports.py
@@ -0,0 +1,18 @@
+"""Imports for IPython"""
+
+# pylint: disable-msg=W0611
+
+import os
+import re
+import sys
+
+from path import path
+from paths import makepath
+from see import see
+from see import see_attributes
+from see import see_methods
+from see import spread
+from see_code import code
+from see_code import highlight
+from see_code import see_code
+from see_code import see_highlight
