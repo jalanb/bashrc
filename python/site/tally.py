59f03dd34394af2d995c59d941403957b5258c35
Alan Brogan <github@al-got-rhythm.net>
2013-10-17 13:56:09 +0000

    pep 8
    git-svn-id: file:///usr/local/svn/jabs@1310 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- python/site/tally.py
+++ python/site/tally.py
@@ -1,60 +1,66 @@
 from spine import Spine
 from words import pluralize
 
-class Tally(Spine,list):
-	'''Tally is a kind of list.
+
+class Tally(Spine, list):
+	"""Tally is a kind of list.
 
 	This tally is intended to be sub-classed,
 		and provides conveniences to represent the class name nicely
 
-	'''
+	"""
 	def __str__(self):
-		'''Represent the list as a string
+		"""Represent the list as a string
 
-		A list of chars 
+		A list of chars
 		>>> str(Tally('abc'))
 		'abc'
 
 		A list of non-chars
-		>>> str(Tally([1,2,3]))
+		>>> str(Tally([1, 2, 3]))
 		'[1, 2, 3]'
-		'''
-		try: return self.as_str()
-		except TypeError: return self.to_str()
+		"""
+		try:
+			return self.as_str()
+		except TypeError:
+			return self.to_str()
 
 	def __repr__(self):
-		'''Represent the list as a type
+		"""Represent the list as a type
 
-		A list of chars 
+		A list of chars
 		>>> Tally('abc')
 		<Tally 'abc'>
 
 		A list of non-chars
-		>>> Tally([1,2,3])
+		>>> Tally([1, 2, 3])
 		<Tally [1, 2, 3]>
-		'''
-		try: return '<%s %r>' % (self.class_name(),self.as_str())
-		except TypeError: return '<%s %s>' % (self.class_name(),self.to_str())
+		"""
+		try:
+			return '<%s %r>' % (self.class_name(), self.as_str())
+		except TypeError:
+			return '<%s %s>' % (self.class_name(), self.to_str())
 
-	def as_str(self,separator=None):
-		'''Use the separator to join the chars into a string
+	def as_str(self, separator=None):
+		"""Use the separator to join the chars into a string
 
 		>>> Tally('123').as_str('|')
 		'1|2|3'
-		'''
-		if separator is None: separator = ''
+		"""
+		if separator is None:
+			separator = ''
 		return separator.join(self)
 
-	def to_str(self,i=None,j=None,separator=None):
-		'''Turn the list into a string
+	def to_str(self, i=None, j=None, separator=None):
+		"""Turn the list into a string
 
 		i and j are indices to take only part of the list
 
-		>>> Tally([0,1,2,3,4,5]).to_str()
+		>>> Tally([0, 1, 2, 3, 4, 5]).to_str()
 		'[0, 1, 2, 3, 4, 5]'
-		>>> Tally([0,1,2,3,4,5]).to_str(2,5,'|')
+		>>> Tally([0, 1, 2, 3, 4, 5]).to_str(2, 5, '|')
 		'[2|3|4]'
-		'''
+		"""
 		if i is None and j is None:
 			items = self
 		elif i is None:
@@ -63,17 +69,18 @@ class Tally(Spine,list):
 			items = self[i:]
 		else:
 			items = self[i:j]
-		if separator is None: separator = ', '
+		if separator is None:
+			separator = ', '
 		return '[%s]' % separator.join([repr(s) for s in items])
 
 	def list_name(self):
-		'''The plural of the class name
-		
+		"""The plural of the class name
+
 		Intended as the name to use for a list of these objects
 
 		>>> print Tally().list_name()
 		Tallies
-		'''
+		"""
 		try:
 			list_of_items = self[0].list_name()
 		except AttributeError:
@@ -82,29 +89,33 @@ class Tally(Spine,list):
 			list_of_items = ''
 		if self.class_name() == list_of_items:
 			return self.class_name()
-		return pluralize( self.class_name() )
+		return pluralize(self.class_name())
 
 	def str_class_and_list(self):
-		'''A tuple with three strings for the value, class and list names
+		"""A tuple with three strings for the value, class and list names
 
 		>>> print Tally().str_class_and_list()
 		('', 'Tally', 'Tallies')
-		'''
+		"""
 		return str(self), self.class_name(), self.list_name()
 
-def tlist(name,super_classes=None,name_space=None):
-	'''Create a sub-class of Tally with the given name
+
+def tlist(name, super_classes=None, name_space=None):
+	"""Create a sub-class of Tally with the given name
 
 	Super classes and a namespace can also be supplied
 
 	>>> tlist('world')('Hello').str_class_and_list()
 	('Hello', 'world', 'worlds')
-	'''
-	if super_classes is None: super_classes = []
-	if name_space is None: name_space = {}
+	"""
+	if super_classes is None:
+		super_classes = []
+	if name_space is None:
+		name_space = {}
 	class MySecondMeta(type):
-		def __init__(cls, name, super_classes, name_space):
-			cls.lets_tlist_again_like_we_did_last_summer = True
-	escaped = name.replace(' ','_')
-	return MySecondMeta(escaped, (Tally,), name_space)
+		def __init__(mcs, _name, _super_classes, _name_space):
+			# pylint: disable-msg=W0231
+			mcs.lets_tlist_again_like_we_did_last_summer = True
+	escaped = name.replace(' ', '_')
+	return MySecondMeta(escaped, (Tally, ), name_space)
 
--- python/site/twine.py
+++ python/site/twine.py
@@ -1,35 +1,41 @@
 from spine import Spine
 
-class Twine(Spine,str):
-	'''Twine is a kind of string.
+
+class Twine(Spine, str):
+	"""Twine is a kind of string.
 
 	This twine is intended to be sub-classed,
 		and provides conveniences to represent the class name nicely
 
 	BTW:
-	Inheritance from str seems OK, given 
+	Inheritance from str seems OK, given
 		"Remember that the Path module have existed for three years in the wild and is
 		extra-ordinarily well-liked. If the fact that Path inherits from
 		string is such a big deal, then *why* is the path module so hugely
 		popular?? :)"
 	at http://mail.python.org/pipermail/python-dev/2006-January/060115.html
-	'''
+	"""
 
 	def str_class_and_list(self):
-		'''A tuple with three strings for the value, class and list names
+		"""A tuple with three strings for the value, class and list names
 
 		>>> print Twine().str_class_and_list()
 		('', 'Twine', 'Twines')
-		'''
+		"""
 		return str(self), self.class_name(), self.list_name()
 
-def twist(name,bases=None,ns=None):
-	if bases is None: bases = []
-	if ns is None: ns = {}
+
+def twist(name, bases=None, ns=None):
+	if bases is None:
+		bases = []
+	if ns is None:
+		ns = {}
 	class MyFirstMeta(type):
-		def __init__(cls, name, bases, ns):
-			cls.lets_twist_again_like_we_did_last_summer = True
-	if not ns: ns = {}
-	escaped = name.replace(' ','_')
-	return MyFirstMeta(escaped, (Twine,), ns)
+		def __init__(mcs, _name, _bases, _namespace):
+			# # pylint: disable-msg=W0231
+			mcs.lets_twist_again_like_we_did_last_summer = True
+	if not ns:
+		ns = {}
+	escaped = name.replace(' ', '_')
+	return MyFirstMeta(escaped, (Twine, ), ns)
 
