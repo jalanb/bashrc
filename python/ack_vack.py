6b5a34361d0a37f7cf9e9a5244ca3c3282b75407
Alan Brogan <github@al-got-rhythm.net>
2013-10-31 11:44:14 +0000

    move arg handling into python
    git-svn-id: file:///usr/local/svn/jabs@1330 f6a8b572-8bf8-43d5-8295-329fc01c5294

---
+++ python/ack_vack.py
@@ -0,0 +1,25 @@
+"""Script to change -v for an ack command to vack"""
+
+import re
+import sys
+
+def main(args):
+	words = ['ack']
+	for word in args:
+		if re.match('-[a-uw-z]*v[a-uw-z]*', word):
+			words[0] = 'vack'
+			word = re.sub('v', '', word)
+			if word != '-':
+				words.append(word)
+		else:
+			if ' ' in word:
+				if ' $' in word:
+					words.append("'%s'" % word)
+				else:
+					words.append('"%s"' % word)
+			else:
+				words.append(word)
+	print ' '.join(words)
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
