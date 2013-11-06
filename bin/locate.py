996231bffe1417f265f01f13930c8e9640d9d1cd
Alan Brogan <github@al-got-rhythm.net>
2013-11-06 13:02:52 +0000

    pass a -i option on to the locate command
    git-svn-id: file:///usr/local/svn/jabs@1338 f6a8b572-8bf8-43d5-8295-329fc01c5294

--- bin/locate.py
+++ bin/locate.py
@@ -8,17 +8,21 @@ import optparse
 
 def path_to_locate():
 	"""Location of the locate command on most unixen"""
+	homebrewed_locate = '/usr/local/Cellar/findutils/4.4.2/bin/locate'
+	if os.path.isfile(homebrewed_locate):
+		return homebrewed_locate
 	return '/usr/bin/locate'
 
 
-def make_locate_command(string):
+def make_locate_command(string, options):
 	"""Make a command to locate that string"""
-	return '%s %s' % (path_to_locate(), string)
+	option = options.ignore_case and '-i' or ''
+	return '%s %s "%s"' % (path_to_locate(), option, string)
 
 
-def run_locate(string):
+def run_locate(string, options):
 	"""Run the locate command on the given string"""
-	command = make_locate_command(string)
+	command = make_locate_command(string, options)
 	status, output = commands.getstatusoutput(command)
 	if status and output:
 		raise ValueError('command: %s\n output: %s' % (command, output))
@@ -42,14 +46,18 @@ def make_check(method, options):
 
 def locate(string, options):
 	"""Locate some files called string, restricted by the given options"""
-	lines = run_locate(string)
+	lines = run_locate(string, options)
 	def has_basename(path):
+		if options.ignore_case:
+			return os.path.basename(path).upper() == string.upper()
 		return os.path.basename(path) == string
 	check = make_check(has_basename, options)
 	result = [l for l in lines if check(l)]
 	if result:
 		return result
 	def directory_in_path(path):
+		if options.ignore_case:
+			return string.upper() in path.upper().split(os.path.sep)
 		return string in path.split(os.path.sep)
 	check = make_check(directory_in_path, options)
 	result = [l for l in lines if check(l)]
@@ -65,6 +73,7 @@ def handle_command_line():
 	parser = optparse.OptionParser()
 	parser.add_option('-d', '--directories', action='store_true', help='only locate directories')
 	parser.add_option('-f', '--files', action='store_true', help='only locate files')
+	parser.add_option('-i', '--ignore-case', action='store_true', help='ignore case in searches')
 	parser.add_option('-U', '--Use_debugger', action='store_true', help='debug with pudb')
 	options, args = parser.parse_args()
 	if options.Use_debugger:
