09c0d3d049010e2569d403bd4280f4cb19108db3
Alan Brogan <github@al-got-rhythm.net>
2014-01-29 10:39:24 +0000

    Keep logs of python installation

--- server/linux/make_python.sh
+++ server/linux/make_python.sh
@@ -6,16 +6,16 @@ cd /tmp/Downloads
 if python -c "import sys; sys.exit(int(sys.version.split()[0] >= '2.7.4'))"
 then
 	wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
-	tar zxf Python-2.7.5.tgz 
-	cd Python-2.7.5
-	./configure --prefix=$HOME
+	tar zxf Python-2.7.6.tgz 
+	cd Python-2.7.6
+	./configure --prefix=$HOME 2>&1 > configuring.log
 	DISTRO=$(lsb_release -a 2>/dev/null | grep "Distributor ID" | cut -d\: -f 2 | tr -d ' \t')
 	if [[ $DISTRO == "Ubuntu" ]]
 	then
 		sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup
 	fi
-	make || exit
-	make install || exit
+	make 2>&1 > making.log || exit
+	make install 2>&1 > installing.log || exit
 	cd ..
 	wget  --no-check-certificate https://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg || exit
 	sh setuptools-0.6c11-py2.7.egg --prefix=$HOME || exit
