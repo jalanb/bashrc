8432088afc974107aa48352aa7e44d570b3fcfc8
Alan Brogan <github@al-got-rhythm.net>
2014-02-10 11:10:07 +0000

    PEP 8

--- src/python/y.py
+++ src/python/y.py
@@ -1,23 +1,26 @@
-'''y not?'''
+"""y not?"""
+
 
 import os
 import argv
-import paths
+
 
 argv.add_options([
     ('delete', 'delete python compiled files as well', False),
-    ('wipe', 'remove known garbage',False),
+    ('wipe', 'remove known garbage', False),
     ('stat', 'run svn stat', False),
-    ('ptags', 'do not refresh the tags file', True),
+    ('tags', 'refresh the tags file', True),
     ('verbose', 'run ptags verbosely', False),
 ])
+
 
 from ls import ly
 from repositories import svn
 
+
 def remove_globs(globs):
     for glob in globs:
-        #print glob, [ f for f in argv.first_directory.files(glob) ]
+        #print glob, [f for f in argv.first_directory.files(glob)]
         for p in argv.first_directory.listdir(glob):
             if p.islink():
                 p.unlink()
@@ -26,19 +29,24 @@ def remove_globs(globs):
             elif p.isdir():
                 p.rmdir()
             else:
-                raise ValueError( 'Do not know how to remove %s' % p )
+                raise ValueError('Do not know how to remove %s' % p)
+
 
 def wipe():
-    remove_globs([ '*~', '.*~', '*.orig', 'fred*', 'mary', '*.tmp', '*.bak', 'one', 'two' ])
-    [ f.rm() for f in argv.first_directory.files('*.fail') if not f.size ]
+    remove_globs([
+        '*~', '.*~', '*.orig', 'fred*', 'mary',
+        '*.tmp', '*.bak', 'one', 'two'])
+    _ = [f.rm() for f in argv.first_directory.files('*.fail') if not f.size]
+
 
 def delete():
-    remove_globs([ '*.pyc', '*.pyo' ] )
+    remove_globs(['*.pyc', '*.pyo'])
 
 
-def ptags():
+def tags():
     import ptags
     ptags.read_write_dir(argv.first_directory)
+
 
 def main():
     if argv.first_directory != os.getcwd():
@@ -48,7 +56,8 @@ def main():
         method()
     ly.show()
     if argv.options.stat:
-        svn.show_stat()
+        svn.show_stat(argv.first_directory)
+
 
 if __name__ == '__main__':
     ly.prepare_argv()
