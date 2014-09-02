3b503226ca8b44c82f528decdb723a923e353292
J Alan Brogan <github@al-got-rhythm.net>
2014-09-02 10:45:05 +0100

    Start with Zen

--- ipython/profile_default/startup/00-imports.py
+++ ipython/profile_default/startup/00-imports.py
@@ -2,30 +2,24 @@
 
 # pylint: disable-msg=W0611
 
+import this
+print
+
 import os
-print 'import os'
 import re
-print 'import re'
 import sys
-print 'import sys'
+print 'import os, re, sys'
 
 from path import path
-print 'from path import path'
 from dotsite.paths import makepath
-print 'from dotsite.paths import makepath'
+print 'from dotsite.paths import path, makepath'
 from see import see
-print 'from see import see'
 from see import see_attributes
-print 'from see import see_attributes'
 from see import see_methods
-print 'from see import see_methods'
 from see import spread
-print 'from see import spread'
+print 'from see import see, see_attributes, see_methods, spread'
 from see_code import code
-print 'from see_code import code'
 from see_code import highlight
-print 'from see_code import highlight'
 from see_code import see_code
-print 'from see_code import see_code'
 from see_code import see_highlight
-print 'from see_code import see_highlight'
+print 'from see_code import code, highlight, see_code, see_highlight'
