792d05db910556c246b852d2b62a2fc1a11ed722
J Alan Brogan <github@al-got-rhythm.net>
2015-01-17 08:34:27 +0000

    Simple is better than complex
    although Complex is better than complicated

--- server/jab/set_ssh.sh
+++ server/jab/set_ssh.sh
@@ -1,25 +1,35 @@
 #! /bin/bash
 
-JAB=${JAB:-$HOME/.jab}
-JAB_SSH=$JAB/ssh
-JAB_STEM=$JAB_SSH/id_jab
-JAB_KEY=${JAB_STEM}.pub
-LOCAL_SSH=~/.ssh
-LOCAL_STEM=$LOCAL_SSH/id_rsa
-KNOWN_KEYS=$LOCAL_SSH/authorized_keys
+[[ -f $JAB/jab_environ ]] && source $JAB/jab_environ || echo "Cannot find $JAB/environ" >&2
 
-test -f $LOCAL_STEM || ssh-keygen -q -t rsa -N "" -f $LOCAL_STEM
-chmod 600 $KNOWN_KEYS
-if [ -f $KNOWN_KEYS ]
-then
-	cat $JAB_KEY >> $KNOWN_KEYS
-else
-	cat $JAB_KEY > $KNOWN_KEYS
-fi
-sudo chmod 400 -R $JAB_SSH $LOCAL_SSH
-sudo chmod 700 -R $JAB_SSH/.svn
-chmod u+X $LOCAL_SSH $JAB_SSH
-chmod 400 $JAB_STEM
-chmod 400 $LOCAL_STEM
-chmod 444 $JAB_STEM.pub
-chmod 444 $LOCAL_STEM.pub
+HOME_SSH=~/.ssh
+HOME_ID=$HOME_SSH/id_rsa
+
+authorize_jab ()
+{
+    KNOWN_KEYS=$HOME_SSH/authorized_keys
+    test -f $HOME_ID || ssh-keygen -q -t rsa -N "" -f $LOCAL_STEM
+    chmod 600 $KNOWN_KEYS
+    if [ -f $KNOWN_KEYS ]
+    then
+        cat $JAB_PUB_ID >> $KNOWN_KEYS
+    else
+        cat $JAB_PUB_ID > $KNOWN_KEYS
+    fi
+}
+
+change_modes ()
+{
+    sudo chmod 400 -R $JAB_SSH $HOME_SSH
+    sudo chmod 700 -R $JAB_SSH/.svn
+    chmod u+X $HOME_SSH $JAB_SSH
+
+    chmod 400 $JAB_ID
+    chmod 444 $JAB_PUB_ID
+
+    chmod 400 $HOME_ID
+    chmod 444 $HOME_ID.pub
+}
+
+change_modes
+authorize_jab
