ad3fd3c52e14e61196e141af8ec98f7cf460ea4e
J Alan Brogan <github@al-got-rhythm.net>
2015-05-11 10:22:05 +0100

    Prefer PEP 8 formatting

--- src/python/site/sis.py
+++ src/python/site/sis.py
@@ -1,32 +1,41 @@
-'''A companion module to sys'''
+"""A companion module to sys"""
 import sys
-from dotsite.path import path as Path
+from dotsite.paths import path as Path
 
-paths = None
+
+paths = []
+
 
 def refresh_paths():
-    '''Refresh the module-level paths'''
-    global paths
-    paths = [ Path(p) for p in sys.path ]
+    """Refresh the module-level paths"""
+    paths[:] = []
+    for p in sys.path:
+        paths.append(Path(p))
+
 
 def get_refreshed_paths():
-    '''Refresh the module-level paths and return it'''
+    """Refresh the module-level paths and return it"""
     refresh_paths()
     return paths
 
+
 def sites(some_paths=None):
-    '''All items in some_paths which have "site-"
+    """All items in some_paths which have "site-"
 
     Uses sis.paths by default, so gives all the site- paths from sys
-    '''
-    if not some_paths: some_paths = paths
-    return [ l for l in some_paths if 'site-' in l ]
+    """
+    if not some_paths:
+        some_paths = paths
+    return [l for l in some_paths if 'site-' in l]
+
 
 def site_packages(some_paths=None):
-    '''All items in some_paths with basename "site-packages"
+    """All items in some_paths with basename "site-packages"
 
-    Uses sis.paths by default, so gives all site-packages from sys'''
-    if not some_paths: some_paths = paths
-    return [ p for p in some_paths if p.basename() =='site-packages' ]
+    Uses sis.paths by default, so gives all site-packages from sys"""
+    if not some_paths:
+        some_paths = paths
+    return [p for p in some_paths if p.basename() == 'site-packages']
+
 
 refresh_paths()
