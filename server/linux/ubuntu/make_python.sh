be0e57581dd73589365f68746b5b3357bf01b8d6
J Alan Brogan <github@al-got-rhythm.net>
2015-07-21 13:15:35 +0100

    Split out Ubuntu specialisation to own script

--- server/linux/ubuntu/make_python.sh
+++ server/linux/ubuntu/make_python.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DOWN=$HOME/Downloads/Python
+DOWN=/home/alanb/Downloads/Python
 test -d $DOWN && rm -rf $DOWN
 mkdir -p $DOWN
 cd $DOWN
@@ -9,8 +9,8 @@ if python -c "import sys; sys.exit(int(sys.version.split()[0] >= '2.7.4'))"; the
     wget http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
     tar zxf Python-2.7.10.tgz 
     cd Python-2.7.10
-    sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr/local/ssl|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup.dist
-    ./configure --enable-shared --prefix=$HOME 2>&1 > configuring.log
+    sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup.dist
+    ./configure --enable-unicode=ucs4 --enable-shared --prefix=$HOME 2>&1 > configuring.log
     make 2>&1 > making.log || exit
     make install 2>&1 > installing.log || exit
     cd ..
