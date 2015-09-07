791eba6cb2bb0cb8db9839541c9d71a79568c5f1
J Alan Brogan <github@al-got-rhythm.net>
2015-09-07 14:53:18 +0100

    Re-use previously defined $DESTINATION_DIRECTORY

--- crontab/jab.ook/Users/jab/bin/save_profile.sh
+++ crontab/jab.ook/Users/jab/bin/save_profile.sh
@@ -5,5 +5,5 @@ PROFILE="/$OSX_HOME/Library/Application Support/Firefox/Profiles/0r6wcrwt.defaul
 SOURCE="$PROFILE/jetpack/jid1-xUfzOsOFlzSOXg@jetpack/simple-storage/store.json"
 DESTINATION_DIRECTORY=/Volumes/HD/$OSX_HOME/log
 DESTINATION=$DESTINATION_DIRECTORY/store.json.$(/bin/date +%Y%m%d)
-/bin/cp -v "$SOURCE" "$DESTINATION" > /$OSX_HOME/bin/save_profile.log 2>/Users/jab/bin/save_profile.err
+/bin/cp -v "$SOURCE" "$DESTINATION" > $DESTINATION_DIRECTORY/save_profile.log 2>$DESTINATION_DIRECTORY/save_profile.err
 find $DESTINATION_DIRECTORY -mtime +30 -delete
