37067fa689531aa6673a67c368107420bf851b29
J Alan Brogan <github@al-got-rhythm.net>
2015-09-15 16:31:07 +0100

    Install autocutsel from source on CentOS

--- server/linux/centos/install_software.sh
+++ server/linux/centos/install_software.sh
@@ -1,6 +1,34 @@
 #! /bin/bash
 
-sudo yum update
-sudo yum upgrade
-test -f guest_additions.sh && /bin/bash guest_additions.sh
-sudo /usr/bin/yum -y -q install -qq ctags ack sshfs openssh-server
+set -e
+
+yummy () {
+    sudo yum update
+    sudo yum upgrade
+    # test -f guest_additions.sh && /bin/bash guest_additions.sh
+    sudo /usr/bin/yum -y -q install ctags ack sshfs openssh-server
+}
+
+get_autocutsel () {
+    wget https://github.com/sigmike/autocutsel/releases/download/0.10.0/autocutsel-0.10.0.tar.gz
+    tar zxf autocutsel-0.10.0.tar.gz 
+    cd autocutsel-0.10.0
+    sudo yum -y -q install glibc-devel glibc-devel.i686 libXtst-devel libXaw-devel
+    ./configure
+    make
+    sudo make install
+}
+
+downloads () {
+    local HERE=$(pwd)
+    cd ~/Downloads
+    get_autocutsel
+    cd $HERE
+}
+
+main () {
+    yummy
+    gitty
+}
+
+main
