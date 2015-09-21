fe658b9b5b769a91faeada50ea30f29f3cba2c46
J Alan Brogan <github@al-got-rhythm.net>
2015-09-21 11:29:52 +0100

    Keep vim backups in ~/old

--- server/jab/clean_directories.sh
+++ server/jab/clean_directories.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 rm -rf ~/Documents ~/Music ~/Pictures ~/Public ~/Templates ~/Videos ~/examples.desktop
-mkdir -p ~/Downloads ~/bin ~/src ~/tmp ~/lib ~/log ~/mnt ~/txt
+mkdir -p ~/Downloads ~/bin ~/src ~/tmp ~/lib ~/log ~/mnt ~/txt ~/old
