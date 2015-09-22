f6f8f4cd05b0e1ad618d633a795a84c9c295db2e
J Alan Brogan <github@al-got-rhythm.net>
2015-09-22 11:30:42 +0100

    Remove trailing spaces

--- server/linux/make_python.sh
+++ server/linux/make_python.sh
@@ -1,55 +1,55 @@
 #! /bin/bash
 
-set -e 
+set -e
 
 SCRATCH=/tmp/Downloads
 MINIMUM_VERSION=2.7.10
 
 set_up () {
-	trap tear_down EXIT
-	if [[ -d ${SCRATCH} ]]; then
-		 rm -rf ${SCRATCH}/*
-	else
-		 mkdir ${SCRATCH}
-	fi
-	cd ${SCRATCH}
+    trap tear_down EXIT
+    if [[ -d ${SCRATCH} ]]; then
+         rm -rf ${SCRATCH}/*
+    else
+         mkdir ${SCRATCH}
+    fi
+    cd ${SCRATCH}
 }
 
 tear_down () {
-	if [[ -d ${SCRATCH} ]]; then
-		rm -rf ${SCRATCH}
-	fi
+    if [[ -d ${SCRATCH} ]]; then
+        rm -rf ${SCRATCH}
+    fi
 }
 
 install_home_python () {
-	wget http://www.python.org/ftp/python/${MINIMUM_VERSION}/Python-${MINIMUM_VERSION}.tgz
-	tar zxf Python-${MINIMUM_VERSION}.tgz 
-	cd Python-${MINIMUM_VERSION}
-	sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr/local/ssl|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup.dist
-	./configure --enable-shared --prefix=$HOME 2>&1 > configuring.log
-	make 2>&1 > making.log || exit
-	make install 2>&1 > installing.log || exit
-	cd ..
+    wget http://www.python.org/ftp/python/${MINIMUM_VERSION}/Python-${MINIMUM_VERSION}.tgz
+    tar zxf Python-${MINIMUM_VERSION}.tgz
+    cd Python-${MINIMUM_VERSION}
+    sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr/local/ssl|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup.dist
+    ./configure --enable-shared --prefix=$HOME 2>&1 > configuring.log
+    make 2>&1 > making.log || exit
+    make install 2>&1 > installing.log || exit
+    cd ..
 }
 
 install_home_pip () {
-	wget https://bootstrap.pypa.io/get-pip.py | $HOME/bin/python
+    wget https://bootstrap.pypa.io/get-pip.py | $HOME/bin/python
 }
 
 pip_install_modules () {
-	$HOME/bin/pip install ipython
-	$HOME/bin/pip install pudb
-	$HOME/bin/pip install virtualenv
-	$HOME/bin/pip install virtualenvwrapper
-	$HOME/bin/pip install ansible
+    $HOME/bin/pip install ipython
+    $HOME/bin/pip install pudb
+    $HOME/bin/pip install virtualenv
+    $HOME/bin/pip install virtualenvwrapper
+    $HOME/bin/pip install ansible
 }
 
 main () {
-		install_home_python
-		install_home_pip
-		pip_install_modules
-	fi
-    if ! mython -c "import sys; from distutils.version import LooseVersion as ver; sys.exit(int(ver(sys.version.split()[0]) < ver('"$MINIMUM_VERSION"')))"; then
+    if ! python -c "import sys; from distutils.version import LooseVersion as ver; sys.exit(int(ver(sys.version.split()[0]) < ver('"$MINIMUM_VERSION"')))"; then
+        install_home_python
+        install_home_pip
+        pip_install_modules
+    fi
 }
 
 set_up
--- server/linux/ubuntu/make_python.sh
+++ server/linux/ubuntu/make_python.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -e 
+set -e
 
 DOWN=/home/alanb/Downloads/Python
 test -d $DOWN && rm -rf $DOWN
@@ -9,7 +9,7 @@ cd $DOWN
 
 if python -c "import sys; sys.exit(int(sys.version.split()[0] >= '2.7.4'))"; then
     wget http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-    tar zxf Python-2.7.10.tgz 
+    tar zxf Python-2.7.10.tgz
     cd Python-2.7.10
     sed -i -e "s|#SSL=/usr/local/ssl|SSL=/usr|" -e "s|^#_ssl|_ssl|" -e "/lssl/s/#//" -e "/USE_SSL/s/#//" Modules/Setup.dist
     ./configure --enable-unicode=ucs4 --enable-shared --prefix=$HOME 2>&1 > configuring.log
@@ -18,7 +18,7 @@ if python -c "import sys; sys.exit(int(sys.version.split()[0] >= '2.7.4'))"; the
     cd ..
     wget http://bootstrap.pypa.io/ez_setup.py | $HOME/bin/python - --user
     pip install ipython
-    easy_install-2.7 pudb 
+    easy_install-2.7 pudb
 fi
 
 #rm -rf $DOWN
