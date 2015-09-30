50ae27f81b0454564636b6084f04a0076b07cdd1
J Alan Brogan <github@al-got-rhythm.net>
2015-09-30 16:04:21 +0100

    Add a fantastic font

---
+++ server/linux/gnome/get_fantasque_font.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -e
+
+SCRATCH=/tmp/Downloads
+
+set_up () {
+	trap tear_down EXIT
+	if [[ -d ${SCRATCH} ]]; then
+		 rm -rf ${SCRATCH}/*
+	else
+		 mkdir ${SCRATCH}
+	fi
+	cd ${SCRATCH}
+}
+
+tear_down () {
+	if [[ -d ${SCRATCH} ]]; then
+		rm -rf ${SCRATCH}
+	fi
+}
+
+main () {
+	wget https://fontlibrary.org/assets/downloads/fantasque-sans-mono/db52617ba875d08cbd8e080ca3d9f756/fantasque-sans-mono.zip
+    unzip fantasque-sans-mono.zip
+    test -d ~/.fonts || mkdir ~/.fonts
+    cp *.ttf ~/.fonts
+}
+
+set_up
+main
+tear_down
