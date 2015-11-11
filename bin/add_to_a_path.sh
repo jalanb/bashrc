818768ff0d37181309117ddb43ad0dfc2a7c9559
J Alan Brogan <github@al-got-rhythm.net>
2015-11-11 22:33:12 +0000

    Typo

--- bin/add_to_a_path.sh
+++ bin/add_to_a_path.sh
@@ -24,8 +24,7 @@ _find_python () {
 
 _run_script () {
     local jab="${JAB:-$(_git_root $BASH_SOURCE)}"
-    local jab_python="${JAB/src/python:-${jab}/src/python}"
-    local script=$jab_python/add_to_a_path.py
+    local script=$JAB/src/python/add_to_a_path.py
     if [[ -f "$script" ]]; then
         if _find_python; then
             $PYTHON $script "$@"
@@ -35,7 +34,7 @@ _run_script () {
         fi
     else
         echo $script is not a file >&2
-        [[ ! -d "$jab_python" ]] && echo $jab_python is not a dir >&2
+        [[ ! -d "$JAB/src/python" ]] && echo $JAB/src/python is not a dir >&2
     fi
 }
 
