222bdc71bce14a1ed9359a6d4983b48bae94a08a
J Alan Brogan <github@al-got-rhythm.net>
2015-11-22 22:53:48 +0000

    New script to provide require()

--- src/bash/login.sh
+++ src/bash/login.sh
@@ -7,11 +7,10 @@ GIT=$SRC/git
 HUB=$GIT/hub
 GITHUB=$HUB
 source $HUB/what/what.sh
-require $HUB/what/what.sh
+source_what $HUB/kd/kd.sh
 
-require $HUB/kd/kd.sh
-KD=$HUB/kd/kd.sh
 JAB=$HUB/dotjab
+source_what $JAB/src/bash/require.sh
 require $JAB/src/bash/functons.sh
 require $JAB/environ.d/environ.sh
 require $JAB/environ.d/python.sh
---
+++ src/bash/require.sh
@@ -0,0 +1,15 @@
+REQUIRE_SOURCE=source
+
+require () {
+    if [[ -z $WHAT_AQCQUIRED ]]; then
+        if [[ -f $HUB/what/what.sh ]]; then
+            source $HUB/what/what.sh
+            REQUIRE_SOURCE=source_what
+        else
+            ls -l $HUB/what/what.sh
+        fi
+    fi
+    REQUIRE_SOURCE "$@"
+}
+
+export REQUIRE_SOURCE
