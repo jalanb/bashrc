e4e15adc3477b2edec0e3877d1f6a210447a8819
J Alan Brogan <github@al-got-rhythm.net>
2015-12-03 04:21:05 +0000

    Extract a function to find this repo

--- environ.d/jab.sh
+++ environ.d/jab.sh
@@ -1,27 +1,25 @@
 #! /bin/bash
 
-_try_known_jab_paths () {
-    local github_jab_dir=~/src/git/hub/dotjab
-    [[ -d "$github_jab_dir" ]] || github_jab_dir=
-    local myhome_jab_dir=~/.jab
-    [[ -d "$myhome_jab_dir" ]] || myhome_jab_dir=
-    local jab_dir=${github_jab_dir:-$myhome_jab_dir}
-    [[ -d "$jab_dir" ]] || jab_dir=
+require $JAB/src/bash/repo.sh
 
-    export JAB=${JAB:-jab_dir}
-    [[ -d "$JAB" ]] || JAB=not_dir_jab
+#! /bin/bash
+
+_set_jab () {
+    JAB=$(path_to_this_repo)
+    export JAB
+    echo JAB = $JAB
 }
 
-_source_jab_environ () {
-    _try_known_jab_paths
-    home_id=~/.ssh/id_jab
+_set_ssh () {
+    local home_id=~/.ssh/id_jab
     [[ -f "$home_id" ]] && export JAB_ID=$home_id
     export JAB_PUB_ID=$JAB_ID.pub
     export JAB_SSH=$(dirname $JAB_PUB_ID)
-    export JAB_HOME=$HOME
-    JABS=
-    [[ -d /usr/local/svn/jabs ]] && JABS=file:///usr/local/svn/jabs
-    export JABS
 }
 
-_source_jab_environ
+_main () {
+    _set_jab
+    _set_ssh
+}
+
+_main
