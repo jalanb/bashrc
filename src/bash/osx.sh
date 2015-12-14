42b6eac60be8af4f01e282f883f7118a4d5cd9af
J Alan Brogan <github@al-got-rhythm.net>
2015-12-14 11:10:47 +0000

    Add functions for use on OS X

---
+++ src/bash/osx.sh
@@ -0,0 +1,14 @@
+#! /bin/cat
+
+_link_to_usr_local_gnu () {
+    for f in $( (cd /usr/local; (cd bin; ls g*;) | ( ([[ -d gnu ]] || mkdir gnu); (cd gnu; sed -e s/^g//;) )) ); do
+        [[ -f $f ]] || ln -s ../bin/g${f} $f
+    done 2>/dev/null | tr '\n' ' '
+}
+
+prefer_gnu () {
+    _link_to_usr_local_gnu
+    export PATH=/usr/local/gnu:$PATH
+}
+
+
