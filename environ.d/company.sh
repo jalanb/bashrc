579d44517ec2bf49b11d56c2c3d630f51560e692
J Alan Brogan <github@al-got-rhythm.net>
2015-12-14 10:20:52 +0000

    Do not connect to undefined server

--- environ.d/company.sh
+++ environ.d/company.sh
@@ -11,8 +11,10 @@ export EMPLOYER_USERNAME=ab13173
 if [[ -d /Users/jab ]]; then  # looks like OS X
     USER=abrogan
     if [[ $1 != "local" ]]; then
-        if ! ping -c 1 -t 3 $EMPLOYER_SERVER >/dev/null 2>&1 ;then
-            echo Cannot contact $EMPLOYER_SERVER
+        if [[ -z $EMPLOYER_SERVER ]]; then
+            echo EMPLOYER_SERVER is undefined >&2
+        elif ! ping -c 1 -t 3 $EMPLOYER_SERVER >/dev/null 2>&1 ;then
+            echo Cannot contact $EMPLOYER_SERVER >&2
         fi
     fi
 fi
