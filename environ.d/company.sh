ca834755db2047a859013cba959911cd6b7957a9
J Alan Brogan <github@al-got-rhythm.net>
2016-01-31 03:27:16 +0000

    Remove redundant code (sic!)

--- environ.d/company.sh
+++ environ.d/company.sh
@@ -2,19 +2,5 @@
 
 # Load employer's environment
 
-unset SITE
-unset PYTHONPATH
-
 export EMPLOYER_DOMAIN=wwts.com
 export EMPLOYER_USERNAME=ab13173
-
-if [[ -d /Users/jab ]]; then  # looks like OS X
-    USER=abrogan
-    if [[ $1 != "local" ]]; then
-        if [[ -z $EMPLOYER_SERVER ]]; then
-            echo EMPLOYER_SERVER is undefined >&2
-        elif ! ping -c 1 -t 3 $EMPLOYER_SERVER >/dev/null 2>&1 ;then
-            echo Cannot contact $EMPLOYER_SERVER >&2
-        fi
-    fi
-fi
