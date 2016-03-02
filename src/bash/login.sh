770d63789fa6c2169b445bd30946fc0379410fb4
J Alan Brogan <github@al-got-rhythm.net>
2016-03-02 14:02:40 +0000

    Run bashrc commands only when interactive

--- src/bash/login.sh
+++ src/bash/login.sh
@@ -1,36 +1,36 @@
 #! /bin/bash
 
-test -f ~/.local_login.sh && source ~/.local_login.sh
 
-LOCAL=/usr/local
-[[ -d $LOCAL/gnu ]] && LOCALS="$LOCAL/gnu:$LOCAL/bin" || LOCALS="$LOCAL/bin"
-BINS=/usr/bin:/bin:/usr/sbin:/sbin
-HOMES=$HOME/bin:$HOME/.local
-export PATH=$LOCALS:$BINS:$HOMES
-[[ -d $LOCAL/go/bin ]] && PATH=$PATH:$LOCAL/go/bin
+if [[ $- =~ i ]]; then
+    LOCAL=/usr/local
+    [[ -d $LOCAL/gnu ]] && LOCALS="$LOCAL/gnu:$LOCAL/bin" || LOCALS="$LOCAL/bin"
+    BINS=/usr/bin:/bin:/usr/sbin:/sbin
+    HOMES=$HOME/bin:$HOME/.local
+    export PATH=$LOCALS:$BINS:$HOMES
+    [[ -d $LOCAL/go/bin ]] && PATH=$PATH:$LOCAL/go/bin
 
-SRC=~/src
-HG=$SRC/hg
-GIT=$SRC/git
-HUB=$GIT/hub
-export HUB
+    SRC=~/src
+    HG=$SRC/hg
+    GIT=$SRC/git
+    HUB=$GIT/hub
+    export HUB
 
-source $HUB/jab/__init__.sh
-export JAB
+    source $HUB/jab/__init__.sh
+    export JAB
 
+    echo JAB is $JAB
 
-echo JAB is $JAB
+    export PS1="\$? [\u@\h:\$PWD]\n$ "
 
-export PS1="\$? [\u@\h:\$PWD]\n$ "
+    vbb () {
+        vim -p ~/.bashrc $HUB/jab/__init__.sh "$@" +/bash
+        ls -l ~/.bashrc $HUB/jab/__init__.sh
+    }
 
-vbb () {
-    vim -p ~/.bashrc $HUB/jab/__init__.sh "$@" +/bash
-    ls -l ~/.bashrc $HUB/jab/__init__.sh
-}
+    export PS1="\$? [\u@\h:\$PWD]\n$ "
 
-export PS1="\$? [\u@\h:\$PWD]\n$ "
-
-# set -x
-jj
-# set +x
-sp
+    # set -x
+    jj
+    # set +x
+    sp
+fi
