b96d2e64bf50841af164e6ff0009918697bcd8d3
J Alan Brogan <github@al-got-rhythm.net>
2016-03-07 11:34:47 +0000

    Changing spacing to emphasize similarity

--- src/bash/hub.sh
+++ src/bash/hub.sh
@@ -1,6 +1,6 @@
 #! /bin/cat
 
 SRC=; [[ -d $HOME/src ]] && SRC=$HOME/src; export SRC
-HG=; [[ -d $SRC/hg ]] && HG=$SRC/hg; export HG
-GIT=; [[ -d $SRC/git ]] && GIT=$SRC/git; export GIT
-HUB=; [[ -d $GIT/hub ]] && HUB=$GIT/hub; export HUB
+ HG=; [[ -d $SRC/hg   ]] &&  HG=$SRC/hg;   export HG
+GIT=; [[ -d $SRC/git  ]] && GIT=$SRC/git;  export GIT
+HUB=; [[ -d $GIT/hub  ]] && HUB=$GIT/hub;  export HUB
