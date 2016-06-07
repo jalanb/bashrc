009a855864a90e8246594c47aa4f6a7a924567c7
J Alan Brogan <github@al-got-rhythm.net>
2016-06-07 03:24:53 +0100

    Here be dragons

---
+++ src/python/jadb.py
@@ -0,0 +1,14 @@
+
+import os
+
+from dotsite import term
+
+
+import pudbs
+
+def back_trace():
+    term_size = term.screen_width(), term.screen_height()
+    pudbs.back_trace(term_size. pudbs.frame_above())
+
+if __name__ == '__main__':
+    back_trace()
