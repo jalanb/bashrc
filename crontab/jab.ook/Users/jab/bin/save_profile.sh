faa0e582a7f7512aa8f01c68b3c6b4fa4286069b
J Alan Brogan <github@al-got-rhythm.net>
2016-06-07 02:45:10 +0100

    Help El Capitan find the way $HOME

--- crontab/jab.ook/Users/jab/bin/save_profile.sh
+++ crontab/jab.ook/Users/jab/bin/save_profile.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
-OSX_HOME=Users/jab
-PROFILE="/$OSX_HOME/Library/Application Support/Firefox/Profiles/0r6wcrwt.default"
+_show_home () {
+    local _home=$1
+    [[ $HOME == $_home ]] && return 0
+    [[ -n $HOME ]] || echo \$HOME was $HOME
+    if [[ ! -d $_home ]]; then
+        echo $_home is not a directory >& 2
+        return 1
+    fi
+    echo $HOME
+}
+
+_osx_home=$(_show_home /Users/jab)
+PROFILE="$HOME/Library/Application Support/Firefox/Profiles/0r6wcrwt.default"
 SOURCE="$PROFILE/jetpack/jid1-xUfzOsOFlzSOXg@jetpack/simple-storage/store.json"
-DESTINATION_DIRECTORY=/Volumes/HD/$OSX_HOME/log
+DESTINATION_DIRECTORY=/Volumes/HD$_osx_home/log
 DESTINATION=$DESTINATION_DIRECTORY/store.json.$(/bin/date +%Y%m%d)
 /bin/cp -v "$SOURCE" "$DESTINATION" > $DESTINATION_DIRECTORY/save_profile.log 2>$DESTINATION_DIRECTORY/save_profile.err
 find $DESTINATION_DIRECTORY -mtime +30 -delete
