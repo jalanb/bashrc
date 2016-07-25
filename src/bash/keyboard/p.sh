0d8beec6484791af897aa8d56c5f758ad813bb1c
J Alan Brogan <github@al-got-rhythm.net>
2016-07-25 22:45:04 +0100

    Remove redundant functions

--- src/bash/keyboard/p.sh
+++ src/bash/keyboard/p.sh
@@ -3,32 +3,19 @@
 # _x
 # _xx
 # _xxx
-
-_pdb () {
-    _python pdb "$@"
-}
-
 # _xxxx
-
-_pudb () {
-    _python pudb "$@"
-}
-
 # _xxxxx
 # _xxxxxx
 
 _python () {
-    _pyth "$@"
-    (cd $(dirname $(readlink -f $BASH_SOURCE))
     local _executable="$1"; shift
-    local _script="$2"; shift
+    local _script="$1"; shift
     if [[ $_executable == python ]]; then
         $(python $_script "$@")
     else
         # Other prgrams might not like the subshell so much
         $_executable $_script "$@"
     fi
-    )
 }
 
 # x
