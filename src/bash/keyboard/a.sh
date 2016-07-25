02f829ef213a86b12e63ce9ab9acdf92cdaaf956
J Alan Brogan <github@al-got-rhythm.net>
2016-07-25 22:43:16 +0100

    ack from known directories

--- src/bash/keyboard/a.sh
+++ src/bash/keyboard/a.sh
@@ -3,6 +3,9 @@
 # _x
 # _xx
 
+_av () {
+    (cd $1; vack "$@")
+}
 # x
 
 # a is defined in ack2vim.sh
@@ -15,4 +18,49 @@
 # xxx
 
 # aaa is defined in ack2vim.sh
+
+avb () {
+    __doc__="vack all args in $JAB/src/bash";
+    _av $JAB/src/bash "$@"
 }
+
+avd () {
+    __doc__="vack all args in $DASHBOARD";
+    _av $DASHBOARD "$@"
+}
+
+avg () {
+    __doc__="vack all args in $GIT";
+    _av $GIT "$@"
+}
+
+avh () {
+    __doc__="vack all args in $HUB";
+    _av $HUB "$@"
+}
+
+avj () {
+    __doc__="vack all args in $JAB";
+    _av $JAB "$@"
+}
+
+avp () {
+    __doc__="vack all args in $JAB/src/python";
+    _av $JAB/src/python "$@"
+}
+
+avs () {
+    __doc__="vack all args in $SRC";
+    _av $SRC "$@"
+}
+
+avu () {
+    __doc__="vack all args in ~";
+    _av ~ "$@"
+}
+
+avv () {
+    __doc__="vack all args in $JAB/vim";
+    _av $JAB/vim "$@"
+}
+
