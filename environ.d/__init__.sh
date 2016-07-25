c8ab6ae263e8c7055d2d47e04f99439fbae3dea9
J Alan Brogan <github@al-got-rhythm.net>
2016-07-25 22:25:57 +0100

    Use local DIR symbol to avoid interference with other scripts

--- environ.d/__init__.sh
+++ environ.d/__init__.sh
@@ -1,11 +1,11 @@
 #! /bin/cat
 
-DIR=~/jab/environ.d
-[[ -n $WELCOME_BYE ]] && echo Welcome to $DIR
+ENVIRON_DIR=~/jab/environ.d
+[[ -n $WELCOME_BYE ]] && echo Welcome to $ENVIRON_DIR
 
-. $DIR/jab.sh
-. $DIR/environ.sh
-. $DIR/python.sh
-. $DIR/company.sh 2>/dev/null
-. $DIR/ssh_completion.sh
-[[ -n $WELCOME_BYE ]] && echo Bye from $DIR
+. $ENVIRON_DIR/jab.sh
+. $ENVIRON_DIR/environ.sh
+. $ENVIRON_DIR/python.sh
+. $ENVIRON_DIR/company.sh 2>/dev/null
+. $ENVIRON_DIR/ssh_completion.sh
+[[ -n $WELCOME_BYE ]] && echo Bye from $ENVIRON_DIR
