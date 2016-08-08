cc3f110dc952a266952f7f8b537c7bdec269faad
J Alan Brogan <github@al-got-rhythm.net>
2016-08-08 11:38:26 +0100

    Template for scripts

---
+++ src/bash/all_abored.sh
@@ -0,0 +1,26 @@
+#! /bin/cat
+
+# . ~/jab/src/bash/all_abored.sh
+
+# aaaa may be defined in all_abored.sh
+
+# x
+
+alias a=ack
+# _
+# xx
+
+alias aa=aack
+# _x
+# xxx
+
+aaa () {
+    aaack "$@"
+}
+# _xx
+# xxxx
+# _xxx
+# xxxxx
+# _xxxx
+# xxxxxx
+# _xxxxx
