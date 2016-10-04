511981a1a890446f0b155aaa30ee2776687fe0c7
J Alan Brogan <github@al-got-rhythm.net>
2016-10-04 21:59:03 +0100

    Add travis integration

--- environ.d/travis.sh
+++ environ.d/travis.sh
@@ -2,12 +2,8 @@
 
 [[ -n $WELCOME_BYE ]] && echo Welcome to $(basename $BASH_SOURCE) in $(dirname $(readlink -f $BASH_SOURCE)) on $(hostname -f)
 
-ENVIRON_DIR=~/jab/environ.d
-. $ENVIRON_DIR/jab.sh
-. $ENVIRON_DIR/environ.sh
-. $ENVIRON_DIR/python.sh
-. $ENVIRON_DIR/company.sh 2>/dev/null
-. $ENVIRON_DIR/ssh_completion.sh
-. $ENVIRON_DIR/travis.sh
+# added by travis gem
+[ -f $HOME/.travis/travis.sh ] && source $HOME/.travis/travis.sh
 
 [[ -n $WELCOME_BYE ]] && echo Bye from $(basename $BASH_SOURCE) in $(dirname $(readlink -f $BASH_SOURCE)) on $(hostname -f)
+
