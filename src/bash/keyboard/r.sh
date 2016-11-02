5c27f79f27b3deb5fd7422233235706a0b94d975
J Alan Brogan <jalanb@users.noreply.github.com>
2016-11-02 20:43:10 +0000

    Scalp Herman

--- src/bash/keyboard/r.sh
+++ src/bash/keyboard/r.sh
@@ -14,11 +14,12 @@ rr () {
     [[ "$_path" == "/" ]] && return 1
     local _home=$(readlink -f $HOME)
     if [[ "$_path" == "$_home" || "$@" == "$HOME"]]; then
+        echo "Scalp Herman"
         echo "Are you mad? What have you done? I made a deal with your general for that man's life!"
         echo "Yeah, they made that deal, but they don't give a fuck about him. They need you"
         echo "You'll be shot for this"
         echo "Nah, I don't think so, more like chewed out. I been chewed out before"
-        rm -ri "$@"
+        rm -rfi "$@"
     else
         rm -rf "$@"
     fi
