ba7f081cc86ad886a3c045bd50dd11ac969727a6
J Alan Brogan <github@al-got-rhythm.net>
2016-11-03 20:02:05 +0000

    remove dangling link

---
+++ src/python/dump_args.py
@@ -0,0 +1,14 @@
+def dump_args(func):
+    "This decorator dumps out"
+    argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
+    fname = func.func_name
+    def echo_func(*a, **kw):
+        print fname, ": "
+        return func(*a, **kw)
+    return echo_func
+
+@dump_args
+def f1(a, b, c):
+    print a + b +c
+
+f1(1, 2, 3)
