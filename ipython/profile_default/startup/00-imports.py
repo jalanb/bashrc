bdd2ed409bdba2297713e5b53f93d48755eb6c5e
J Alan Brogan <github@al-got-rhythm.net>
2016-11-13 06:45:58 +0000

    Viral spread

--- ipython/profile_default/startup/00-imports.py
+++ ipython/profile_default/startup/00-imports.py
@@ -3,23 +3,23 @@
 # pylint: disable-msg=W0611
 
 # import this
-# print
+from __future__ import print_function
 
 import os
 import re
 import sys
 import dotsite
-print 'import os, re, sys, dotsite'
 
-from path import path
-print 'from path import path'
 from see import see
 from see import see_attributes
 from see import see_methods
 from see import spread
-print 'from see import see, see_attributes, see_methods, spread'
 from see_code import code
 from see_code import highlight
 from see_code import see_code
 from see_code import see_highlight
-print 'from see_code import code, highlight, see_code, see_highlight'
+
+
+print('import os, re, sys, dotsite')
+print('from see import see, see_attributes, see_methods, spread')
+print('from see_code import code, highlight, see_code, see_highlight')
