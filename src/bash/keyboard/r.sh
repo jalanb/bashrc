4ec256580dec3a20205f9c18fb46e32a0bb51ea8
J Alan Brogan <github@al-got-rhythm.net>
2016-11-13 23:46:10 +0000

    Add interactive chew

--- src/bash/keyboard/r.sh
+++ src/bash/keyboard/r.sh
@@ -9,14 +9,25 @@ r () {
 
 # xx
 
+ri () {
+    rr -i "$@"
+}
+
 rr () {
     local _path=$(readlink -f "$@")
     [[ "$_path" == "/" ]] && return 1
-    local _home=$(readlink -f $HOME)
-    if [[ "$_path" == "$_home" || "$@" == "$HOME" ]]; then
+    local _shoot=
+    if [[ $1 == '-i' ]]; then
+        _shoot=2
+        shift
+    fi
+    if [[ "$_path" == $(readlink -f $HOME) || "$@" == "$HOME" ]]; then
+        _shoot=1
         echo "Scalp Herman"
         echo "Are you mad? What have you done? I made a deal with your general for that man's life!"
         echo "Yeah, they made that deal, but they don't give a fuck about him. They need you"
+    fi
+    if [[ -z $_shoot ]]; then
         echo "You'll be shot for this"
         echo "Nah, I don't think so, more like chewed out. I been chewed out before"
         rm -rfi "$@"
