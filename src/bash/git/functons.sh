11732cfb35e23a70823870cfe217c46fdf9d206d
J Alan Brogan <github@al-got-rhythm.net>
2016-11-15 14:44:35 +0000

    Remove temp debugging statements

--- src/bash/git/functons.sh
+++ src/bash/git/functons.sh
@@ -37,7 +37,7 @@ gc () {
     if [[ -z "$@" ]]; then git commit --verbose
     else
         python -c "print 'git commit -m\"$*\"'" > $_storage
-        (set -x; _show_run_storage)
+        _show_run_storage
     fi
     GIT_ADDED=
 }
@@ -110,7 +110,7 @@ gcj () {
     if [[ -z "$@" ]]; then $JIT commit --all --verbose
     else
         python -c "print '$JIT commit -m\"$*\"'" > $_storage
-        (set -x; _run_storage)
+        _run_storage
     fi
 }
 
