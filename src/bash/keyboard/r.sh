a372655ffb1f3f54bec491d4ee25fc2c79551141
J Alan Brogan <github@al-got-rhythm.net>
2016-11-15 14:44:12 +0000

    typo

--- src/bash/keyboard/r.sh
+++ src/bash/keyboard/r.sh
@@ -27,7 +27,7 @@ rr () {
         echo "Are you mad? What have you done? I made a deal with your general for that man's life!"
         echo "Yeah, they made that deal, but they don't give a fuck about him. They need you"
     fi
-    if [[ -z $_shoot ]]; then
+    if [[ -n $_shoot ]]; then
         echo "You'll be shot for this"
         echo "Nah, I don't think so, more like chewed out. I been chewed out before"
         rm -rfi "$@"
