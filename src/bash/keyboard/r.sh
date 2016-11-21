17aff37cc6176cb5b7a637a0bcd63591bdecfe2a
J Alan Brogan <github@al-got-rhythm.net>
2016-11-21 15:48:27 +0000

    Prompt user for directory removal
    "rm -ri" will ask for every file

--- src/bash/keyboard/r.sh
+++ src/bash/keyboard/r.sh
@@ -10,17 +10,31 @@ r () {
 # xx
 
 ri () {
-    rr -i "$@"
+    local _argc=${#*}
+    if [[ $_argc == 1 ]]; then
+        if [[ -d $1 ]]; then
+            shoot
+            read -p "rm -rf $1 [Y]" reply
+            [[ $reply =~ (^$|[yY]) ]] && rm -rf "$1"
+        fi
+    else
+        rr -i "$@"
+    fi
+}
+
+shoot () {
+    echo "You'll be shot for this"
+    echo "Nah, I don't think so, more like chewed out. I been chewed out before"
 }
 
 rr () {
-    local _path=$(readlink -f "$@")
-    [[ "$_path" == "/" ]] && return 1
     local _shoot=
     if [[ $1 == '-i' ]]; then
         _shoot=2
         shift
     fi
+    local _path=$(readlink -f "$@")
+    [[ "$_path" == "/" ]] && return 1
     if [[ "$_path" == $(readlink -f $HOME) || "$@" == "$HOME" ]]; then
         _shoot=1
         echo "Scalp Herman"
@@ -28,6 +42,7 @@ rr () {
         echo "Yeah, they made that deal, but they don't give a fuck about him. They need you"
     fi
     if [[ -n $_shoot ]]; then
+        shoot
         echo "You'll be shot for this"
         echo "Nah, I don't think so, more like chewed out. I been chewed out before"
         rm -rfi "$@"
