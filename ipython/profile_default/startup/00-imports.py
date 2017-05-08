c376a1c7fadd90281b77abbf01ab6a90292c0eb8
J Alan Brogan <github@al-got-rhythm.net>
2017-05-08 02:18:30 +0100

    If blind, do not try to see

--- ipython/profile_default/startup/00-imports.py
+++ ipython/profile_default/startup/00-imports.py
@@ -10,16 +10,20 @@ import re
 import sys
 import dotsite
 
-from see import see
-from see import see_attributes
-from see import see_methods
-from see import spread
-from see_code import code
-from see_code import highlight
-from see_code import see_code
-from see_code import see_highlight
+try:
+    from see import see
+    from see import see_attributes
+    from see import see_methods
+    from see import spread
+    from see_code import code
+    from see_code import highlight
+    from see_code import see_code
+    from see_code import see_highlight
+except ImportError:
+    see = False
 
 
 print('import os, re, sys, dotsite')
-print('from see import see, see_attributes, see_methods, spread')
-print('from see_code import code, highlight, see_code, see_highlight')
+if see:
+    print('from see import see, see_attributes, see_methods, spread')
+    print('from see_code import code, highlight, see_code, see_highlight')
