9e67783e20d78df35d117a3ed7f0cf56cdaa641c
J Alan Brogan <github@al-got-rhythm.net>
2017-05-25 01:49:14 +0100

    First vital installs on a new machine
    (cherry picked from commit 5e927f3c542ee5924f81aaa33f749aff89318811)

---
+++ brews.sh
@@ -0,0 +1,12 @@
+#! bash
+
+# jab.mini
+
+brew install bumpversion
+brew install htop
+brew install ping
+brew install pyenv
+brew install pyenv-virtualenv
+brew install python
+brew install python3
+
