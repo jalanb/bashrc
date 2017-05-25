487d4982caa1b23896728c2cfce60eef6a2efd4c
J Alan Brogan <github@al-got-rhythm.net>
2017-05-25 01:58:56 +0100

    Add full brews from jab.ook

--- brews.sh
+++ brews.sh
@@ -1,12 +1,66 @@
 #! bash
 
-# jab.mini
+brew update
 
+GIT= brew install codeclimate
+GIT=/usr/bin/git brew install git
+brew install Caskroom/cask/gimp
+brew install Caskroom/cask/mypaint
+brew install Caskroom/cask/qfinder
+brew install MyPaint
+brew install ack
+brew install bash
 brew install bumpversion
+brew install cmake
+brew install codeclimate
+brew install elasticsearch
+brew install elasticsearch
+brew install find
+brew install findutils
+brew install freetds
+brew install freetds
+brew install freetds
+brew install freetds
+brew install freetds
+brew install fundutils
+brew install gcc
+brew install gcc
+brew install ghi
+brew install gimp
+brew install git
+brew install gnu-sed --with-default-names
+brew install graphviz
+brew install hg
+brew install homebrew/versions/freetds091
+brew install homebrew/x11/meld
 brew install htop
+brew install kibana
+brew install lighttpd
+brew install meld
+brew install mysql
+brew install mysql
+brew install mysql
+brew install ncdu
+brew install ncdu
+brew install pgrep
+brew install phantomjs
+brew install phantomjs
 brew install ping
 brew install pyenv
 brew install pyenv-virtualenv
 brew install python
 brew install python3
+brew install qfinder
+brew install ranger
+brew install ranger
+brew install sloccount
+brew install synergy
+brew install tig
+brew install tree
+brew install upgrade python3
+brew install vifm
+brew install vifm
+brew install vim
+brew install wget
+brew install youtube-dl
 
