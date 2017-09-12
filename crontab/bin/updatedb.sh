a6e35a663aefe0340c5c80ee5eaea7fab34956b4
J Alan Brogan <github@al-got-rhythm.net>
2017-09-12 12:51:50 +0100

    Convert locate.py from optparse to argparse
    (cherry picked from commit 01230322704431614209702c418814f1021e70fd)

--- crontab/bin/updatedb.sh
+++ crontab/bin/updatedb.sh
@@ -1,30 +1,13 @@
 #! /bin/bash
 
-export PRUNEPATHS=\
-    "/Library/Ruby/Gems/1.8/gems/xapian-full-1.1.3.4/xapian-bindings-1.1.3/ruby/docs/rdocs/files/data/home/olly/tmp" \
-    "/Users/jab/.rvm/tmp" \
-    "/Users/jab/Library/Caches/Java/cache/6.0/tmp" \
-    "/Users/jab/Library/Caches/Java/tmp" \
-    "/Users/jab/Library/Caches/PyCharm20/tmp" \
-    "/Users/jab/Sites/jlf/mail/jlf.ie/jlf/tmp" \
-    "/Users/jab/Sites/jlf/mail/tmp" \
-    "/Users/jab/Sites/jlf/tmp" \
-    "/Users/jab/jab/tmp" \
-    "/Users/jab/tmp" \
-    "/Volumes/MobileBackups" \
-    "/mnt " \
-    "/private/tmp" \
-    "/private/var/amavis/tmp" \
-    "/private/var/at/tmp" \
-    "/private/var/spool/cups/tmp" \
-    "/private/var/tmp" \
-    "/tmp" \
-    "/usr/share/devicemgr/backend/tmp" \
+export PRUNEPATHS="/mnt /Applications /Library/Ruby/Gems/1.8/gems/xapian-full-1.1.3.4/xapian-bindings-1.1.3/ruby/docs/rdocs/files/data/home/olly/tmp /Users/jab/.rvm/tmp /Users/jab/Library/Caches/Java/cache/6.0/tmp /Users/jab/Library/Caches/Java/tmp /Users/jab/Library/Caches/PyCharm20/tmp /Users/jab/Sites/jlf/mail/jlf.ie/jlf/tmp /Users/jab/Sites/jlf/mail/tmp /Users/jab/Sites/jlf/tmp /Users/jab/jab/tmp /Users/jab/tmp /Volumes/MobileBackups /private/tmp /private/var/amavis/tmp /private/var/at/tmp /private/var/spool/cups/tmp /private/var/tmp /tmp /usr/share/devicemgr/backend/tmp"
 
-date > /Volumes/HD/Users/jab/log/updatedb.log
-echo "/usr/bin/sudo /bin/bash /usr/local/Cellar/findutils/4.4.2/bin/updatedb" >> /Volumes/HD/Users/jab/log/updatedb.log
-/usr/bin/sudo \
+LOG=/Users/jab/log/updatedb.log
+date > $LOG
+echo "/usr/bin/sudo /bin/bash /usr/local/Cellar/findutils/4.4.2/bin/updatedb" >> $LOG
+LC_ALL='C' /usr/bin/sudo \
     /bin/bash \
         /usr/local/Cellar/findutils/4.4.2/bin/updatedb \
-        --prunepaths=$PRUNEPATHS \
-        >> /Volumes/HD/Users/jab/log/updatedb.log 2>&1
+        --prunepaths="$PRUNEPATHS" \
+        2>&1 \
+        >> $LOG
--- src/python/site/locate.py
+++ src/python/site/locate.py
@@ -5,10 +5,11 @@ from __future__ import print_function
 
 import re
 import os
-import shlex
-import commands  # Eh? pylint: disable=import-error
-# Hello future me , please convert to argparse
-# import optparse
+import argparse
+try:
+    from commands import getoutput, getstatusoutput
+except ImportError:
+    from subprocess import getoutput, getstatusoutput
 from fnmatch import fnmatch
 
 
@@ -30,14 +31,14 @@ def _path_to_locate():
 def _add_option(path_to_locate, option):
     if option[:2] != '--':
         option = '--%s' % option
-    if option in commands.getoutput('%s -h' % path_to_locate):
+    if option in getoutput('%s -h' % path_to_locate):
         return '%s %s' % (path_to_locate, option)
     return path_to_locate
 
 
 def _locate_regexp_option():
     command = '%s --help' % _path_to_locate()
-    _status, output = commands.getstatusoutput(command)
+    _status, output = getstatusoutput(command)
     return '--regexp' if '--regexp' in output else '--regex'
 
 
@@ -51,14 +52,16 @@ def glob_to_regexp(string):
     return '.*'.join(parts)
 
 
-def _make_locate_command(string, options):
-    """Make a command to locate that string"""
-    option = options.ignore_case and '-i' or ''
-    if options.globs:
+def _make_locate_command(args):
+    """Make a command to locate sought arg"""
+    option = args.ignore_case and '-i' or ''
+    if args.globs:
         option = _locate_regexp_option()
-        string = glob_to_regexp(string)
+        string = glob_to_regexp(args.sought)
+    else:
+        string = args.sought
     path_to_locate = _path_to_locate()
-    if options.files or options.directories or options.executables:
+    if args.files or args.directories or args.executables:
         _add_option(path_to_locate, '--basename')
     return '%s %s "%s"' % (path_to_locate, option, string)
 
@@ -72,10 +75,12 @@ def _locatable(path):
     return not repository.is_repository_path(path)
 
 
-def _run_locate(string, options):
-    """Run the locate command on the given string"""
-    command = _make_locate_command(string, options)
-    status, output = commands.getstatusoutput(command)
+def _run_locate(args):
+    """Run the locate command on the sought arg"""
+    command = _make_locate_command(args)
+    if args.verbose:
+        print(command)
+    status, output = getstatusoutput(command)
     if status and output:
         old = re.search('is more than .* days old', output)
         if not old:
@@ -83,122 +88,92 @@ def _run_locate(string, options):
     elif not output:
         return []
     locatable = [l for l in output.split('\n') if _locatable(l)]
-    for exclude in options.exclude or []:
+    for exclude in args.exclude or []:
         locatable = [_ for _ in locatable if not re.search(exclude, _)]
     return locatable
 
 
-def _make_check_method(method, options):
-    """Make a check method by combining the given method with the options"""
+def _make_check_method(method, args):
+    """Make a check method by combining the given method with the args"""
 
     def is_file(string):
         return method(string) and os.path.isfile(string)
 
-    if options.directories:
+    if args.directories:
         def is_dir(string):
             return method(string) and os.path.isdir(string)
         return is_dir
-    if options.files:
+    if args.files:
         return is_file
-    if options.executables:
+    if args.executables:
         def is_exe(string):
             return is_file(string) and os.access(string, os.X_OK)
         return is_exe
     return method
 
 
-def _locate(string, options):
-    """Locate some files called string, restricted by the given options"""
+def _locate(args):
+    """Locate some files called sought arg, restricted by the given args"""
 
     def _compare(one, two):
-        if options.ignore_case:
+        if args.ignore_case:
             one = one.upper()
             two = two.upper()
-        if options.globs:
+        if args.globs:
             return fnmatch(one, two)
         # cmp? pylint: disable=undefined-variable
-        return cmp(one, two) == 0
-
-    def _has_basename(path):
-        return _compare(os.path.basename(path), string)
+        try:
+            return cmp(one, two) == 0
+        except NameError:
+            return (one > two) - (one < two)
 
     def _directory_in_path(path):
-        test_string = string
+        test_string = args.sought
         parts = path.split(os.path.sep)
-        if options.basename:
+        if args.basename:
             parts = parts[-1:]
         for part in parts:
             if _compare(part, test_string):
                 return True
         return False
 
-    lines = _run_locate(string, options)
-    check = _make_check_method(_directory_in_path, options)
+    lines = _run_locate(args)
+    check = _make_check_method(_directory_in_path, args)
     result = [l for l in lines if check(l)]
     if result:
         return result
-    if options.directories or options.files:
+    if args.directories or args.files:
         return []
     return lines
 
 
-def _handle_command_line(args):
+def parse_args():
     """Handle options and arguments from the command line"""
-    parser = optparse.OptionParser()
-    parser.add_option('-b', '--basename', action='store_true',
-                      help='only find basenames')
-    parser.add_option('-d', '--directories', action='store_true',
-                      help='only locate directories')
-    parser.add_option('-e', '--executables', action='store_true',
-                      help='only locate executable files')
-    parser.add_option('-f', '--files', action='store_true',
-                      help='only locate files')
-    parser.add_option('-g', '--globs', action='store_true',
-                      help='match on globs')
-    parser.add_option('-i', '--ignore-case', action='store_true',
-                      help='ignore case in searches')
-    parser.add_option('-l', '--lsld', action='store_true',
-                      help='run "ls -ld" on locations')
-    parser.add_option('-x', '--exclude', type='str', action='append',
-                      help='exclude paths which match regexp(s)')
-    options, args = parser.parse_args(args)
-    return options, args
+    parser = argparse.ArgumentParser()
+    pa = parser.add_argument
+    pa('sought', help='String to locate')
+    pa('-b', '--basename', action='store_true', help='only find basenames')
+    pa('-d', '--directories', action='store_true', help='only locate directories')
+    pa('-e', '--executables', action='store_true', help='only locate executable files')
+    pa('-f', '--files', action='store_true', help='only locate files')
+    pa('-g', '--globs', action='store_true', help='match on globs')
+    pa('-i', '--ignore-case', action='store_true', help='ignore case in searches')
+    pa('-l', '--lsld', action='store_true', help='run "ls -ld" on locations')
+    pa('-x', '--exclude', type=str, nargs='+', help='exclude paths which match regexp(s)')
+    pa('-v', '--verbose', help='Show locate command')
+    args = parser.parse_args()
+    return args
 
 
-def locate_file(command_line):
-    """Locate the file(s) in that command line"""
-    return locate('%s -f' % command_line)
-
-
-def locate_directory(command_line):
-    """Locate the directory/ies in that command line"""
-    return locate('%s -d' % command_line)
-
-
-def locate(command_line):
-    """Main method for calling from python"""
-    args = shlex.split(command_line)
-    options, args = _handle_command_line(args)
-    result = {}
-    if len(args) == 1:
-        return _locate(args[0], options)
-    for arg in args:
-        result[arg] = _locate(arg, options)
-    return result
-
-
-def main(args):
+def main():
     """Main method for calling from bash"""
-    options, args = _handle_command_line(args)
-    result = os.EX_DATAERR
-    for arg in args:
-        located_arg = _locate(arg, options)
-        if not located_arg:
-            continue
-        if options.lsld:
-            ls_commands = [str('ls -ld %r' % str(_)) for _ in located_arg]
-            __ = [print(commands.getoutput(_)) for _ in ls_commands]
-        else:
-            print('\n'.join(located_arg))
-        result = os.EX_OK
-    return result
+    args = parse_args()
+    located_arg = _locate(args)
+    if not located_arg:
+        return not os.EX_OK
+    if args.lsld:
+        ls_commands = [str('ls -ld %r' % str(_)) for _ in located_arg]
+        __ = [print(getoutput(_)) for _ in ls_commands]
+    else:
+        print('\n'.join(located_arg))
+    return os.EX_OK
