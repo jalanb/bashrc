73c5a7017cca63ac98b4a575e0ceb5bd575253b3
J Alan Brogan <github@al-got-rhythm.net>
2017-09-23 02:31:47 +0100

    Use newer function

--- src/bash/osx.sh
+++ src/bash/osx.sh
@@ -3,7 +3,7 @@
 _link_to_usr_local_gnu () {
     for f in $( (cd /usr/local; (cd bin; ls g*;) | ( ([[ -d gnu ]] || mkdir gnu); (cd gnu; sed -e s/^g//;) )) ); do
         [[ -f $f ]] || ln -s ../bin/g${f} $f
-    done 2>/dev/null | tr '\n' ' '
+    done 2>/dev/null | lines_to_spaces
 }
 
 prefer_gnu () {
