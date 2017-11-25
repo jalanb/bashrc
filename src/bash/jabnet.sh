d055289a94db0e6ebc4a3bc5079ec67279778977
J Alan Brogan <github@al-got-rhythm.net>
2017-11-25 20:24:33 +0000

    Better naming

--- src/bash/jabnet.sh
+++ src/bash/jabnet.sh
@@ -6,23 +6,23 @@ Welcome_to $BASH_SOURCE
 # xx
 # xxx
 
-jai () {
-    _ssh jalanmini.local
+ssi () {
+    _ssj jalanmini.local
 }
 
-jab () {
-    _ssh jalan-book.local
+ssb () {
+    _ssj jalan-book.local
 }
 
-jam () {
-    _ssh jalan.mac
+ssm () {
+    _ssj jalan.mac
 }
 
 ssu () {
     local _server=$1
     shift
     if [[ -z "$@" ]]; then
-        ssh -tq $_server "sudo su -" 
+        ssh -tq $_server "sudo su -"
     else
         ssh -tq $_server "sudo $@"
     fi
@@ -34,9 +34,9 @@ ssw () {
 
 # xxxx
 
-_ssh () {
+_ssj () {
     _server=$1
-    [[ $(hostname) == $_server ]] || ssh -q -i ~/.ssh/id_jab $_server
+    [[ $(hostname) == $_server ]] || sst jab $_server -q -i ~/.ssh/id_jab
 }
 
 Bye_from $BASH_SOURCE
