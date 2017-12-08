2758585af6306c9604d0cd31bbcbdc77b7cb0a4c
J Alan Brogan <github@al-got-rhythm.net>
2017-12-08 21:30:27 +0000

    Look for virtualenv after cd

--- src/bash/cde.sh
+++ src/bash/cde.sh
@@ -37,22 +37,27 @@ cde () {
 _post_kd () {
     _show_todo_here && echo
     # set -x
-    _post_kd_bash
-    _post_kd_git  && _post_kd_python && _post_kd_venv
-    _post_kd_show  # see also _post_kd_git
+    _post_kd_bash;  _post_kd_git;  _post_kd_python;  _post_kd_venv; 
+    _post_kd_show && _post_kd_run
     # set +x
 }
 
 # xxxxxxxxxxxx
 
 _post_kd_git () {
-    [[ -d ./.git ]] || return 1
+    [[ -d ./.git ]] || return 0
     show_git_time . | head -n $LOG_LINES_ON_CD_GIT_DIR
     local _branch=$(git rev-parse --abbrev-ref HEAD)
     echo $_branch
     git_simple_status .
     show_version_here
     gl11
+    return 0
+}
+
+_post_kd_run () {
+    [[ -d . ]] || return 1
+    cdra .
 }
 
 # xxxxxxxxxxxxx
@@ -63,27 +68,41 @@ _post_kd_bash () {
     [[ -f $_init ]] || _init=bash/__init__.sh
     [[ -f $_init ]] || _init=src/bash/__init__.sh
     [[ -f $_init ]] && . $_init
+    return 0
 }
 
 _post_kd_show () {
-    l
+    for path in $(find . -type f -name '*.sw*'); do
+        ls -l $path 2> /dev/null
+        rri $path && continue
+        [[ $? == 1 ]] && break
+    done
+    l 2> /dev/null
 }
 
 _post_kd_venv () {
+    if [[ -e bin/activate ]]; then
+        . bin/activate
+        return 0
+    fi
     # set -x
     local _venvs=$HOME/.virtualenvs
-    [[ -d $_venvs ]] || return 1
+    [[ -d $_venvs ]] || return 0
     local _here=$(realpath $(pwd))
-    local _venvs_path=$_venvs/$(basename $_here)
-    local _path=$_here
-    while [[ ! -d "$_venvs_path" ]]; do
-        [[ -z $_path || $_path == "/" || $_path == $HOME ]] && return 1
-        _path=$(dirname $_path)
-        _venvs_path=$_venvs/$(basename $_path)
+    local _name=$(basename $_here)
+    local _venv_path=
+    for _venv_path in $_venvs/*; do
+        local _venv_dir="$_venv_path"
+        [[ -d "$_venv_dir" ]] || continue
+        local _venv_name=$(basename $_venv_dir)
+        if [[ $_venv_name == $_name ]]; then
+            local _venv_activate="$_venv_dir/bin/activate"
+            [[ -f $_venv_activate ]] || return 1
+            . $_venv_activate
+            return 0
+        fi
     done
-    local _venv_activate="$_venvs_path/bin/activate"
-    [[ -f $_venv_activate ]] || return 1
-    . $_venv_activate 
+    return 0
     # set +x
 }
 
@@ -93,7 +112,7 @@ any_python_scripts_here () {
 }
 
 python_project_here () {
-    [[ -f setup.py || -d ./$_dir_name ]]
+    [[ -f setup.py || -f requirements.txt || -d ./$_dir_name ]]
 }
 
 # _xxxx
@@ -117,6 +136,7 @@ _post_kd_python () {
     if [[ -d $egg_info ]]; then
         ri $egg_info
     fi
+    return 0
 }
 
 _show_todo_here () {
@@ -140,7 +160,7 @@ show_version_here () {
     echo "current_version = 0.0.0" >> $config
     git add $config
     echo "git commit -m\"v0.0.0\""
-    echo bump 
+    echo bump
 }
 
 # xxxxxxxxxxxxxxxxxxx
