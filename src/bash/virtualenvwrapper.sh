85e3bbaad949cfbe3afcff09e9c98dd3e9c0acbc
J Alan Brogan <github@al-got-rhythm.net>
2018-02-08 19:28:31 -0500

    Local adaptations

--- src/bash/virtualenvwrapper.sh
+++ src/bash/virtualenvwrapper.sh
@@ -4,7 +4,12 @@ Welcome_to $BASH_SOURCE
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PYM_HOME=~/hub/pym
-. /usr/local/bin/virtualenvwrapper.sh
+for vew_dir in /usr/local/bin $HOME/.local/bin; do
+    vew_sh=$vew_dir/virtualenvwrapper.sh
+    [[ -f $vew_sh ]] || continue
+    . $vew_sh
+    break
+done
 
 Bye_from $BASH_SOURCE
 
