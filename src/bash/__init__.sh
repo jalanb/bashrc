3a8f87db65f147ad97d2af5d1597167af261bc84
J Alan Brogan <github@al-got-rhythm.net>
2018-05-11 17:04:51 +0100

    KISS for bashrc

--- src/bash/__init__.sh
+++ src/bash/__init__.sh
@@ -4,26 +4,52 @@ Welcome_to $BASH_SOURCE
 
 __version__=0.5.3
 
-_BASH_DIR=~/jab/src/bash
-. $_BASH_DIR/tput.sh
-. $_BASH_DIR/aliases.sh
-. $_BASH_DIR/asserts.sh
-. $_BASH_DIR/first_num.sh
-. $_BASH_DIR/functons.sh
-. $_BASH_DIR/github.sh
-. $_BASH_DIR/hosts.sh
-. $_BASH_DIR/history.sh
-. $_BASH_DIR/jabnet.sh
-. $_BASH_DIR/prompt.sh
-. $_BASH_DIR/pt.sh
-. $_BASH_DIR/pym.sh
-. $_BASH_DIR/repo.sh
-. $_BASH_DIR/require.sh
-. $_BASH_DIR/rf.sh
-. $_BASH_DIR/run_python.sh
-. $_BASH_DIR/virtualenvwrapper.sh
-. $_BASH_DIR/wwts.sh
-. $_BASH_DIR/keyboard/__init__.sh
-. $_BASH_DIR/git/__init__.sh
+local_rc () {
+    local __doc__="There might be local files with new $PWD and commands"
+    local _cd=$1; shift
+    local _cdd=
+    local _rcf=${BASH_RC:-_rc.sh}
+    local _cdf=${BASH_CD:-_cd.in}
+    [[ -f $_rcf ]] && _cdd=$(dirname $(readlink -f $_rcf))
+    if [[ -f $_cdf ]]; then
+        _cdd=$(dirname $(readlink -f $_cdf))
+        _cd=$(cat $_cdf)
+        rm -f $_cdf
+    fi
+    cd $_cd
+    if [[ -f $_rcf ]]; then
+        echo "-f "'$_rcf'"($_rcf)" >&2
+        local _rc="$(cat $_rcf)"
+        [[ -n $_rc ]] || echo "-z "'$_rc'"'$_rc'" >&2
+        [[ -n $_rc ]] && eval $_rc
+        rm -f $_rcf
+    else
+        echo "! -f "'$_rcf'"($_rcf)" >&2
+    fi 2>/dev/null
+}
+
+. ~/bash/tput.sh
+. ~/bash/aliases.sh
+. ~/bash/asserts.sh
+. ~/bash/cde.sh
+. ~/bash/first_num.sh
+. ~/bash/functons.sh
+. ~/bash/github.sh
+. ~/bash/hosts.sh
+. ~/bash/history.sh
+. ~/bash/jabnet.sh
+. ~/bash/prompt.sh
+. ~/bash/pt.sh
+. ~/bash/pym.sh
+. ~/bash/repo.sh
+. ~/bash/require.sh
+. ~/bash/rf.sh
+. ~/bash/run_python.sh
+. ~/bash/virtualenvwrapper.sh
+. ~/bash/wwts.sh
+. ~/bash/keyboard/__init__.sh
+. ~/bash/git/__init__.sh
+
+export _BASH_DIR=~/bash
 
 Bye_from $BASH_SOURCE
