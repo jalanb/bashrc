1e175faaaac9489c4c56e07f2f9caf4c84685e2c
J Alan Brogan <github@al-got-rhythm.net>
2018-05-30 16:39:59 +0100

    Declare functions

--- __init__.sh
+++ __init__.sh
@@ -17,8 +17,16 @@ Welcome_to $BASH_SOURCE
 . ~/hub/kd/kd.sh
 . ~/hub/what/what.sh
 
+_out_funcs=$(declare -f | grep "^[^ ]* ()" | wc -l)
+
 . ~/jab/src/bash/__init__.sh
 . ~/jab/environ.d/__init__.sh
 . ~/jab/local/__init__.sh optional
+
+_all_funcs=$(declare -f | grep "^[^ ]* ()" | wc -l)
+_in_funcs=$(( $_all_funcs - $_out_funcs ))
+echo $_all_funcs functions
+echo $_out_funcs before '~/jab'
+echo $_in_funcs in '~/jab'
 
 Bye_from $BASH_SOURCE
