6e1d64aec71dd5957d5d7f21d39d1b11ab78276e
J Alan Brogan <github@al-got-rhythm.net>
2018-06-09 04:07:46 +0100

    Script to run iPython

---
+++ src/bash/ipython.sh
@@ -0,0 +1,19 @@
+#! /bin/cat
+
+Welcome_to $BASH_SOURCE
+
+ipp () {
+    local __doc__="Run ipython with tab title"
+    local me=$USER
+    local here=$(jostname)
+    local options=-noconfirm_exit
+    local _ipython=${IPYTHON:-ipython}
+    if [[ $($_ipython --help | grep no.*confirm) == "--no-confirm-exit" ]]; then
+        options=--no-confirm-exit
+    fi
+    console_title_on "ipython@${here}" && \
+        $_ipython $options "$@" && \
+        console_title_off "${me}@${here}"
+}
+
+Bye_from $BASH_SOURCE
