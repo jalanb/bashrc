990b2c91c93f4d6239e1c2b135b5bc0340a16d5c
J Alan Brogan <github@al-got-rhythm.net>
2019-06-23 22:15:57 +0100

    Rethinking args for vim

--- src/python/site/script_paths.py
+++ src/python/site/script_paths.py
@@ -1,11 +1,30 @@
 """Provide paths for known scripts"""
 
 
-from pysyte.vim.args import sift
 from pysyte import paths
+
+args = {
+    '2': ['~/jab/todo.txt', '~/python/todo.py'],
+    'scripts': ['~/python/scripts.py', '~/python/site/script_paths.py'],
+    'try': ['~/python/testing/try.py'],
+    'v': ['~/python/vim.py', '~/python/vim_script.py', '~/python/site/script_paths.py'],
+    'rf': ['~/python/rf.py', '~/.config/rf/config'],
+    'jpm': ['~/hub/jpm/jpm/jpm.py', '~/hub/jpm/bin/jpm'],
+    'cd':  ['~/hub/cde/cde.sh', '~/hub/cde/cde.py', '~/.config/cd'],
+    'locate': ['~/jab/bin/locate', '~/python/site/locate.py'],
+    'main': ['~/hub/main.py'],
+    'paths': ['~/hub/pysyte/pysyte/paths.py'],
+    'whap': ['~/hub/what/whap.py'],
+    'whyp': ['~/hub/whyp/whyp.sh', '~/hub/whyp/whyp.py'],
+}
+
+def sift(sys_args):
+    return {a: args[a] for a in sys_args if a in args}
 
 
 def arg_paths(sys_args):
     arg_files = paths.files(sift(sys_args))
-    return arg_files if arg_files else map(
-        paths.pyc_to_py, map(paths.tab_complete, args))
+    if arg_files:
+        return arg_files
+    return [paths.pyc_to_py(a) for a in
+            paths.tab_complete(sys_args, paths.add_stars)]
