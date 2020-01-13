6bf289f60050f426b66c8c115510bd641ba1699b
J Alan Brogan <github@al-got-rhythm.net>
2020-01-13 15:13:52 +0000

    Readability counts

--- src/bash/git/functons.sh
+++ src/bash/git/functons.sh
@@ -335,9 +335,9 @@ gmt () {
 }
 
 gob () {
-    local _new_branch=$(_to_branch "$1" | tr ' ' '_'); shift
+    local _new_branch=$(_to_branch "$1" | tr ' ' '_') _old_commit=$2
+    shift 2
     [[ $_new_branch ]] || return 1
-    local _old_commit=$1; shift
     [[ $_old_commit ]] || _old_commit=$(get_branch)
     show_run_command git checkout -b $_new_branch $_old_commit
 }
