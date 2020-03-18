233ffa068e4c1eed5a8d4a412026dad8688ec003
J Alan Brogan <github@al-got-rhythm.net>
2020-03-18 23:39:36 +0000

    Remove redundant functions

--- environ.d/python.sh
+++ environ.d/python.sh
@@ -8,15 +8,7 @@ PYTHON=${PYTHON:-$(pypath which python 2> ~/bash/fd/2)}
 export PIP_REQUIRE_VIRTUALENV=false
 export PIP_RESPECT_VIRTUALENV=true
 
-_has_ext () {
-    [[ -d $2 ]] && [[ -n $(ls ${2:-.}/*.$1 2>/dev/null | grep -v -e fred -e log  | head -n 1) ]]
-}
-
-_has_py () {
-    _has_ext py "$@"
-}
-
-_try="~/jab/src/python/testing/try.py"
+_try="$HOME/jab/src/python/testing/try.py"
 [[ -f "$_try" ]] || _try=no_file_try_py
 export TRY=$_try
 
@@ -25,7 +17,6 @@ type add_to_a_path >/dev/null 2>&1 || . ~/bash/add_to_a_path.sh
 
 add_to_a_path PYTHONPATH ~/jab/src/python/site
 add_to_a_path PYTHON_SOURCE_PATH ~/jab/src/python
-add_to_a_path PYTHON_SOURCE_PATH ~/src/python
 [[ -f ~/jab/src/python/pythonrc.py ]] && export PYTHONSTARTUP=~/jab/src/python/pythonrc.py
 
 
