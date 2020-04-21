0c1dcf32725f19aa82aaba38cf7c0d05309da893
J Alan Brogan <github@al-got-rhythm.net>
2020-04-21 15:52:37 +0100

    Be more careful of clearing welcome times

--- src/bash/welcome.sh
+++ src/bash/welcome.sh
@@ -1,4 +1,6 @@
-[[ -f /tmp/welcomes ]] || touch /tmp/welcomes
+TMP_WELCOMES=/tmp/welcomes
+
+[[ -f $TMP_WELCOMES ]] || touch $TMP_WELCOMES
 
 echo_host () {
     [[ -z $WELCOME_BYE ]] && return
@@ -9,17 +11,17 @@ echo_host () {
 
 Welcome_to () {
     local _welcome_time=$(error_free_time)
-    echo "$1:$_welcome_time" >> /tmp/welcomes
+    echo "$1:$_welcome_time" >> $TMP_WELCOMES
     # echo_host "Welcome to" $1
 }
 
 Bye_from () {
-    local _welcome_time=$(grep $1 /tmp/welcomes | sed -e s,.*:,, )
+    local _welcome_time=$(grep $1 $TMP_WELCOMES 2>/dev/null | sed -e s,.*:,, )
+    local _name=${1//\//.}
+    [[ $_name ]] && sed -i -e "/$_name/d" $TMP_WELCOMES
     [[ $_welcome_time ]] || return 1
     local _bye_time=$(error_free_time)
     [[ $_bye_time ]] || return 1
-    local _name=${1//\//.}
-    [[ $_name ]] && sed -i -e /$_name/d /tmp/welcomes
     local _diff=$(( $_bye_time - $_welcome_time ))
     [[ $_diff == 0 ]] && return 0
     echo "$_diff seconds: $1"
@@ -28,4 +30,8 @@ Bye_from () {
 
 error_free_time () {
     python_time 2>/dev/null
+}
+
+clean_welcomes () {
+    echo "" > $TMP_WELCOMES
 }
