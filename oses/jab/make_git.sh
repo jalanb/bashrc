ca4d09b497c1a7e3c97503c095fcab64b148cfc1
J Alan Brogan <github@al-got-rhythm.net>
2021-01-30 01:20:01 +0000

    Consolidate git setup

--- oses/jab/make_git.sh
+++ oses/jab/make_git.sh
@@ -5,6 +5,7 @@ make_git () {
     sudo yum -y install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel curl-devel
     # For latest release see $GITHUB/git/git/releases
     GIT_VERSION=2.23.0
+    GITHUB=https://jalanb@github.com
     wget $GITHUB/git/git/archive/v${GIT_VERSION}.tar.gz
     tar xvf v${GIT_VERSION}.tar.gz
     cd git-${GIT_VERSION}
@@ -13,3 +14,9 @@ make_git () {
     sudo make install
 }
 
+main () {
+    sudo timedatectl set-timezone Europe/Dublin
+    make_git
+}
+
+main
--- oses/jab/setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/cat
-
-use ~/jab/src/bash/github.sh
-use make_git.sh
-
-sudo timedatectl set-timezone Europe/Dublin
-make_git
