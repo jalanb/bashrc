62a515da9eb9579197fc9814ce5802e7d86da382
J Alan Brogan <github@al-got-rhythm.net>
2021-05-05 03:00:11 +0100

    Bump version: 0.8.51 → 0.8.52

--- src/bash/__init__.sh
+++ src/bash/__init__.sh
@@ -1,7 +1,7 @@
 #! /bin/cat
 
 
-__version__=0.8.51
+__version__=0.8.52
 
 . ~/bash/tput.sh
 . ~/bash/aliases.sh
--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,3 +1,3 @@
 #! /user/bin/env python
 
-__version__ = '0.8.51'
+__version__ = '0.8.52'
