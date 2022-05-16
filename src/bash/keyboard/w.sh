a573edbe0579fc9be8fac6aafcf40e577552f573
J Alan Brogan <github@al-got-rhythm.net>
2022-05-16 16:51:45 +0100

    Mover wtf() into w.sh

--- src/bash/keyboard/w.sh
+++ src/bash/keyboard/w.sh
@@ -15,3 +15,23 @@ else
     alias .w="whyp_source $WHYP_SOURCE"
     alias wq="quietly whyp "
 fi
+
+wtf () {
+    local status_=$1 name_=Pass out_=1 err_=1
+#   [[ $PROMPT_STATUS ]] && status_=$PROMPT_STATUS
+    [[ $1 ]] && status_=$1 && shift
+    [[ $status_ == 0 ]] || name_=Fail
+    [[ $name_ == Fail ]] && out_=
+    [[ $name_ == Pass ]] && err_=
+    [[ $1 =~ -[qQ] ]] && out_=
+    [[ $1 =~ -[Q] ]] && err_=
+    message_="$name_ $status_ $(face $status_)" 
+    if [[ $name_ == Pass ]]; then
+        [[ $out_ ]] && echo $message_
+    elif [[ $name_ == Fail ]]; then
+        [[ $err_ ]] && echo $message_ >&2
+    fi
+    [[ $name_ == Fail ]] && return $status_
+    return 0
+    local __doc__="""Errors from a status"""
+}
