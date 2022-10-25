7cde158da1a88890fb1da14b71587cf7e88bbffe
J Alan Brogan <github@al-got-rhythm.net>
2022-10-25 02:24:03 +0100

    Rebase from __main__

--- src/bash/python.sh
+++ src/bash/python.sh
@@ -12,8 +12,12 @@ pym () {
     python -m "${args[@]}"
 }
 
-pyp () {
-    pym pip "$@"
+pmp () {
+    pym pip "$@" 
+}
+
+ppd () {
+    pip_install_develop "$@"
 }
 
 ppi () {
@@ -34,6 +38,9 @@ ppu () {
     ppi --upgrade "$@"
 }
 
+ppy () {
+    pmp uninstall -y "$@"
+}
 
 # xxx
 
@@ -55,34 +62,17 @@ ppie () {
     fi
 }
 
-ppii () {
-    local _ipython=$(which ipython)
-    [[ -n $IPYTHON ]] && _ipython=$IPYTHON
-    pypath $_ipython "$@"
-}
-
-ppip () {
-    [[ "$@" ]] || return 1
-    if ppi "$@" | grep -q "pip install --upgrade pip"; then 
-        quietly ppiu pip
-    fi
-}
-
 pipv () {
     local dir_=$PWD setup_py_= setup_cfg= requires_=
     [[ -d "$1" ]] && dir_="$1" && shift
     [[ -f "$dir_/setup.py" ]] && setup_py_="$dir_/setup.py"
     [[ -f "$dir_/setup.cfg" ]] && setup_cfg_="$dir_/setup.cfg"
     if [[ -d "$dir_/requirements" ]]; then
-        requires_="$dir_"'/requirements/*'
+        requires_="$dir_/requirements/*"
     elif [[ -f "$dir_/requirements.txt" ]]; then
         requires_="$dir_/requirements.txt"
     fi
     vim -p "$setup_py_" "$setup_cfg_" "$requires_"
-}
-
-pirr () {
-    ppie "$dir_"
 }
 
 venv () {
@@ -145,69 +135,11 @@ unhash_activate () {
     show_data "python3 is $(which python3)"
 }
 
-pirr () {
-    _install_requirements_from "$@"
-}
-
-piup () {
-    [[ $1 == [-]v ]] && ppiu pip || quietly ppiu pip
-}
-
-venv () {
-    local activate_="$ACTIVATE"
-    quietly make_venv "$@"
-    local venv_=.venv
-    [[ -d "$1" ]] && venv_="$1/.venv"
-    cde_activate_there $venv_ || return 1
-    [[ "$activate_" != "$ACTIVATE" ]] && show_command "cde_activate_there $venv_"
-    quietly whyp python
-}
-
-# _xxxx
-
-# xxxxxxx
-
-# xxxxxxxxx
-
-make_venv () {
-    local __doc__="""Make a .venv"""
-    local root_=. venv_=.venv venv_dir_=.venv
-    if [[ -d "$1" ]]; then
-        root_="$1"
-        venv_dir_="$1/$venv_"
-        shift
-    fi
-    if [[ -d $venv_dir_ ]]; then
-        if [[ $1 =~ ^-(f|-force)$ ]]; then
-            show_command "rm -rf $venv_dir_"
-            rm -rf $venv_dir_
-        else
-            piup
-            return 0
-        fi
-    fi
+unhash_deactivate () {
+    local arg_=
+    for arg_ in python python2 python3 ipython ipython2 ipython3 pudb pudb3 pdb ipdb pip pip2 pip3; do
+        hash -d $arg_ 2>/dev/null
+    done
     [[ $VIRTUAL_ENV ]] && deactivate
-    show_command "python3 -m venv $venv_"
-    python3 -m venv --copies $venv_
-    $python_ -m ensurepip
-    ppiu pip setuptools wheel
-    local requirements_=$(requirements_file $root_)
-    [[ $requirements_ ]] || return 0
-    $venv_python_ -m pip install -r $requirements_
-}
-
-requirements_file () {
-    local dir_=.
-    [[ -d $1 ]] && dir_=$1
-    local name_= file_= requirements_=
-    [[ -f $dir_/requirements.txt ]] && requirements_=$dir_/requirements.txt
-    if [[ -d $dir_/requirements ]]; then
-        for name_ in requirements linting testing devops development; do
-            file_=$dir_/requirements/$name_.txt
-            [[ -f $file_ ]] || continue 
-            requirements_=$file_
-        done
-    fi
-    [[ -f $requirements_ ]] || return
-    echo $requirements_
+    [[ $1 == -q ]] || show_data "python3 is $(which python3)"
 }
