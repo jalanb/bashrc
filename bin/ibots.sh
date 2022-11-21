1f80b5bd8604e5a3c8b0864f6801ac08c635d65e
J Alan Brogan <github@al-got-rhythm.net>
2022-11-21 16:06:19 +0000

    Check whether "--extra-index-url" needed before use
    If VPN is not connected then it is not needed

--- bin/ibots.sh
+++ bin/ibots.sh
@@ -2,8 +2,14 @@
 
 hash -d python3 ipython3
 
+extra () {
+    local wwts_="https://pypi.wwts.com"
+    local extra_="--extra-index-url=$wwts_"
+    curl -s --head  --request GET $wwts_ >/dev/null 2>&1 && echo "--extra-index-url=$wwts_"
+}
+
 pip_upgrade () {
-    python3 -m pip install --extra-index-url=https://pypi.wwts.com --upgrade --no-deps "$@" | grep -v -e already -e Looking -e [uU]ninstall -e [cC]ached -e [cC]ollecting -e build
+    python3 -m pip install -v $(extra) --upgrade "$@" | grep -v -e already -e Looking -e [uU]ninstall -e [cC]ached -e [cC]ollecting -e build
 }
 
 main () {
@@ -16,13 +22,16 @@ main () {
     source .venv/bin/activate
     echo Upgrading pip packages
     pip_upgrade pip pysyte
+    [[ $(extra) ]] || /usr/local/bin/sed --in-place --expression="/wwts.com/d" requirements/requirements.txt
     pip_upgrade -r requirements/development.txt
+    [[ $(extra) ]] || git restore requirements/
+
     export PYTHONHTTPSVERIFY=0
     export TERM=xterm-256color
     PATH=$clone_/.venv/bin:/usr/local/gnu:/usr/local/bin:/Users/jab/bin:/Users/jab/jab/bin:/Users/jab/.local:/bin:/usr/bin \
       PYTHONPATH=. \
       .venv/bin/ipython3 \
-        --profile=bots \
+        --profile=ibots \
         --no-confirm-exit
 }
 
