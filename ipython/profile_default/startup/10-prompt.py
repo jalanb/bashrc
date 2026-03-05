612996915bf9289b12bea1d7de264703445af9b9
J Alan Brogan <github@al-got-rhythm.net>
2026-03-05 16:11:03 +0000

    Update IPython prompt to new style

--- ipython/profile_default/startup/10-prompt.py
+++ ipython/profile_default/startup/10-prompt.py
@@ -6,31 +6,50 @@ import sys
 import time
 
 
-from IPython.utils import coloransi
-from IPython.core.prompts import LazyEvaluate as promptify
+from IPython.terminal.prompts import Prompts
+from pygments.token import Token
+from pygments.style import Style
 
 
-@promptify
-def prompt():
-    color = coloransi.TermColors()
-    virtual_env = os.environ.get("VIRTUAL_ENV", "")
-    virtual_environment = ""
-    if virtual_env:
-        virtual_environment = ".%s" % os.path.basename(virtual_env)
-    return "%s0 %s[%s%s python%s%s %s@%s:%s%s]%s%s\n>>> " % (
-        color.White,
-        color.LightRed,
-        color.LightCyan,
-        time.strftime("%A %Y-%m-%d.%H:%M:%S"),
-        sys.version.split()[0],
-        virtual_environment,
-        os.environ.get("USER", "nobody"),
-        os.uname()[1],
-        os.getcwd(),
-        color.LightRed,
-        color.Normal,
-        color.Green,
-    )
+# Create a custom style that maps our tokens to specific colors
+class BashPromptStyle(Style):
+    styles = {
+        Token.Prompt.Day: 'ansired',            # Red for day
+        Token.Prompt.DateTime: 'ansibrightred', # Light red for date/time
+        Token.Prompt.Python: 'ansigreen',      # Green for python version
+        Token.Prompt.Venv: 'ansibrightgreen',  # Light green for venv
+        Token.Prompt.UserHost: 'ansibrightblue', # Light blue for user@host:path
+        Token.Prompt.Arrow: 'ansigreen',       # Green for >>>
+    }
 
 
-my_prompt = prompt()
+class MyPrompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        virtual_env = os.environ.get("VIRTUAL_ENV", "")
+        virtual_environment = ""
+        if virtual_env:
+            virtual_environment = ".%s" % os.path.basename(virtual_env)
+        
+        return [
+            (Token.Text, "0 "),
+            (Token.Prompt.Day, time.strftime("%A")),
+            (Token.Prompt.DateTime, time.strftime(" %Y-%m-%d.%H:%M:%S")),
+            (Token.Prompt.Python, " python%s" % sys.version.split()[0]),
+            (Token.Prompt.Venv, virtual_environment),
+            (Token.Prompt.UserHost, " %s@%s:%s" % (
+                os.environ.get("USER", "nobody"),
+                os.uname()[1],
+                os.getcwd(),
+            )),
+            (Token.Prompt.Arrow, "\n>>> "),
+        ]
+
+
+# Set the custom prompt and style
+get_ipython().prompts = MyPrompt(get_ipython())
+
+# Try to set the custom style
+try:
+    get_ipython().highlighting_style = BashPromptStyle
+except:
+    pass  # Fallback to default style if this doesn't work
