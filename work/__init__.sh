a984531cbcc0b84208cfcb60db47ef1587eafca2
J Alan Brogan <github@al-got-rhythm.net>
2026-04-20 09:19:32 +0000

    Add .b and .s to source bashrc and work

--- work/__init__.sh
+++ work/__init__.sh
@@ -1,5 +1,11 @@
 #! /bin/cat
 
+WORK_SOURCE="$(readlink -f "$BASH_SOURCE")"
+
+# s is for smbc (.w is used by whyp)
+.s () {
+     source "$WORK_SOURCE"
+}
 
 . ~/jab/work/aliases.sh
 . ~/jab/work/environ.sh
